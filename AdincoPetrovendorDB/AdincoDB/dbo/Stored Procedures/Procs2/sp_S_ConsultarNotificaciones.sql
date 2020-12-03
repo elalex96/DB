5a01ca8eb3336ea60f3388069eb71a16a6a69e76
Daniel Moreno <daniel.moreno@adinco.mx>
2020-12-03 13:43:08 -0600

    issue 1157

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_S_ConsultarNotificaciones.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_S_ConsultarNotificaciones.sql
@@ -1,6 +1,7 @@
-﻿CREATE procedure [dbo].[sp_S_ConsultarNotificaciones]   
+﻿-- [sp_S_ConsultarNotificaciones] '',0
+alter procedure [dbo].[sp_S_ConsultarNotificaciones]   
 @pIdsNotificaciones VARCHAR(500),    
-@pSoloPendientes    BIT          = 0  
+@pSoloPendientes    BIT          = 1  
   
 AS  
    
@@ -18,93 +19,18 @@ AS
              SELECT 0;  
          END; 
 
-   
-/*CREATE TABLE #tmpNotificacionesIds(IdNotificacion INT);  
-     IF(isnull(@pIdsNotificaciones, '') <> '')  
-         BEGIN  
-             INSERT INTO #tmpNotificacionesIds  
-                    SELECT splitdata  
-                    FROM [dbo].[fnSplitString](@pIdsNotificaciones, ',');  
-         END;  
-         ELSE  
-         BEGIN  
-             INSERT INTO #tmpNotificacionesIds  
-             SELECT 0;  
-         END;  
-     SELECT t1.IdNotificacion,    
-            t1.Para,    
-            t1.Asunto,    
-            Mensaje = CAST(t1.Mensaje AS VARCHAR(MAX)),    
-            t1.FechaProgramadaEnvio,    
-            t1.Enviada,    
-            t1.FechaEnvio,    
-            t1.CreadoPor,    
-            t1.CreadoEl,    
-            t1.ModificadoPor,    
-            t1.ModificadoEl,    
-            t1.De  
-     INTO #tmpNotificacion  
-     FROM S_Notificacion t1  
-          INNER JOIN #tmpNotificacionesIds t2 ON(t2.IdNotificacion = t1.IdNotificacion  
-                                                 OR t2.IdNotificacion = 0)  
-     WHERE t1.Enviada = 0;  
-     SELECT n.IdNotificacion,    
-            n.Para,    
-            n.Asunto,    
-            Mensaje = n.Mensaje,    
-            n.FechaProgramadaEnvio,    
-            n.Enviada,    
-            n.FechaEnvio,    
-            TieneError = CAST(CASE  
-                                  WHEN isnull(MAX(error.IdNotificacion), 0) > 0  
-                                       AND enviada = 0  
-                                  THEN 1  
-                                  ELSE 0  
-                              END AS BIT),    
-            n.CreadoPor,    
-            n.CreadoEl,    
-            n.ModificadoPor,    
-            n.ModificadoEl,    
-            n.De,    
-            IdNotificacionMA = 0  
-     FROM #tmpNotificacion n  
-          INNER JOIN #tmpNotificacionesIds tmp ON(tmp.IdNotificacion = n.IdNotificacion  
-                                                  OR tmp.IdNotificacion = 0)  
-          LEFT JOIN S_NotificacionError error ON error.IdNotificacion = n.IdNotificacion  
-     WHERE Enviada = (CASE  
-                          WHEN @pSoloPendientes = 1  
-                          THEN 0  
-                          ELSE Enviada  
-                      END)  
-     GROUP BY n.IdNotificacion,    
-              n.Para,    
-              n.Asunto,    
-              n.Mensaje,    
-              n.FechaProgramadaEnvio,    
-              n.Enviada,    
-              n.FechaEnvio,    
-              n.CreadoPor,    
-              n.CreadoEl,    
-              n.ModificadoPor,    
-              n.ModificadoEl,    
-              n.De  
-     ORDER BY IdNotificacion ASC;*/  
-   
--- UPDATE dbo.S_Notificacion   SET Enviada = 1   WHERE (Para LIKE '%@wintershalldea.com' OR De LIKE '%@wintershalldea.com%') or    Para in ( 'silvina.aviles@pemex.com', 'guillermo.gutierrez@pemex.com','Okko.Ulrichs@dea-group.com'  
--- ,'Natasya.Maura@dea-group.com',   'athenea.coral.cartela@pemex.com', 'rafael.hernandezsal@pemex.com', 'sergio.abraham.parra@pemex.com', 'gregorio.alfaro@pemex.com', 'pablo.emilio.reyes@pemex.com',   'soporte@ogss.com.mx', 'alejandro.torresolan@hotmail.com
---  ')   AND Enviada = 0  
- -- AND Asunto like '%Control%de%Obra%';  
+  
    
 
-   SELECT top 50 
-	N.IdNotificacion,    
-            N.Para,    
-         N.Asunto, 
+   SELECT  top 30
+			N.IdNotificacion,    
+            Para = N.Para,    
+			N.Asunto, 
             Mensaje = CAST(N.Mensaje AS VARCHAR(MAX)),    
             N.FechaProgramadaEnvio,    
             N.Enviada,    
             N.FechaEnvio,    
-            1 AS TieneError,    
+            TieneError = CASE WHEN max(ne.IdNotificacionError) IS NOT NULL THEN 1 ELSE 0 END,    
 			N.CreadoPor,    
             N.CreadoEl,    
             N.ModificadoPor,    
@@ -112,14 +38,29 @@ AS
             N.De,    
             IdNotificacionMA = 0  
      FROM  dbo.S_Notificacion N    (NOLOCK)
-LEFT JOIN dbo.S_NotificacionError NE 
-	ON N.IdNotificacion = NE.IdNotificacion 
-	AND NE.Error <> '|Aún no se cumple la fecha de envío para este correo'
- inner join #tmpNotificacionesIds tmp on    
-  (  
-   tmp.IdNotificacion = n.IdNotificacion OR  
-   tmp.IdNotificacion = 0  
-  )  
+	LEFT JOIN dbo.S_NotificacionError NE  (NOLOCK)
+		ON N.IdNotificacion = NE.IdNotificacion 	
+	 inner join #tmpNotificacionesIds tmp on    
+	  (  
+	   tmp.IdNotificacion = n.IdNotificacion OR  
+	   tmp.IdNotificacion = 0  
+	  )  
      WHERE N.Enviada = 0
-	 AND NE.IdNotificacion IS NULL
+	 and ltrim(rtrim(isnull(n.Para,''))) <> ''
+	 group by N.IdNotificacion,                 
+			N.Asunto, 
+            CAST(N.Mensaje AS VARCHAR(MAX)),    
+            N.FechaProgramadaEnvio,    
+            N.Enviada,    
+            N.FechaEnvio,                    
+			N.CreadoPor,    
+            N.CreadoEl,    
+            N.ModificadoPor,    
+            N.ModificadoEl,    
+            N.De,
+			N.Para
+     having count(distinct ne.IdNotificacionError) < 3--Solo se intentará enviar hasta 3 veces un mismo correo
      ORDER BY N.IdNotificacion ASC;
+
+
+
