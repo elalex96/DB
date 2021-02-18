97c2056334a3c74243964f3c2184bb1f287804ca
Alexander Gomez <alexander.gomez@adinco.mx>
2021-02-18 17:51:21 -0600

    Tabla de guardado de usuarios y sp para la consulta de los mismos

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Tables/TA_EnvioCorreoPersonalizado.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[TA_EnvioCorreoPersonalizado](
+	[IdEnvioCorreo] [int] IDENTITY(1000,1) NOT NULL,
+	[Correo] [varchar](200) NULL,
+	[NombreDestinatario] [varchar](100) NULL,
+	[IdUsuario] [int] NULL,
+	[IdProveedor] [int] NULL,
+	[IdContrato] [int] NULL,
+	[TipoNotificacion] [nvarchar](50) NULL,
+	[Activo] [bit] NULL,
+	[CreadoEl] [datetime] NULL,
+ CONSTRAINT [PK_TA_EnvioCorreoPersonalizado] PRIMARY KEY CLUSTERED 
+(
+	[IdEnvioCorreo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
