ec2e331132ed6d648af7777f5fc7eb4d0861e920
Daniel Moreno <daniel.moreno@adinco.mx>
2021-03-10 15:23:09 -0600

    NUEVOS PROCEDIMIENTOS

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_CO_ProgramaImplementaElemento_Grd.sql
@@ -0,0 +1,53 @@
+﻿
+create proc sp_CO_ProgramaImplementaElemento_Grd
+(
+	@pIdProgramaImplementaPolitica	int,
+	@pIdProgramaImplementa			int
+)
+as
+begin
+
+	--select		@pIdProgramaImplementa = 21, @pIdProgramaImplementaPolitica = 139
+
+	declare		@orden int
+	select		@orden =	max(isnull(e.Orden,0))
+	from		CO_ProgramaImplementaElemento	e
+	inner join	CO_ProgramaImplementaPoliticas	p
+	on			e.IdProgramaImplementaPolitica	=	p.IdProgramaImplementaPolitica
+	and			e.IdProgramaImplementa			=	p.IdProgramaImplementa
+	where		e.IdProgramaImplementaPolitica	=	@pIdProgramaImplementaPolitica--139
+	and			e.IdProgramaImplementa			=	@pIdProgramaImplementa--21
+
+	select		Id = case when e.Orden is null then   ROW_NUMBER() OVER (	ORDER BY e.Orden   )+@orden else e.Orden end,
+				e.IdProgramaImplementa,
+				e.IdProgramaImplementaPolitica,
+				e.IdProgramaImplementaElemento,			
+				Elemento						=	cast(e.Descripcion as varchar(50)),
+				Orden							=	isnull(e.Orden,0)
+	into		#tmp
+	from		CO_ProgramaImplementaElemento	e
+	inner join	CO_ProgramaImplementaPoliticas	p
+	on			e.IdProgramaImplementaPolitica	=	p.IdProgramaImplementaPolitica
+	and			e.IdProgramaImplementa			=	p.IdProgramaImplementa
+	where		e.IdProgramaImplementaPolitica	=	@pIdProgramaImplementaPolitica--139
+	and			e.IdProgramaImplementa			=	@pIdProgramaImplementa--21
+
+	update		CO_ProgramaImplementaElemento
+	set			CO_ProgramaImplementaElemento.orden	=	t1.Id	
+	from		CO_ProgramaImplementaElemento		e
+	inner join	#tmp								t1
+	on			t1.IdProgramaImplementaElemento		=	e.IdProgramaImplementaElemento
+
+	select		e.IdProgramaImplementa,
+				e.IdProgramaImplementaPolitica,
+				e.IdProgramaImplementaElemento,			
+				Elemento						=	cast(e.Descripcion as varchar(50)),
+				Orden							=	isnull(e.Orden,0)
+	from		CO_ProgramaImplementaElemento	e
+	inner join	CO_ProgramaImplementaPoliticas	p
+	on			e.IdProgramaImplementaPolitica	=	p.IdProgramaImplementaPolitica
+	and			e.IdProgramaImplementa			=	p.IdProgramaImplementa
+	where		e.IdProgramaImplementaPolitica	=	@pIdProgramaImplementaPolitica--139
+	and			e.IdProgramaImplementa			=	@pIdProgramaImplementa--21
+	order by	e.Orden
+end
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_ProgramaImplementaPolitica_ReOrden.sql
@@ -0,0 +1,72 @@
+﻿create proc sp_ProgramaImplementaPolitica_ReOrden --21, 139, 1047, 8353,0,1
+(
+	@pIdProgramaImplementa			int,
+	@pIdProgramaImplementaPolitica	int,
+	@pOrden							int, 
+	@pUpDown						int
+)
+as
+begin
+
+	if(@pOrden = 1 and @pUpDown=1)
+	begin
+		select	@pIdProgramaImplementaPolitica	=	IdProgramaImplementaPolitica,
+				@pOrden							=	Orden,
+				@pUpDown						=	-1
+				
+		from	CO_ProgramaImplementaPoliticas where IdProgramaImplementa = @pIdProgramaImplementa and Orden = 2
+	end
+
+	--select  @pIdProgramaImplementa,@pIdProgramaImplementaPolitica,@pOrden,@pUpDown
+
+	select @pOrden = isnull(@pOrden,0)+@pUpDown
+	
+	if(@pOrden<1)
+	begin
+		select @pOrden = 1
+	end
+	
+	update		CO_ProgramaImplementaPoliticas
+	set			Orden							=	@pOrden
+	where		IdProgramaImplementaPolitica	=	@pIdProgramaImplementaPolitica
+	
+	declare		@IdProgramaImplementaPolitica	int
+	
+	select		@IdProgramaImplementaPolitica	=	p.IdProgramaImplementaPolitica
+	from		CO_ProgramaImplementaPoliticas	p
+	where		p.IdProgramaImplementa			=	@pIdProgramaImplementa
+	and			p.IdProgramaImplementaPolitica	<>	@pIdProgramaImplementaPolitica
+	and			p.Orden							=	@pOrden
+
+	Update		CO_ProgramaImplementaPoliticas
+	set			CO_ProgramaImplementaPoliticas.Orden	=	p.Orden +1
+	from		CO_ProgramaImplementaPoliticas	p
+	where		p.IdProgramaImplementa			=	@pIdProgramaImplementa
+	and			p.IdProgramaImplementaPolitica	<>	@pIdProgramaImplementaPolitica
+	and			p.Orden							>=	@pOrden
+	
+	update		CO_ProgramaImplementaElemento
+	set			Orden							=	@pOrden+(@pUpDown*-1)
+	where		IdProgramaImplementaElemento	=	@IdProgramaImplementaPolitica
+
+	select		Orden							=	ROW_NUMBER() OVER( ORDER BY p.Orden), 
+				p.IdProgramaImplementa,
+				p.IdProgramaImplementaPolitica,
+				OrdenPolitica					=	p.Orden				
+	into		#tmp
+	from		CO_ProgramaImplementaPoliticas	p
+	where		p.IdProgramaImplementa			=	@pIdProgramaImplementa
+	--and		p.IdProgramaImplementaPolitica	=	@pIdProgramaImplementaPolitica
+	and			p.Orden							is not null
+	order by	p.Orden
+	
+	--select 999, * from #tmp
+	
+	Update		CO_ProgramaImplementaPoliticas
+	set			CO_ProgramaImplementaPoliticas.Orden	=	t1.Orden
+	from		CO_ProgramaImplementaPoliticas	p
+	inner join	#tmp							t1
+	on			t1.IdProgramaImplementa			=	p.IdProgramaImplementa
+	and			t1.IdProgramaImplementaPolitica	=	p.IdProgramaImplementaPolitica
+	
+end
