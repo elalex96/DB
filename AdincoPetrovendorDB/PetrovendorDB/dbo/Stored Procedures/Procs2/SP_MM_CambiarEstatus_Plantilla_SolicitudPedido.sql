9820611986ccbaa28cf01d4c63888266a7ab7254
Daniel Antonio Cruz <daniel.antonio@adinco.mx>
2021-03-10 13:56:32 -0600

    Se actualziuo sp para guardar bitacora de las plantillas que son usadas para generar una solicitud de pedido

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_CambiarEstatus_Plantilla_SolicitudPedido.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_CambiarEstatus_Plantilla_SolicitudPedido.sql
@@ -1,23 +1,52 @@
-﻿-- =============================================
+﻿USE [Petrovendor]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_MM_CambiarEstatus_Plantilla_SolicitudPedido'
+)
+    DROP PROCEDURE SP_MM_CambiarEstatus_Plantilla_SolicitudPedido;
+GO 
+/****** Object:  StoredProcedure [dbo].[SP_MM_CambiarEstatus_Plantilla_SolicitudPedido]    Script Date: 10/03/2021 11:07:08 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		<Alexander Gomez>
 -- Create date: <21/11/2019>
 -- Description:	<cambiar el estado de la plantilla de solicitud de pedido>
 -- =============================================
-create PROCEDURE [dbo].[SP_MM_CambiarEstatus_Plantilla_SolicitudPedido]
+CREATE PROCEDURE [dbo].[SP_MM_CambiarEstatus_Plantilla_SolicitudPedido]
 	-- Add the parameters for the stored procedure here
 	@IdPlantillaSolicitudPedido INT,
-	@IdUsuario INT
+	@IdUsuario INT,
+	@SolicitudPedidoId INT = 0
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
+	DECLARE @Bitacora NVARCHAR(MAX)
+
+	SELECT @Bitacora=Bitacora 
+	FROM MM_Plantillas_SolicitudPedido 
+	WHERE IdPlantillaSolicitudPedido = @IdPlantillaSolicitudPedido;
+
+	IF ISNULL(@Bitacora,'')=''
+		SET @Bitacora =CONCAT('Genero SP #',ISNULL(@SolicitudPedidoId,0),' - ',(FORMAT(GETDATE(),'dd/MM/yyyy hh:mm tt')))
+	ELSE 
+		SET @Bitacora =CONCAT(@Bitacora,',#',ISNULL(@SolicitudPedidoId,0),' - ',(FORMAT(GETDATE(),'dd/MM/yyyy hh:mm tt')))
+
     -- Insert statements for procedure here
-	UPDATE dbo.MM_Plantillas_SolicitudPedido
+	UPDATE MM_Plantillas_SolicitudPedido
 	SET Enviada = 1,
 	EnviadoPor = @IdUsuario,
-	FechaModificacion = GETDATE()
+	FechaModificacion = GETDATE(),
+	Bitacora= @Bitacora
 	WHERE IdPlantillaSolicitudPedido = @IdPlantillaSolicitudPedido;
 
 END
+
