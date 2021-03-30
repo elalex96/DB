42b8fef0db85fb539b01e9051ef355454d6ee255
RamonPortales <ramon.portales@adinco.mx>
2021-03-30 17:12:18 -0600

    Se concluyó el requerimiento

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/spCargaProgramadaTrabajoIns.sql
@@ -0,0 +1,47 @@
+﻿if exists (select * from sys.procedures cedures where name = 'spCargaProgramadaTrabajoIns')
+begin
+	drop proc spCargaProgramadaTrabajoIns
+end
+
+go
+
+create proc spCargaProgramadaTrabajoIns
+(
+	@Actividad			varchar(200),
+	@Unidad				varchar(100),
+	@Cantidad			decimal(10,2),
+	@UnidadesActividad	decimal(10,2),
+	@UnidadesTrabajo	decimal(10,2),
+	@Estatus			varchar(100),
+	@IdContrato			int
+)
+as
+begin
+	declare	@IdCargaProgramadaTrabajo int
+	select @IdCargaProgramadaTrabajo = isnull(max(IdCargaProgramadaTrabajo),0)+1 from CargaProgramadaTrabajo
+
+	insert into CargaProgramadaTrabajo
+				(
+					IdCargaProgramadaTrabajo,
+					Actividad,
+					Unidad,
+					Cantidad,
+					UnidadesActividad,
+					UnidadesTrabajo,
+					IdContrato,
+					Fecha,
+					Estatus
+				)
+			values
+				(
+					@IdCargaProgramadaTrabajo,
+					@Actividad,
+					@Unidad,
+					@Cantidad,
+					@UnidadesActividad,
+					@UnidadesTrabajo,
+					@IdContrato,
+					Getdate(),
+					@Estatus
+				)
+end
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables2/CargaProgramadaTrabajo.sql
@@ -0,0 +1,22 @@
+﻿use Adinco
+
+go
+
+if not exists (select * from sys.tables where name  = 'CargaProgramadaTrabajo')
+begin
+	create table CargaProgramadaTrabajo
+	(
+		IdCargaProgramadaTrabajo	int,
+		Actividad					varchar(200),
+		Unidad						varchar(100),
+		Cantidad					decimal (10,2),
+		UnidadesActividad			decimal (10,2),
+		UnidadesTrabajo				decimal (10,2),
+		Estatus						varchar(100),
+		IdContrato					int,
+		Fecha						smalldatetime
+		constraint					PK_CargaProgramadaTrabajo	primary key (IdCargaProgramadaTrabajo)
+	)
+end
+
+--Se agregaron columnas
