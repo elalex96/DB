dd3d96baafb58037b0aa7ee2f3d987c5ed56b86a
RamonPortales <ramon.portales@adinco.mx>
2021-04-09 13:22:01 -0500

    Se agregaron Procedimientos para la funcionalidad de la pantalla AccesosCentrosCosto.aspx

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/spCentroCostoFiltroIns.sql
@@ -0,0 +1,57 @@
+﻿use Petrovendor
+
+go
+
+if exists (select * from sys.procedures where name  = 'spCentroCostoFiltroIns')
+begin
+	drop proc spCentroCostoFiltroIns
+end
+
+go
+
+create proc spCentroCostoFiltroIns
+(
+	@IdCentroCosto	int,
+	@IdUsuario		int,
+	@IdProveedor	int
+)
+as
+begin
+	if exists(
+		select * 
+		from	CentroCostoFiltro 
+		where	IdCentroCosto		=	@IdCentroCosto
+		and		IdUsuario			=	@IdUsuario
+		and		IdProveedor			=	@IdProveedor
+	)
+	begin
+			update	CentroCostoFiltro 
+			set		Activo				=	1,
+					ModificadoEl		=	getdate()
+			where	IdCentroCosto		=	@IdCentroCosto
+			and		IdUsuario			=	@IdUsuario
+			and		IdProveedor			=	@IdProveedor
+	end
+	else
+	begin
+	
+		insert into CentroCostoFiltro
+					(
+						IdCentroCosto,
+						IdUsuario,
+						IdProveedor,
+						Activo,
+						CreadoEl,
+						ModificadoEl
+					)
+				values
+					(
+						@IdCentroCosto,
+						@IdUsuario,
+						@IdProveedor,
+						1,
+						getdate(),
+						null
+					)
+	end
+end
---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/sp_S_Usuario_Cmb.sql
@@ -0,0 +1,18 @@
+﻿use Petrovendor
+
+go
+
+if exists(select * from sys.procedures where name = 'sp_S_Usuario_Cmb')
+begin
+	drop proc sp_S_Usuario_Cmb
+end
+
+go
+
+create proc sp_S_Usuario_Cmb
+as
+begin
+		select	IdUsuario,
+				Nombre
+		from	S_Usuario
+end
