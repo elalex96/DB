f02c742ef440d3d9131435878e3b22e28f511bbb
Pedro Pouchoulen <pedro.pouchoulen@adinco.mx>
2021-05-28 01:35:30 -0500

    Se modifica el sp de division de gastos

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_CO_DividirGastoPorEstado.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_CO_DividirGastoPorEstado.sql
@@ -2,7 +2,6 @@ CREATE PROCEDURE [dbo].[SP_CO_DividirGastoPorEstado]
 	@IdFactura int, @IdUsuario INT, @Porcentaje decimal(18,4), @IdClvEstado int, @RegistroOriginal nvarchar(max)
     AS
 	BEGIN
-
 		CREATE TABLE #RegistroABorrar(IdRegistro INT)
 
 		INSERT INTO #RegistroABorrar
@@ -10,9 +9,6 @@ CREATE PROCEDURE [dbo].[SP_CO_DividirGastoPorEstado]
 		FROM dbo.fnSplitString ( @RegistroOriginal, ',' )
 
 		CREATE TABLE #REGISTRO_RES(IdRegistro INT, IdFactura INT, MontoRegistro DECIMAL(18, 4), CreadoPor INT, MontoRegistroPorcentaje  DECIMAL(18, 4))
-
-		INSERT INTO #REGISTRO_RES(IdRegistro, IdFactura, MontoRegistro, CreadoPor, MontoRegistroPorcentaje)
-		SELECT IdRegistro, IdFactura, MontoRegistro, CreadoPor, (MontoRegistro * @Porcentaje) / 100 FROM CO_Registro WHERE IdFactura = @IdFactura
 
 		IF NOT EXISTS(SELECT 1 FROM RespaldoRegistro WHERE IdFactura = @IdFactura)
 		BEGIN
@@ -25,10 +21,11 @@ CREATE PROCEDURE [dbo].[SP_CO_DividirGastoPorEstado]
 								IsEditable, CostosAtribuiblesAdministracion, EstadoDePresentacion, MesCertificadoCIEP, IdGastoRubro, PCN, Importado, BitPCNP,
 								IdCBSISH, IdAceptacionPedidoDetalle, MesGasto, ModificadoEn, CapexOpexEdicion 
 			FROM CO_Registro R 
-			WHERE IdFActura = @IdFactura
-
-			
+			WHERE IdFActura = @IdFactura		
 		END
+			
+			INSERT INTO #REGISTRO_RES(IdRegistro, IdFactura, MontoRegistro, CreadoPor, MontoRegistroPorcentaje)
+			SELECT IdRegistro, IdFactura, MontoRegistro, CreadoPor, (MontoRegistro * @Porcentaje) / 100 FROM RespaldoRegistro WHERE IdFactura = @IdFactura
 
 			INSERT INTO CO_Registro(IdPrograma, IdFactura, MontoRegistro, InicioEjecucion, FinEjecucion, Comentarios, MesPresentacion, IdEstado, IdUsuarioCreadoPor,
 							IdUsuarioModPor, FecMovto, IdInstalacion, CreadoPor, Fila, IdPedimentoComprobante, CvTipoDocFacturacion, IdCatalogoCuentasSH, Poliza,
@@ -38,7 +35,7 @@ CREATE PROCEDURE [dbo].[SP_CO_DividirGastoPorEstado]
 							IdUsuarioModPor, FecMovto, IdInstalacion, @IdUsuario, Fila, IdPedimentoComprobante, CvTipoDocFacturacion, IdCatalogoCuentasSH, Poliza,
 							IsEditable, CostosAtribuiblesAdministracion, EstadoDePresentacion, MesCertificadoCIEP, IdGastoRubro, PCN, Importado, BitPCNP,
 							IdCBSISH, IdAceptacionPedidoDetalle, MesGasto, ModificadoEn, CapexOpexEdicion 
-			FROM CO_Registro R 
+			FROM RespaldoRegistro R 
 			INNER JOIN #REGISTRO_RES RES ON R.IdRegistro = RES.IdRegistro
 			
 			DELETE FROM CO_Registro WHERE IdRegistro IN (SELECT IdRegistro FROM #RegistroABorrar)
