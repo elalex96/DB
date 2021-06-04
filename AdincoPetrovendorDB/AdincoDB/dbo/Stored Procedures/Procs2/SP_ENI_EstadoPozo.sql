607571e8d065da800368633878a9bd0dbe93c6f6
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-06-04 15:40:44 -0500

    sp creados

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_ENI_EstadoPozo.sql
@@ -0,0 +1,34 @@
+﻿USE [Adinco]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_ENI_EstadoPozo]    Script Date: 04/06/2021 01:32:05 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Alexander Gomez>
+-- Create date: <02-06-2021>
+-- Description:	<cosnulta de estados de pozozs>
+-- =============================================
+ALTER PROCEDURE [dbo].[SP_ENI_EstadoPozo]
+	-- Add the parameters for the stored procedure here
+	@IdContrato INT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT DISTINCT
+		EP.idEstatus,
+		EP.TipoEstatus
+	FROM dbo.CO_Instalacion I
+             JOIN dbo.CO_ActividadCIEP A ON I.IdActividad = A.IdActividad
+             JOIN dbo.CO_EstadoPozos EP ON EP.idEstatus = I.IdEstatus
+             JOIN dbo.CO_AreaContractual AC ON I.IdAreaContractual = AC.IdAreaContractual
+             JOIN dbo.CO_Contrato C ON AC.IdAreaContractual = C.IdAreaContractual
+        WHERE C.IdContrato = @IdContrato
+              AND A.IdActividad = 5;
+
+END
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_ENI_VerificacionUsuarioAdmin.sql
@@ -0,0 +1,41 @@
+﻿USE [Adinco]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_ENI_VerificacionUsuarioAdmin]    Script Date: 04/06/2021 01:34:31 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Alexander Gomez>
+-- Create date: <02-06-2021>
+-- Description:	<Verificar si el usuario logueado es admin>
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_ENI_VerificacionUsuarioAdmin]
+	-- Add the parameters for the stored procedure here
+	@IdUsuario  INT,
+	@IdContrato INT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	DECLARE @ADMIN BIT = 0; 
+	DECLARE @TIPOUSUARIO NVARCHAR(100) = (SELECT TOP 1 R.Rol
+											FROM dbo.AP_Usuario AS US
+											JOIN dbo.AP_PerfilUsuario AS PU ON US.UsuarioID = PU.UsuarioID
+											JOIN dbo.AP_Perfil as P ON PU.PerfilID = P.IdPerfil
+											JOIN dbo.AP_Rol AS R ON P.IdPerfil = R.IdRol
+										WHERE US.UsuarioID = @IdUsuario AND P.IdContrato = @IdContrato);
+
+	IF @TIPOUSUARIO <> 'Admon Contrato'
+	BEGIN
+
+		SET @ADMIN = 1;
+
+	END
+
+	SELECT @ADMIN;
+
+END
