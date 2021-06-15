2209906717c727acc3cedc885e5628da590944f3
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-06-15 16:10:10 -0500

    modificaciones y agregado de sp para issue 1141

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_ADMIN_INS_OperadoresCNProveedor.sql
@@ -0,0 +1,23 @@
+﻿-- =============================================
+-- Author:		<Alexander Gomez>
+-- Create date: <15/06/2021>
+-- Description:	<Consulta para verificar las operadores que pueden generar carta CN DE PROVEEDOR A PROVEEDOR>
+-- =============================================
+CREATE PROCEDURE SP_ADMIN_INS_OperadoresCNProveedor
+	-- Add the parameters for the stored procedure here
+	@IdProveedor INT,
+	@RFC NVARCHAR(100),
+	@Observaciones NVARCHAR(1000),
+	@Activo BIT
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	INSERT INTO DEA_Proveedor (IdProveedor,RFC,Observaciones,Activo) VALUES (@IdProveedor, @RFC, @Observaciones,@Activo);
+
+END
+GO
