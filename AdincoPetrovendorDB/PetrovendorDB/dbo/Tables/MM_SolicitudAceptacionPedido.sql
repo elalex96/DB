e581407f6e108048f59f67b0179989983c93f1a4
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2021-06-29 19:26:46 -0500

    Se adjunta sp`s relacionados al issue relacioando

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Tables/MM_SolicitudAceptacionPedido.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[MM_SolicitudAceptacionPedido](
+	[IdSolicitudAceptacionPedido] [int] IDENTITY(1000,1) NOT NULL PRIMARY KEY,
+	[IdProveedorVenta] [int] NULL,
+	[IdPedido] [int] NULL,
+	[IdAceptacionPedido] [int] NULL,
+	[Comentario] [nvarchar](max) NULL,
+	[NombreUsuarioEntrega] [nvarchar](300) NULL,	
+	[NombreRecibidoPor] [nvarchar](300) NULL,	
+	[Activo] [bit] NULL,
+	[CreadoEl] [datetime] NULL,	
+	[CreadorPor] [int] NULL,
+	[ModificadoEl] [datetime] NULL,
+	[ModificadoPor] [int] NULL
+) 
+
+
+
---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Tables/MM_SolicitudAceptacionPedidoDetalle.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[MM_SolicitudAceptacionPedidoDetalle](
+	[IdSolicitudAceptacionPedidoDetalle] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[IdSolicitudAceptacionPedido] [int] NULL,
+	[IdPedidoDetalle] [int] NULL,
+	[Cantidad] [float] NULL,
+	[CreadoPor] [int] NULL,
+	[CreadoEl] [datetime] NULL,
+	[EditadoPor] [int] NULL,
+	[EditadoEl] [datetime] NULL
+) 
+GO
