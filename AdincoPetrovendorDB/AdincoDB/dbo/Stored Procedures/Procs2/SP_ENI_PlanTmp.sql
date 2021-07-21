7379de9dec4ed17cf330114336cb5bf5b332d76b
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-07-21 13:07:47 -0500

    Update SP_ENI_PlanTmp.sql

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_ENI_PlanTmp.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_ENI_PlanTmp.sql
@@ -1,9 +1,9 @@
-﻿-- =============================================
+-- =============================================
 -- Author:		Manuel Cruz
 -- Create date: 20-05-2020
 -- Description:	
 -- =============================================
-CREATE PROCEDURE SP_ENI_PlanTmp 
+CREATE PROCEDURE [dbo].[SP_ENI_PlanTmp] 
 -- Add the parameters for the stored procedure here
 @IdContrato INT, 
 @IdUsuario  INT
@@ -14,7 +14,19 @@ AS
          SET NOCOUNT ON;
 
          -- Insert statements for procedure here
-         SELECT Fecha, 
-                Evento
-         FROM dbo.ENI_PlanTmp;
+         --SELECT Fecha, 
+         --       Evento
+         --FROM dbo.ENI_PlanTmp;
+
+		 SELECT
+			E.DocumentoEntregable, ISNULL(IE.FechaRealEntregaRegulador,IE.FechaCalculadaEntregaReg) AS Fecha, E.DocumentoEntregable AS Evento
+		FROM
+			EN_Entregable E
+		JOIN EN_ContratoEntregable CE ON E.IdEntregable = CE.IdEntregable
+			AND E.BitInterno = 1
+			AND CE.IdContrato = @IdContrato
+			AND E.DocumentoEntregable LIKE 'Aprobación%Plan%'
+			AND CE.Activo = 1
+		JOIN EN_InstanciasEntregable IE ON CE.IdContratoEntregable = IE.IdContratoEntregable
+
      END;
