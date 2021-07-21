2cf351b8671ba0a4ee8e5b2a4bc515b3f8fa80c7
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-07-21 13:08:34 -0500

    Update SP_ENIPMTSpreadsheet.sql

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_ENIPMTSpreadsheet.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_ENIPMTSpreadsheet.sql
@@ -1,9 +1,9 @@
-﻿-- =============================================
+-- =============================================
 -- Author:		Manuel Cruz
 -- Create date: 26-06-2020
 -- Description:	
 -- =============================================
-CREATE PROCEDURE SP_ENIPMTSpreadsheet
+CREATE PROCEDURE [dbo].[SP_ENIPMTSpreadsheet] --10054,0
 -- Add the parameters for the stored procedure here
 @IdContrato INT, 
 @IdUsuario  INT
@@ -14,16 +14,18 @@ AS
          SET NOCOUNT ON;
 
          -- Insert statements for procedure here
-         SELECT TOP 1 A.IdAWSDocumento, 
-                      A.NombreArchivo, 
-                      A.Bucket, 
-                      A.Folder, 
+         SELECT TOP 1 
+					  A.IdAWSDocumento, 
+                      A.NombreArchivo,
+					  'adinco' as Bucket,                       
+					  A.Folder, 
                       UPPER(A.UUIDAmazon) AS UUIDAmazon, 
                       LTRIM(RTRIM(SUBSTRING(A.NombreArchivo, CHARINDEX('.', A.NombreArchivo, LEN(A.NombreArchivo)-5), LEN(A.NombreArchivo)))) AS TipoArchivo
          FROM dbo.AWS_DocumentoENI A
               JOIN dbo.CO_Contrato C ON C.IdContrato = A.IdContrato
               JOIN dbo.AP_Usuario U ON A.CreadoPor = U.UsuarioID
-         WHERE A.Folder = 'ENIArchivos/PMT/'
-               AND A.IdContrato = @IdContrato --3
+         WHERE A.Folder = 'ENIArchivos/PROGRAMAMÍNIMODETRABAJO/'
+               AND A.IdContrato = @IdContrato 
          ORDER BY A.CreadoEl DESC;
+
      END;
