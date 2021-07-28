7e922930b71ed042909f86fe6a36c94f35a56b2f
RamonPortales <ramon.portales@adinco.mx>
2021-07-28 15:03:42 -0500

    Se actualizaron algunos sp's y unas tablas

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DG_AgDocumentoSistemaGestion.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DG_AgDocumentoSistemaGestion.sql
@@ -1,4 +1,10 @@
-﻿-- =============================================
+﻿if exists (select * from sys.procedures where name = 'SP_DG_AgDocumentoSistemaGestion')
+begin
+	drop proc SP_DG_AgDocumentoSistemaGestion
+end
+
+go
+-- =============================================
 -- Author:		<Alexander Gomez>
 -- Create date: <30/052017>
 -- Description:	<Procedimiento para insertar un documento en especidico(INE, RCF, ACTA CONSTITUTIVA) en la tabla S_Documento>
@@ -21,53 +27,55 @@ CREATE PROCEDURE[dbo].[SP_DG_AgDocumentoSistemaGestion]
 	@FechaVigenciaInicio         datetime,
     @FechaVigenciaTermino        datetime,
     @MetodosProcesosCertificados nvarchar(500),
-    @Carpeta NVARCHAR(MAX),
-	@Mime NVARCHAR(MAX),
-	@Extension NVARCHAR(MAX),
-	@Identificador NVARCHAR(MAX)
+    @Carpeta						NVARCHAR(MAX),
+	@Mime							NVARCHAR(MAX),
+	@Extension						NVARCHAR(MAX),
+	@Identificador					NVARCHAR(MAX),
+	@bucket							NVARCHAR(MAX)
 AS
 
 BEGIN
 
 	INSERT INTO [dbo].[PV_SistemaGestion]
          (
-		 [IdProveedor]
-		,[IdTipoDocSG]
-		,[Documento]
-		,[Activo]
-		,[NombreCertificacion]
-        ,[CasaCertificadora]
-		,[NoDeCertificado]
-		,[FechaEmisionCertificado]
-		,[FechaVigenciaInicio]
-		,[FechaVigenciaTermino]
-		,[MetodosProcesosCertificados]
-		,Carpeta
-		,Mime
-		,Extension
-		,Identificador
+			IdProveedor,
+			IdTipoDocSG,
+			Documento,
+			Activo,
+			NombreCertificacion,
+			CasaCertificadora,
+			NoDeCertificado,
+			FechaEmisionCertificado,
+			FechaVigenciaInicio,
+			FechaVigenciaTermino,
+			MetodosProcesosCertificados,
+			Carpeta,
+			Mime,
+			Extension,
+			Identificador,
+			Bucket
          )
          VALUES
          (
-		 @IdProveedor,
-		 @IdTipoDocSG,
-		 @Documento,
-		 1,
-		 @NombreCertificacion,
-		 @CasaCerficadora,            
-		 @NoDeCertificado,           
-		 @FechaEmisionCertificado,    
-		 @FechaVigenciaInicio,         
-		 @FechaVigenciaTermino,        
-		 @MetodosProcesosCertificados,
-		 @Carpeta,
-		 @Mime,
-		 @Extension,
-		 @Identificador
+			 @IdProveedor,
+			 @IdTipoDocSG,
+			 @Documento,
+			 1,
+			 @NombreCertificacion,
+			 @CasaCerficadora,            
+			 @NoDeCertificado,           
+			 @FechaEmisionCertificado,    
+			 @FechaVigenciaInicio,         
+			 @FechaVigenciaTermino,        
+			 @MetodosProcesosCertificados,
+			 @Carpeta,
+			 @Mime,
+			 @Extension,
+			 @Identificador,
+			 @bucket
          )
 		 IF @@ERROR <> 0
              SELECT 'false' AS msj;
              ELSE
          SELECT 'true' AS msj;
 END
-
--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DG_AgDocumentoSistemaGestionOC.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DG_AgDocumentoSistemaGestionOC.sql
@@ -1,4 +1,10 @@
-﻿-- =============================================
+﻿if exists (select * from sys.procedures where name = 'SP_DG_AgDocumentoSistemaGestionOC')
+begin
+	drop proc	SP_DG_AgDocumentoSistemaGestionOC
+end
+
+go
+-- =============================================
 -- Author:		<Alexander Gomez>
 -- Create date: <30/052017>
 -- Description:	<Procedimiento para insertar un documento en especidico(INE, RCF, ACTA CONSTITUTIVA) en la tabla S_Documento>
@@ -8,7 +14,7 @@
 -- Create date: 26-09-2019
 -- Description: Add refencias s3 
 -- =============================================
-CREATE PROCEDURE[dbo].[SP_DG_AgDocumentoSistemaGestionOC] 
+CREATE PROCEDURE SP_DG_AgDocumentoSistemaGestionOC 
 
 	-- Insertar Documento nuevo---
 	@IdProveedor                 int,
@@ -21,54 +27,55 @@ CREATE PROCEDURE[dbo].[SP_DG_AgDocumentoSistemaGestionOC]
 	@FechaVigenciaInicio         datetime,
     @FechaVigenciaTermino        datetime,
     @MetodosProcesosCertificados nvarchar(500),
-    @Carpeta NVARCHAR(MAX),
-	@Mime NVARCHAR(MAX),
-	@Extension NVARCHAR(MAX),
-	@Identificador NVARCHAR(MAX)
-
+    @Carpeta						NVARCHAR(MAX),
+	@Mime							NVARCHAR(MAX),
+	@Extension						NVARCHAR(MAX),
+	@Identificador					NVARCHAR(MAX),
+	@Bucket							NVARCHAR(MAX)
 AS
 
 BEGIN
 
-	INSERT INTO [dbo].[PV_SistemaGestion]
+	INSERT INTO dbo.PV_SistemaGestion
          (
-		 [IdProveedor]
-		,[IdTipoDocSG]
-		,[Documento]
-		,[Activo]
-		,[NombreCertificacion]
-		,[CasaCertificadora]
-		,[NoDeCertificado]
-		,[FechaEmisionCertificado]
-		,[FechaVigenciaInicio]
-		,[FechaVigenciaTermino]
-		,[MetodosProcesosCertificados]
-		,Carpeta
-		,Mime
-		,Extension
-		,Identificador
+			IdProveedor,
+			IdTipoDocSG,
+			Documento,
+			Activo,
+			NombreCertificacion,
+			CasaCertificadora,
+			NoDeCertificado,
+			FechaEmisionCertificado,
+			FechaVigenciaInicio,
+			FechaVigenciaTermino,
+			MetodosProcesosCertificados,
+			Carpeta,
+			Mime,
+			Extension,
+			Identificador,
+			Bucket
          )
          VALUES
          (
-		 @IdProveedor,
-		 @IdTipoDocSG,
-		 @Documento,
-		 1,
-		 @NombreCertificacion,
-		 @CasaCerficadora,            
-		 @NoDeCertificado,           
-		 @FechaEmisionCertificado,    
-		 @FechaVigenciaInicio,         
-		 @FechaVigenciaTermino,        
-		 @MetodosProcesosCertificados,
-		 @Carpeta,
-		 @Mime,
-		 @Extension,
-		 @Identificador
+			 @IdProveedor,
+			 @IdTipoDocSG,
+			 @Documento,
+			 1,
+			 @NombreCertificacion,
+			 @CasaCerficadora,            
+			 @NoDeCertificado,           
+			 @FechaEmisionCertificado,    
+			 @FechaVigenciaInicio,         
+			 @FechaVigenciaTermino,        
+			 @MetodosProcesosCertificados,
+			 @Carpeta,
+			 @Mime,
+			 @Extension,
+			 @Identificador,
+			 @Bucket
          )
 		 IF @@ERROR <> 0
              SELECT 'false' AS msj;
              ELSE
          SELECT 'true' AS msj;
 END
-
