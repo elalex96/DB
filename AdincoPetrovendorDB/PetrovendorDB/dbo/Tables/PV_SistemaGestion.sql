5454759cc666861b120146385581066d2d619e20
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2021-07-28 20:03:06 -0500

    Se corrige tipo de datos

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Tables/PV_SistemaGestion.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Tables/PV_SistemaGestion.sql
@@ -16,7 +16,7 @@
     [Extension]                   NVARCHAR (300) NULL,
     [Mime]                        NVARCHAR (300) NULL,
     [AMS3]                        BIT            NULL,
-    [Bucket] NVARCHAR(10) NULL, 
+    [Bucket] NVARCHAR(MAX) NULL, 
     CONSTRAINT [PK_PV_SistemaGestion] PRIMARY KEY CLUSTERED ([IdSistemaGestion] ASC) WITH (STATISTICS_NORECOMPUTE = ON)
 );
 
