df0c35a4166df32ee84473c1d33235744718e06c
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-08-10 18:02:55 -0500

    Update SP_MM_Aceptacion_ConsultaPedidoDetalle_MV1_5.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_Aceptacion_ConsultaPedidoDetalle_MV1_5.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_Aceptacion_ConsultaPedidoDetalle_MV1_5.sql
@@ -1,33 +1,26 @@
-﻿    
--- =============================================
--- Author:             Daniel AC
--- Create date: 20-12-17
--- Description: CONSULTA materiales para Aceptación de Pedido
--- =============================================
--- Author:             Jose Roman
--- Create date: 15-10-18
--- Description: CONSULTA materiales para Aceptación de Pedido
--- =============================================
+USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_MM_Aceptacion_ConsultaPedidoDetalle_MV1_5]    Script Date: 10/08/2021 09:48:31 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+    
 -- =============================================
 -- Author:           Daniel AC
--- Create date: 02-10-2019
--- Description: Add Marca, Modelo, No Parte a Descripción material, Add Numero de Partida
+-- Create date: 05-01-2021
+-- Description: Se agrego información del yacimiento, en la columna de observación
 -- =============================================
--- =============================================
--- Author:           Daniel AC
--- Create date: 02-10-2019
--- Description: Se agrego validación para descartar las aceptaciones eliminadas y no contarlas en los inficadores de aceptado y restante
--- =============================================
-CREATE PROCEDURE [dbo].[SP_MM_Aceptacion_ConsultaPedidoDetalle_MV1_5]  
+ALTER PROCEDURE [dbo].[SP_MM_Aceptacion_ConsultaPedidoDetalle_MV1_5] --19607,516
 -- Add the parameters for the stored procedure here
 @IdPedido      INT, 
 @IdProveedor   INT,
-@IdContrato    INT, 
-@IdUsuario     INT, 
-@FechaRegistro DATETIME
+@IdContrato    INT = NULL, 
+@IdUsuario     INT = NULL, 
+@FechaRegistro DATETIME = NULL
 
 AS
-     BEGIN
+BEGIN
 
 	 	SELECT PD.IdPedidoDetalle, 
                 PD.IdMaterialVendedor, 
@@ -37,30 +30,42 @@ AS
                         ' Modelo: ', CASE WHEN ISNULL(LEN(M.Modelo),0)>0 THEN M.Modelo ELSE ' S/M' END,
                         ' No. Parte: ',CASE WHEN ISNULL(LEN(M.NumeroParte),0)>0 THEN M.NumeroParte  ELSE ' S/NP' END) AS MaterialSolicitadoCorto, 
                 M.DescripcionLarga AS MaterialSolicitadoLarga, 
-                CONCAT('Instalación: ', INS.NombreInstalacion COLLATE DATABASE_DEFAULT,
+                CONCAT('Instalación: ', INS.NombreInstalacion COLLATE DATABASE_DEFAULT,' / Yacimiento: ', ISNULL(Y.NombreYacimiento COLLATE DATABASE_DEFAULT,''),
 					' / Requisitor: ', US.Nombre, ' / Comentarios: ', spd.observaciones ) AS observaciones, 
                  CONCAT('P.U. $', POD.PrecioUnitario, ' Cantidad: ', pd.Cantidad, ' ' , POD.MaterialCotizadoTextoC ) AS DescripcionCortaCotizado,
 				 --ISNULL(PD.Cantidad,0) AS CantidadEnPedido,
 				 CAST( SUM(ISNULL(APD.Cantidad,0)) AS NVARCHAR(100) ) AS CantidadAceptada,
 				 CAST( (ISNULL(PD.Cantidad,0) - SUM(ISNULL(APD.Cantidad,0))) AS NVARCHAR(100) ) AS CantidadRestante,
 				 ROW_NUMBER() OVER (ORDER BY PD.IdPedidoDetalle ASC) AS Partida
-         FROM dbo.MM_Pedido AS P
-              INNER JOIN dbo.MM_PedidoDetalle AS pd ON pd.IdPedido = P.IdPedido
-              INNER JOIN dbo.MM_PeticionOfertaDetalle AS POD ON PD.IdPeticionOfertaDetalle = POD.IdPeticionOfertaDetalle
-              INNER JOIN dbo.MM_SolicitudPedidoDetalle AS spd ON spd.IdSolicitudPedidoDetalle = POD.IdSolicitudPedidoDetalle
-              LEFT JOIN dbo.MM_SolicitudPedidoDetalleLineaPresupuesto SPLP ON SPLP.IdSolicitudPedidoDetalle = spd.IdSolicitudPedidoDetalle
-              LEFT JOIN Adinco.dbo.CO_Instalacion INS ON INS.IdInstalacion = SPLP.IdInstalacion
-              INNER JOIN dbo.MM_Material m ON m.IdMaterial = spd.IdMaterial
-              LEFT JOIN dbo.MM_SolicitudPedido SOL ON SOL.IdSolicitudPedido = P.IdSolicitudPedido
-              LEFT JOIN dbo.S_Usuario US ON US.IdUsuario = SOL.IdUsuarioSolicitante
-			  LEFT JOIN dbo.MM_AceptacionPedido AP ON AP.IdPedido=P.IdPedido AND AP.IdEstatusEliminado IS NULL --> QUE LA ACEPTACIÓN NO ESTE ELIMINADA
-			  LEFT JOIN dbo.MM_AceptacionPedidoDetalle APD ON PD.IdPedidoDetalle = APD.IdPedidoDetalle 
+         FROM dbo.MM_Pedido AS P WITH (NOLOCK)
+              JOIN dbo.MM_PedidoDetalle AS pd WITH (NOLOCK) 
+			  ON P.IdPedido = pd.IdPedido 
+			  AND PD.RecepcionPedido = 1 
+              JOIN dbo.MM_PeticionOfertaDetalle AS POD WITH (NOLOCK) 
+			  ON PD.IdPeticionOfertaDetalle = POD.IdPeticionOfertaDetalle
+              JOIN dbo.MM_SolicitudPedidoDetalle AS spd WITH (NOLOCK) 
+			  ON POD.IdSolicitudPedidoDetalle = spd.IdSolicitudPedidoDetalle
+              JOIN dbo.MM_SolicitudPedidoDetalleLineaPresupuesto SPLP WITH (NOLOCK) 
+			  ON spd.IdSolicitudPedidoDetalle = SPLP.IdSolicitudPedidoDetalle
+              JOIN Adinco.dbo.CO_Instalacion INS WITH (NOLOCK) 
+			  ON SPLP.IdInstalacion = INS.IdInstalacion
+              JOIN dbo.MM_Material m WITH (NOLOCK) 
+			  ON spd.IdMaterial = m.IdMaterial
+              JOIN dbo.MM_SolicitudPedido SOL WITH (NOLOCK) 
+			  ON P.IdSolicitudPedido = SOL.IdSolicitudPedido
+			  LEFT JOIN Adinco..CO_Yacimiento Y WITH (NOLOCK)
+			  ON INS.IdYacimiento=Y.IdYacimiento
+              LEFT JOIN dbo.S_Usuario US WITH (NOLOCK) 
+			  ON SOL.IdUsuarioSolicitante = US.IdUsuario
+			  INNER JOIN dbo.MM_AceptacionPedido AP
+			  ON AP.IdPedido=P.IdPedido AND ISNULL(AP.IdEstatusEliminado,0) <> 1 --> QUE LA ACEPTACIÓN NO ESTE ELIMINADA
+			  LEFT JOIN dbo.MM_AceptacionPedidoDetalle APD 
+			  ON PD.IdPedidoDetalle = APD.IdPedidoDetalle 
 			  AND AP.IdAceptacionPedido=APD.IdAceptacionPedido
          WHERE P.IdPedido = @IdPedido
                AND P.IdProveedorCompras = @IdProveedor
                AND P.RecepcionServicio = 1
-               AND PD.RecepcionPedido = 1
-			   GROUP BY 
+		GROUP BY 
 			   spd.idmaterial, 
 			   m.DescripcionCorta,
                INS.NombreInstalacion,
@@ -74,8 +79,6 @@ AS
 			   m.DescripcionLarga,
 			   M.Marca,
 			   M.Modelo,
-			   M.NumeroParte
-     END;
-
-
-
+			   M.NumeroParte,
+			   Y.NombreYacimiento
+END;
