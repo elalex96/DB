9879a415384f9ee7116d4acca40027ed5960996c
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-08-18 14:39:39 -0500

    SP PARA NUEVO FORMATO DE IMPORTACION DE ENTREGABLES

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_ENT_ImportacionEntregables_INS_ADINCO.sql
@@ -0,0 +1,241 @@
+﻿USE [Adinco]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_ENT_ImportacionEntregables_INS_ADINCO]    Script Date: 18/08/2021 01:11:58 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================  
+-- Author:  <Alexander Gomez>  
+-- Create date: <06/12/2019>  
+-- Description: <Actualizacion de los registros existentes>  
+-- =============================================  
+CREATE PROCEDURE [dbo].[SP_ENT_ImportacionEntregables_INS_ADINCO] 
+@Layout dbo.Entregables_Importacion_01 READONLY,
+@IdContrato INT,
+@IdUsuario INT
+AS
+BEGIN
+
+	SELECT
+		ROW_NUMBER() OVER (ORDER BY IdEntregable DESC) AS R,
+		*
+	INTO #TB_EXCEL
+	FROM @Layout;
+
+    DECLARE @CONT INT = 1;
+	DECLARE @CONTTOTAL INT = (SELECT COUNT(R) FROM #TB_EXCEL);
+	DECLARE @IDAREA INT;
+	DECLARE @AREA NVARCHAR(500);
+	DECLARE @USUARIOELABORADOR NVARCHAR(500);
+	DECLARE @IDUSUARIOELABORADOR INT;
+	DECLARE @IDACTIVIDADACTUAL INT;
+	DECLARE @USUARIOREVISOR NVARCHAR(500);
+	DECLARE @IDUSUARIOREVISOR INT;
+	DECLARE @USUARIOAPROBADOR NVARCHAR(500);
+	DECLARE @IDUSUARIOAPROBADOR INT;
+	DECLARE @ACTIVO BIT;
+	DECLARE @ERRORES NVARCHAR(MAX) = '';
+	DECLARE @IDENTREGABLE INT;
+	DECLARE @CONTADORAFECTADOS INT = 0;
+	DECLARE @CONTADORERRORES INT = 0;
+	DECLARE @DIASALERTAPREVIO INT;
+	DECLARE @DIASELABORACION INT;
+	DECLARE @DIASREVISION INT;
+	DECLARE @DIASAPROBACION INT;
+
+
+	WHILE @CONT <= @CONTTOTAL
+	BEGIN
+		
+		SET @IDENTREGABLE = (SELECT TOP 1 IdEntregable FROM #TB_EXCEL WHERE R = @CONT);
+		--BUSQUEDA DE AREA SELECCIONADA
+		SET @AREA = (SELECT TOP 1 Area FROM #TB_EXCEL WHERE R = @CONT);
+		SET @IDAREA = (SELECT TOP 1 idArea FROM dbo.EN_Area WHERE NombreArea = @AREA AND idContrato = @IdContrato AND Activo = 1);
+
+		--BUSQUEDA DE USUARIO ELABORADOR SELECCIONADO
+		SET @USUARIOELABORADOR = (SELECT TOP 1 Elaborador FROM #TB_EXCEL WHERE R = @CONT);
+		SET @IDUSUARIOELABORADOR = (SELECT TOP 1 UsuarioID FROM AP_Usuario WHERE Nombre = @USUARIOELABORADOR);
+
+		--BUSQUEDA DE USUARIO REVISOR SELECCIONADO
+		SET @USUARIOREVISOR = (SELECT TOP 1 Revisor FROM #TB_EXCEL WHERE R = @CONT);
+		SET @IDUSUARIOREVISOR = (SELECT TOP 1 UsuarioID FROM AP_Usuario WHERE Nombre = @USUARIOREVISOR);
+
+		--BUSQUEDA DE USUARIO APROBADOR SELECCIONADO
+		SET @USUARIOAPROBADOR = (SELECT TOP 1 Aprobador FROM #TB_EXCEL WHERE R = @CONT);
+		SET @IDUSUARIOAPROBADOR = (SELECT TOP 1 UsuarioID FROM AP_Usuario WHERE Nombre = @USUARIOAPROBADOR);
+
+		--ASIGNACION DE ACTIVO
+		SELECT
+			@ACTIVO = CASE
+						WHEN Activo = 'SI' THEN 1
+						WHEN Activo = 'NO' THEN 0
+					END,
+			@DIASALERTAPREVIO = DiasAlertaPrevia,
+			@DIASELABORACION = DiasElaboracion,
+			@DIASAPROBACION = DiasAprobacion,
+			@DIASREVISION = DiasRevicion
+		FROM #TB_EXCEL
+		WHERE R = @CONT;
+
+		--VALIDACION DE DATOS SELECCIONADOS
+		IF ISNULL(@IDAREA,0) > 0 
+			AND ISNULL(@IDUSUARIOELABORADOR,0) > 0 
+			AND ISNULL(@IDUSUARIOREVISOR,0) > 0 
+			AND ISNULL(@IDUSUARIOAPROBADOR,0) > 0 
+			AND @ACTIVO IS NOT NULL 
+			AND ISNULL(@IDENTREGABLE,0) > 0
+			AND ISNULL(@DIASALERTAPREVIO,0) > 0
+			AND ISNULL(@DIASAPROBACION,0) > 0
+			AND ISNULL(@DIASELABORACION,0) > 0
+			AND ISNULL(@DIASREVISION,0) > 0
+		BEGIN 
+
+			--GUARDADO DE LOS DATOS
+			UPDATE CE
+			SET CE.IdArea = @IDAREA,
+				CE.DiasAlerta = TE.DiasAlertaPrevia,
+				CE.DiasElaboracion = TE.DiasElaboracion,
+				CE.DiasRevision = TE.DiasRevicion,
+				CE.DiasAprobacion = TE.DiasAprobacion,
+				CE.Activo = ISNULL(@ACTIVO,0),
+				CE.ReceptorAlerta = TE.ReceptorAlerta,
+				CE.ModificadoEl = GETDATE(),
+				CE.ModificadoPor = @IdUsuario
+			FROM dbo.EN_ContratoEntregable AS CE
+			JOIN #TB_EXCEL AS TE ON CE.IdContratoEntregable = TE.IdEntregable
+			WHERE TE.R = @CONT AND CE.IdContrato = @IdContrato;
+
+			--BUSQUEDA EL APROBADOR
+			SET @IDACTIVIDADACTUAL = (SELECT TOP 1 ActividadID FROM dbo.EN_Actividad WHERE IdContratoEntregable = @IDENTREGABLE AND Activo = 1 AND EstadoID = 10002 ORDER BY CreadoEn ASC);
+			--ACTUALIZACION DEL APROBADOR
+			IF @IDACTIVIDADACTUAL IS NOT NULL
+			BEGIN
+				UPDATE ACAPROB
+				SET ACAPROB.idUsuario = @IDUSUARIOAPROBADOR,
+					ACAPROB.ModificadoEn = GETDATE(),
+					ACAPROB.ModificadoPor = @IdUsuario
+				FROM dbo.EN_ContratoEntregable AS CE
+				JOIN #TB_EXCEL AS TE ON CE.IdContratoEntregable = TE.IdEntregable
+				LEFT JOIN EN_Actividad AS ACAPROB ON CE.IdContratoEntregable = ACAPROB.IdContratoEntregable AND ACAPROB.EstadoID = 10002
+				WHERE TE.R = @CONT AND CE.IdContrato = @IdContrato;
+			END
+			ELSE
+			BEGIN
+				INSERT INTO EN_Actividad (EstadoID,idUsuario,IdContratoEntregable,CreadoPor,CreadoEn,Activo)
+				VALUES
+				(10002,@IDUSUARIOAPROBADOR,@IDENTREGABLE,@IdUsuario,GETDATE(),1);
+			END;
+
+			--BUSQUEDA DEL REVISOR
+			SET @IDACTIVIDADACTUAL = (SELECT TOP 1 ActividadID FROM dbo.EN_Actividad WHERE IdContratoEntregable = @IDENTREGABLE AND Activo = 1 AND EstadoID = 10001 ORDER BY CreadoEn ASC);
+			--ACTUALIZACION DEL REVISOR
+			IF @IDACTIVIDADACTUAL IS NOT NULL
+			BEGIN
+				UPDATE ACAPROB
+				SET ACAPROB.idUsuario = @IDUSUARIOREVISOR,
+					ACAPROB.ModificadoEn = GETDATE(),
+					ACAPROB.ModificadoPor = @IdUsuario
+				FROM dbo.EN_ContratoEntregable AS CE
+				JOIN #TB_EXCEL AS TE ON CE.IdContratoEntregable = TE.IdEntregable
+				LEFT JOIN EN_Actividad AS ACAPROB ON CE.IdContratoEntregable = ACAPROB.IdContratoEntregable AND ACAPROB.EstadoID = 10001
+				WHERE TE.R = @CONT AND CE.IdContrato = @IdContrato;
+			END
+			ELSE
+			BEGIN
+				INSERT INTO EN_Actividad (EstadoID,idUsuario,IdContratoEntregable,CreadoPor,CreadoEn,Activo)
+				VALUES
+				(10001,@IDUSUARIOREVISOR,@IDENTREGABLE,@IdUsuario,GETDATE(),1);
+			END;
+
+			--BUSQUEDA DEL ELABORADOR
+			SET @IDACTIVIDADACTUAL = (SELECT TOP 1 ActividadID FROM dbo.EN_Actividad WHERE IdContratoEntregable = @IDENTREGABLE AND Activo = 1 AND EstadoID = 10000 ORDER BY CreadoEn ASC);
+			--ACTUALIZACION DEL ELABORADOR
+			IF @IDACTIVIDADACTUAL IS NOT NULL
+			BEGIN
+				UPDATE ACAPROB
+				SET ACAPROB.idUsuario = @IDUSUARIOELABORADOR,
+					ACAPROB.ModificadoEn = GETDATE(),
+					ACAPROB.ModificadoPor = @IdUsuario
+				FROM dbo.EN_ContratoEntregable AS CE
+				JOIN #TB_EXCEL AS TE ON CE.IdContratoEntregable = TE.IdEntregable
+				LEFT JOIN EN_Actividad AS ACAPROB ON CE.IdContratoEntregable = ACAPROB.IdContratoEntregable AND ACAPROB.EstadoID = 10000
+				WHERE TE.R = @CONT AND CE.IdContrato = @IdContrato;
+			END
+			ELSE
+			BEGIN
+				INSERT INTO EN_Actividad (EstadoID,idUsuario,IdContratoEntregable,CreadoPor,CreadoEn,Activo)
+				VALUES
+				(10000,@IDUSUARIOAPROBADOR,@IDENTREGABLE,@IdUsuario,GETDATE(),1);
+			END;
+
+			--GUARDADO EXITOSO AGREGADO AL CONTADOR
+			SET @CONTADORAFECTADOS = @CONTADORAFECTADOS + 1;
+
+		END
+		ELSE
+		BEGIN
+			--VALIDACION DE DATOS ERRONES Y AGREGADO DE TEXTO DESCRIPTIVO DEL ERROR
+			IF ISNULL(@IDENTREGABLE,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>Se detecto que en la fila <strong>#' + CAST((@CONT + 1) AS NVARCHAR) + '</strong> no se señalo el entregable a editar. </li>'; 
+			END
+
+			IF ISNULL(@IDAREA,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> el Area seleccionada no es aceptable.</li>';
+			END
+
+			IF ISNULL(@IDUSUARIOREVISOR,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> el Revisor seleccionado no es aceptable.</li>';
+			END
+
+			IF ISNULL(@IDUSUARIOAPROBADOR,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> el Aprobador seleccionado no es aceptable.</li>';
+			END
+
+			IF ISNULL(@IDUSUARIOELABORADOR,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> el Elaborador seleccionado no es aceptable.</li>';
+			END
+
+			IF @ACTIVO IS NULL
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> el valor de Activo seleccionado no es aceptable.</li>'
+			END
+
+			IF ISNULL(@DIASALERTAPREVIO,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> los dias de alerta previa deben ser mayor a 0.</li>';
+			END
+
+			IF ISNULL(@DIASAPROBACION,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> los dias de aprobación deben ser mayor a 0.</li>';
+			END
+
+			IF ISNULL(@DIASELABORACION,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> los dias de elaboracion deben ser mayor a 0.</li>';
+			END
+
+			IF ISNULL(@DIASREVISION,0) = 0
+			BEGIN
+				SET @ERRORES = @ERRORES + '<li>En el Entregable <strong>#'+ CAST(@IDENTREGABLE AS nvarchar) + '</strong> los dias de revisión deben ser mayor a 0.</li>';
+			END
+
+			--ERROR AGREGADO AL CONTADOR
+			SET @CONTADORERRORES = @CONTADORERRORES + 1;
+		END
+
+		SET @CONT = @CONT + 1;
+
+	END
+
+	SELECT @CONTADORERRORES AS ERRORES,
+			@CONTADORAFECTADOS AS AFECTADOS,
+			@ERRORES AS TEXTOERRORES
+
+END
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/User Defined Types/Entregables_Importacion_01.sql
@@ -0,0 +1,20 @@
+﻿USE [Adinco]
+GO
+
+/****** Object:  UserDefinedTableType [dbo].[Entregables_Importacion_01]    Script Date: 18/08/2021 01:39:31 p. m. ******/
+CREATE TYPE [dbo].[Entregables_Importacion_01] AS TABLE(
+	[IdEntregable] [nvarchar](50) NULL,
+	[Area] [nvarchar](500) NULL,
+	[DiasAlertaPrevia] [nvarchar](50) NULL,
+	[DiasElaboracion] [nvarchar](50) NULL,
+	[DiasRevicion] [nvarchar](50) NULL,
+	[DiasAprobacion] [nvarchar](50) NULL,
+	[Activo] [nvarchar](50) NULL,
+	[Elaborador] [nvarchar](500) NULL,
+	[Revisor] [nvarchar](500) NULL,
+	[Aprobador] [nvarchar](500) NULL,
+	[ReceptorAlerta] [nvarchar](500) NULL
+)
+GO
+
+
