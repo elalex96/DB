de9f4bab4cd42ea15922caead32dd6d6c3800ae5
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2021-09-07 15:29:23 -0500

    Se agrega la columna Bucket

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/p_NC_ConsultaDocAnexosNotaCredito.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/p_NC_ConsultaDocAnexosNotaCredito.sql
@@ -1,7 +1,14 @@
-﻿-- =============================================
+﻿DROP PROCEDURE IF EXISTS p_NC_ConsultaDocAnexosNotaCredito
+go
+-- =============================================
 -- Author:	Luis David De La Cruz 
 -- Create date: 11/11/19
 -- Description:	<Consulta para los documentos de soporte de recepcion de nota de credito.>
+-- =============================================
+-- =============================================
+-- Author:	Luis David De La Cruz 
+-- Create date: 07/09/2021
+-- Description:	Se agrega el bucket en la descarga para la estandarización de descarga amazon s3
 -- =============================================
 CREATE PROCEDURE p_NC_ConsultaDocAnexosNotaCredito
     @IdNotaCredito INT,
@@ -22,7 +29,6 @@ BEGIN
 		AND NC.IdAceptacionNotaCredito=@IdNotaCredito
 
     END;
-
     IF @Accion = 'DESCARGAR'
     BEGIN
         SELECT  D.NombreDocumento,
@@ -30,7 +36,8 @@ BEGIN
 				D.Mime,				             
 				D.Carpeta,
 				D.Identificador,
-				D.IdDocumento
+				D.IdDocumento,
+				ISNULL(D.Bucket,'') as Bucket
         FROM dbo.S_Documento_S3 D
             LEFT JOIN dbo.MPY_MM_AceptacionNotaCredito NC
                 ON NC.IdAceptacionNotaCredito = D.IdDocumentoTabla
@@ -38,5 +45,4 @@ BEGIN
 		AND NC.IdAceptacionNotaCredito=@IdNotaCredito
 		AND D.IdDocumento=@IdDocumento
     END;
-
 END;
