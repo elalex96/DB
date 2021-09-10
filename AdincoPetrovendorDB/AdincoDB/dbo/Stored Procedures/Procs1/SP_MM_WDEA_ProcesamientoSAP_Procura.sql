8e60d62d0b4c6fc46c1a6e012105a88eb249d66f
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-09-10 10:49:36 -0500

    correccion

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SP_MM_WDEA_ProcesamientoSAP_Procura.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SP_MM_WDEA_ProcesamientoSAP_Procura.sql
@@ -52,6 +52,10 @@ BEGIN
 															@CONTRATO,
 															@IDBITACORA;
 
+		UPDATE PendientesProcesarProcura_WSDEA
+		SET Procesado = 1,
+		WHERE IdBitacora = @IDBITACORA;
+
 		SET @CONT = @CONT + 1;
 
 	END
