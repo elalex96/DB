83f96aecb023b94c4a74c83f62345ba8b43d57fb
Manuel Cruz <manuel.cruz@adinco.mx>
2021-09-24 15:45:11 -0500

    Estandar avance 5 descarga archivos S3

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DEA_GetDocumentoCNN.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DEA_GetDocumentoCNN.sql
@@ -1,10 +1,19 @@
-﻿
+﻿USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_DEA_GetDocumentoCNN]    Script Date: 24/09/2021 02:50:06 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 -- =============================================
 -- Author:		DANIEL Cruz
 -- Create date: 08-02-18
 -- Description:	Consultar detalle de encabezado de aprobación de carta de contenido nacional en procura, se agrego valicación de enviar siempre y cuando se solicito en la aceptación
+-- Author:		<Manuel Cruz>
+-- Create date: <23/09/20219>
+-- Description:	<Se agrega columna Bucket para que devuelva el select descarga estandar avance 5>
 -- =============================================
-CREATE PROCEDURE [dbo].[SP_DEA_GetDocumentoCNN]
+ALTER PROCEDURE [dbo].[SP_DEA_GetDocumentoCNN]
 	-- Add the parameters for the stored procedure here
 @IdProveedor INT,
 @IdAceptacionPedido INT,
@@ -17,21 +26,20 @@ AS
          SET NOCOUNT ON;
 
     -- Insert statements for procedure here
- 
-
 
         SELECT CONCAT('Carta contenido nacional ',PG.IdPedido,'_',AC.IdAceptacionPedido,'.pdf') AS Documento,--0
 			D.Extension, 
 			d.Mime,
 			D.Carpeta,
 			D.Identificador,
-			 AC.IdAceptacionCartaPCN,
-			 Ac.IdAceptacionPedido, 
-			 AP.IdPedido, 
-			 AC.IdDocumento,
+			AC.IdAceptacionCartaPCN,
+			AC.IdAceptacionPedido, 
+			AP.IdPedido, 
+			AC.IdDocumento,
 			TD.TipoValidacion, 
 			TD.IdTipoValidacionDoc, 		
-			TP.IdTipoPedido			
+			TP.IdTipoPedido,
+			D.Bucket
 		FROM [dbo].[MM_AceptacionCartaPCN] AS AC
 		INNER JOIN [dbo].[S_Documento_S3] AS D ON D.IdDocumento = AC.IdDocumento
 		INNER JOIN [dbo].[MM_AceptacionPedido] AS AP ON AP.IdAceptacionPedido = AC.IdAceptacionPedido
@@ -40,14 +48,12 @@ AS
 		INNER JOIN [dbo].[S_TipoValidacionDoc] AS TD ON TD.IdTipoValidacionDoc = AC.IdEstatus
 		INNER JOIN MM_Pedidos AS PG ON P.IdPedido = PG.IdIdentificador AND PG.IdProveedorCliente = @IdProveedor
 		LEFT JOIN [dbo].[S_Usuario] as U ON U.IdUsuario = AC.IdUsuarioEvaluador
-		LEFT  JOIN dbo.MM_TipoPedido AS TP ON TP.IdTipoPedido = PG.IdTipoPedido
+		LEFT JOIN dbo.MM_TipoPedido AS TP ON TP.IdTipoPedido = PG.IdTipoPedido
 		LEFT JOIN dbo.RelacionCartaCNPedido RCN ON RCN.IdAceptacionPedido = AP.IdAceptacionPedido
 		WHERE  
 		AC.IdEstatus= 2  --> QUE ESTE APROBADA 
 		AND P.IdProveedorCompras = @IdProveedor
-		AND  AC.IdAceptacionPedido =@IdAceptacionPedido---AND IdAceptacionCartaPCN=@IdAceptacionCartaPCN
+		AND AC.IdAceptacionPedido = @IdAceptacionPedido---AND IdAceptacionCartaPCN=@IdAceptacionCartaPCN
 		AND RCN.PedirCarta=1 ---> QUE SE SOLICTO CARTA
 
      END;
-
-	  
--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DEA_GetDocumentoPRExterno.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DEA_GetDocumentoPRExterno.sql
@@ -1,10 +1,19 @@
-﻿-- =============================================
+﻿USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_DEA_GetDocumentoPRExterno]    Script Date: 24/09/2021 04:38:04 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		Daniel AC
 -- Create date: <18-09-18>
 -- Description:	<obtiene la informacion del documento del factura ya sea xml o pdf>
+-- Author:		<Manuel Cruz>
+-- Create date: <23/09/20219>
+-- Description:	<Se agrega columna Bucket para que devuelva el select descarga estandar avance 5>
 -- =============================================
-CREATE  PROCEDURE [dbo].[SP_DEA_GetDocumentoPRExterno]
-
+ALTER  PROCEDURE [dbo].[SP_DEA_GetDocumentoPRExterno]
 @IdProveedor INT,
 @IdUsuario INT,
 @IdDocumento INT,
@@ -15,14 +24,10 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-	SELECT D.NombreDocumento,D.Extension,D.Mime,D.Carpeta,D.Identificador, PR.IdSolicitudPedido,PR.ID_PR
+	SELECT D.NombreDocumento,D.Extension,D.Mime,UPPER(D.Carpeta),D.Identificador,PR.IdSolicitudPedido,PR.ID_PR,D.Bucket
 	FROM dbo.DEA_Documento_S3 D
- 	LEFT JOIN dbo.DEA_AdjuntoPR PR
-		ON PR.IdAjuntoPr = PR.IdAjuntoPr
-	WHERE PR.IdSolicitudPedido=@IdSolicitudPedido	
-	AND D.Activo=1
-
+ 	LEFT JOIN dbo.DEA_AdjuntoPR PR ON PR.IdAjuntoPr = PR.IdAjuntoPr
+	WHERE PR.IdSolicitudPedido = @IdSolicitudPedido	
+	AND D.Activo = 1
+	ORDER BY D.CreadoEl DESC
 END
-
-
-
