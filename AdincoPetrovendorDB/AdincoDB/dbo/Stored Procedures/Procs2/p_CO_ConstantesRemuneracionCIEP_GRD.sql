612160ceb463545db04f142b38763ba3d9450257
Daniel Moreno <daniel.moreno@adinco.mx>
2021-09-30 13:36:37 -0500

    isssue 1545 scripts

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/p_CO_ConstantesRemuneracionCIEP_GRD.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROC p_CO_ConstantesRemuneracionCIEP_GRD
+@IdContrato int
+as
+
+	SELECT	
+		IdConstantesRemuneracionCIEP,
+		IdContrato,
+		Anio,
+		ConstanteST240kbls,
+		IPPj,
+		IPP0,
+		Tarifa,
+		TasaDescuento,
+		ConstanteST,
+		CreadoPor,
+		CreadoEn,
+		ModificadoPor,
+		ModificadoEn
+	FROM CO_ConstantesRemuneracionCIEP
+	WHERE IdContrato = @IdContrato
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/p_CO_ConstantesRemuneracionCIEP_UPD.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROC p_CO_ConstantesRemuneracionCIEP_UPD
+@IdConstantesRemuneracionCIEP	int,
+@IdContrato	int,
+@Anio	int,
+@ConstanteST240kbls	float,
+@IPPj	float,
+@IPP0	float,
+@Tarifa	float,
+@TasaDescuento	float,
+@ConstanteST	float,
+@ModificadoPor	int
+AS
+
+
+	UPDATE CO_ConstantesRemuneracionCIEP
+		SET Anio=@Anio,
+		ConstanteST240kbls=@ConstanteST240kbls,
+		IPPj=@IPPj,
+		IPP0=@IPP0,
+		Tarifa=@Tarifa,
+		TasaDescuento=@TasaDescuento,
+		ConstanteST=@ConstanteST,
+		ModificadoPor=@ModificadoPor,
+		ModificadoEn=GETDATE()
+	WHERE IdConstantesRemuneracionCIEP = @IdConstantesRemuneracionCIEP
+
