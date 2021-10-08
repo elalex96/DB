86ed02a353367d3453f19c5f81ac49ce6dfda5fa
RamonPortales <ramon.portales@adinco.mx>
2021-10-08 11:59:38 -0500

    Se concluyó el requerimiento

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Tables/BitacoraReversaComprobantesExtranjeros.sql
@@ -0,0 +1,8 @@
+﻿create table BitacoraReversaComprobantesExtranjeros
+	(
+		IdBitacoraReversaComprobanteExtranjero	int,
+		Motivo									varchar(max),
+		Fecha									datetime,
+		IdOperacion								int
+		constraint								PK_BitacoraReversaComprobantesExtranjeros	primary key(IdBitacoraReversaComprobanteExtranjero)
+	)
