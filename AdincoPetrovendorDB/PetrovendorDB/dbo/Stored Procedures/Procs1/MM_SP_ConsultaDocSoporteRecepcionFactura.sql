2fbec0290d1940afe8a569a2952f4bade15b603a
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-10-12 14:15:42 -0500

    Update MM_SP_ConsultaDocSoporteRecepcionFactura.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/MM_SP_ConsultaDocSoporteRecepcionFactura.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/MM_SP_ConsultaDocSoporteRecepcionFactura.sql
@@ -1,11 +1,18 @@
-﻿
+USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[MM_SP_ConsultaDocSoporteRecepcionFactura]    Script Date: 12/10/2021 12:18:18 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
 -- =============================================
 -- Author:		<Jose Roman>
 -- Create date: <01-04-2018>
 -- Description:	<Consulta para los documentos de soporte de recepcion de factura.>
 -- =============================================
 
-CREATE procedure MM_SP_ConsultaDocSoporteRecepcionFactura
+CREATE procedure [dbo].[MM_SP_ConsultaDocSoporteRecepcionFactura]
 	@IdAceptacionPedido INT,
 	/*--------------------parametros contrato  --------------------*/
     @IdContrato    INT = null,
@@ -14,10 +21,15 @@ CREATE procedure MM_SP_ConsultaDocSoporteRecepcionFactura
 	/*-------------------------------------------------------------*/
 AS
 BEGIN
-	SELECT IdDocSoporteRecepcionFactura,
-		NombreDoc,
-		Comentario
-	FROM dbo.MM_DocSoporteRecepcionFactura
+	SELECT 
+		DSF.IdDocSoporteRecepcionFactura,
+		DSF.NombreDoc,
+		DSF.Comentario,
+		DSF.CargadoEl,
+		US.Nombre
+	FROM dbo.MM_DocSoporteRecepcionFactura AS DSF
+	LEFT JOIN S_Usuario AS US 
+		ON DSF.CargadoPor = US.IdUsuario
 	WHERE IdAceptacionPedido = @IdAceptacionPedido
-		AND Eliminado = 0
+		AND Eliminado = 0;
 END
