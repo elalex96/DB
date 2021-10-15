d7eceb2a176670cf03d86e90e892240803bdb344
David De La Cruz <david.cruz@adinco.mx>
2021-10-14 19:08:07 -0500

    sPS RELACIONADOS

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Functions/EN_SP_Actualiza_Frecuencias.sql
@@ -0,0 +1,17 @@
+﻿USE Adinco
+GO
+DROP PROCEDURE IF EXISTS EN_SP_Actualiza_Frecuencias
+GO
+CREATE PROCEDURE EN_SP_Actualiza_Frecuencias
+@IdFrecuenciaEntregable int,
+@FrecuenciaEntregable varchar(max),
+@FrecuenciaIngles varchar(max),
+@IdContrato int = null,
+@IdUsuario int = null
+AS
+BEGIN
+	update EN_FrecuenciaEntregable
+	set FrecuenciaEntregable = @FrecuenciaEntregable,
+	FrecuenciaIngles = @FrecuenciaIngles
+	where IdFrecuenciaEntregable = @IdFrecuenciaEntregable
+END
