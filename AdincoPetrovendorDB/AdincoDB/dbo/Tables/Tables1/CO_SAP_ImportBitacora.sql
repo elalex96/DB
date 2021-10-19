d50b8900bfba4aaeb03055285ea21e6169a484bf
Daniel Moreno <daniel.moreno@adinco.mx>
2021-10-19 13:05:02 -0500

    cambios ISSUE 1577

--- AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/CO_SAP_ImportBitacora.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/CO_SAP_ImportBitacora.sql
@@ -7,6 +7,7 @@
     [IdNotificacion] BIGINT   NULL,
     [CreadoEl]       DATETIME NOT NULL,
     [CreadoPor]      INT      NOT NULL,
+    [NotificacionEnviada] BIT NULL, 
     CONSTRAINT [PK_CO_SAP_ImportBitacora] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (STATISTICS_NORECOMPUTE = ON),
     CONSTRAINT [FK_CO_SAP_ImportBitacora_CO_Contrato] FOREIGN KEY ([IdContrato]) REFERENCES [dbo].[CO_Contrato] ([IdContrato]),
     CONSTRAINT [FK_CO_SAP_ImportBitacora_S_Notificacion] FOREIGN KEY ([IdNotificacion]) REFERENCES [dbo].[S_Notificacion] ([IdNotificacion])
