47e463ba994119c512dfca7192444d7f3cb4ed09
Daniel Moreno <daniel.moreno@adinco.mx>
2021-11-17 13:08:58 -0600

    Procedimientos almacenandos

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/p_CO_SAP_PO_DisableAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[p_CO_SAP_PO_DisableAll]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/p_CO_SAP_PO_Enable.sql
@@ -0,0 +1,13 @@
+﻿create proc p_CO_SAP_PO_Enable
+@pIdContratista int,
+@pPO varchar(20)
+as
+
+	update CO_SAPPO
+	set POActivo = 1,
+		CancaladoPor = null,
+		CancaladoEl = null
+	from CO_SAPPO po
+	inner join CO_Contrato c on c.IdContrato = po.IdContrato
+	where c.IdContratista = @pIdContratista and
+	po.SAPPONumber = @pPO
