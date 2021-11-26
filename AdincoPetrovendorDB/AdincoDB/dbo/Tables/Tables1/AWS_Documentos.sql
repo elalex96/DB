e9885d328ac191e75b684039dcf0d57cdda67438
RamonPortales <ramon.portales@adinco.mx>
2021-11-26 10:34:53 -0600

    Se modificó la tabla AWS_Documentos,  no estaba actualizada en productivo en la longitud del campo Meta

--- AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/AWS_Documentos.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/AWS_Documentos.sql
@@ -4,7 +4,7 @@
     [Folder]         VARCHAR (100)    NOT NULL,
     [UUIDAmazon]     UNIQUEIDENTIFIER NOT NULL,
     [NombreArchivo]  VARCHAR (250)    NOT NULL,
-    [Meta]           VARCHAR (1000)     NOT NULL,
+    [Meta]           VARCHAR (200)     NOT NULL,
     [CreadoPor]      INT              NOT NULL,
     [CreadoEl]       DATETIME         NOT NULL,
     [ModificadoPor]  INT              NULL,
