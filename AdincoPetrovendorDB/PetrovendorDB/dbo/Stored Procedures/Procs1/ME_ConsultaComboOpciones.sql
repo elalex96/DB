10e575960e62b3951f3d1488605bd9c83246f291
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-11-26 14:43:50 -0600

    Update ME_ConsultaComboOpciones.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/ME_ConsultaComboOpciones.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/ME_ConsultaComboOpciones.sql
@@ -1,10 +1,17 @@
-﻿
-CREATE procedure ME_ConsultaComboOpciones
+USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[ME_ConsultaComboOpciones]    Script Date: 26/11/2021 01:53:50 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER procedure [dbo].[ME_ConsultaComboOpciones]
 	@IdPregunta int 
 as
 begin
 	SELECT Respuesta, IdRespuestasOpciones
-		FROM dbo.ME_RespuestasOpciones
+		FROM dbo.ME_RespuestasOpciones (NOLOCK)
 		WHERE IdPregunta = @IdPregunta
 END
 
