ce79cf2cf3acb88fd59d58925522b9a34dc65f75
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-11-26 14:41:56 -0600

    Update SP_MM_ConsultaDatosCorreoSolicitudPedido.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_ConsultaDatosCorreoSolicitudPedido.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_ConsultaDatosCorreoSolicitudPedido.sql
@@ -1,10 +1,17 @@
-﻿/****** Object:  StoredProcedure [dbo].[SP_MM_ConsultaDatosCorreoSolicitudPedido]    Script Date: 28/09/2020 14:13:08 ******/
+USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_MM_ConsultaDatosCorreoSolicitudPedido]    Script Date: 26/11/2021 01:57:35 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  StoredProcedure [dbo].[SP_MM_ConsultaDatosCorreoSolicitudPedido]    Script Date: 28/09/2020 14:13:08 ******/
 -- =============================================
 -- Author:		<Alexander Gomez>
 -- Create date: <28/09/2020>
 -- Description:	<Consulta de datos de solicitud de pedido para evitar errores en campos vacios>
 -- =============================================
-CREATE PROCEDURE [dbo].[SP_MM_ConsultaDatosCorreoSolicitudPedido] 
+ALTER PROCEDURE [dbo].[SP_MM_ConsultaDatosCorreoSolicitudPedido] 
 	-- Add the parameters for the stored procedure here
 	@IdSolicitudPedido INT
 AS
@@ -25,12 +32,12 @@ BEGIN
 			@NumeroContrato=ISNULL(C.NumeroContrato,''),  
 			@Contrato=(CONCAT(ISNULL(C.NumeroContrato,''),' - ',ISNULL(AC.NombreAreaContractual,''))),
 			@Mensaje = ISNULL(OP.Descripcion,'')   
-	FROM Adinco.dbo.CO_Contrato C  
-	  LEFT JOIN Adinco.dbo.CO_AreaContractual AC  
+	FROM Adinco.dbo.CO_Contrato C  (NOLOCK)
+	  LEFT JOIN Adinco.dbo.CO_AreaContractual AC  (NOLOCK)
 			ON C.IdAreaContractual = AC.IdAreaContractual  
-	  LEFT JOIN Petrovendor.dbo.MM_SolicitudPedido SP 
+	  LEFT JOIN Petrovendor.dbo.MM_SolicitudPedido SP (NOLOCK)
 			ON sp.IdContrato= C.IdContrato  
-	  LEFT JOIN dbo.TA_Operacion AS OP 
+	  LEFT JOIN dbo.TA_Operacion AS OP (NOLOCK)
 			ON OP.IdDocumento = SP.IdSolicitudPedido
 			AND OP.IdTipoOperacion = 2
 			AND OP.IdProveedor = SP.IdProveedor
