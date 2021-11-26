00a1c626a8c1df23c9556f3b9999f5aebc0a8928
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-11-26 14:49:54 -0600

    Update AD_SP_ComboInstalaciones.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/AD_SP_ComboInstalaciones.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/AD_SP_ComboInstalaciones.sql
@@ -1,14 +1,22 @@
-﻿
-CREATE procedure [dbo].[AD_SP_ComboInstalaciones]
+USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[AD_SP_ComboInstalaciones]    Script Date: 26/11/2021 01:44:37 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER procedure [dbo].[AD_SP_ComboInstalaciones]
 	@IdContrato INT
 
 AS
 BEGIN
+
 	SELECT
 		i.IdInstalacion,
 		i.NombreInstalacion
-    FROM Adinco.dbo.CO_Instalacion AS i
-	INNER JOIN adinco.dbo.CO_Contrato AS c ON c.IdAreaContractual = i.IdAreaContractual
-	WHERE c.IdContrato = @IdContrato
-		AND ISNULL(i.Activo,0) = 1
+    FROM Adinco.dbo.CO_Instalacion AS i (NOLOCK)
+	 JOIN adinco.dbo.CO_Contrato AS c (NOLOCK) ON c.IdAreaContractual = i.IdAreaContractual and c.IdContrato = @IdContrato
+	WHERE ISNULL(i.Activo,0) = 1
+		
 END
