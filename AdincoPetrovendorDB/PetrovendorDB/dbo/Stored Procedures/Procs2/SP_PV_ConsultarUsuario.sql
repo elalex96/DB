66b7eac6e0c2e2cf0d3bfd9c4a1ec3a060246a3d
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2021-11-30 14:19:44 -0600

    Update SP_PV_ConsultarUsuario.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_PV_ConsultarUsuario.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_PV_ConsultarUsuario.sql
@@ -1,22 +1,57 @@
-﻿-- =============================================
+USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_PV_ConsultarUsuarios]    Script Date: 30/11/2021 02:18:20 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author: DANIEL AC
 -- Create date: 31/08/2017
 -- Description:	CONSULTAR USUARIO
+-- Author: DANIEL AC
+-- Update date: 07/03/2018
+-- Description:	Se agrego groupby 
 -- =============================================
-CREATE  PROCEDURE [dbo].[SP_PV_ConsultarUsuario]
-@IdUsuario int
-
+-- Author:		Jose Roman
+-- Update date: 25/04/2018
+-- Description:	Se agrega filtro por contrato  
+-- =============================================
+-- =============================================
+-- Author:		Pedro Acu�a
+-- Update date: 13/08/2018
+-- Description:	Se agrega el retorno de los roles separados por coma  
+-- =============================================
+-- =============================================
+-- Author:		Alexander Gomez
+-- Update date: 25/11/21
+-- Description:	se optimiza el sp para disminuir la carga de los modulos donde es usado
+-- =============================================
+ALTER PROCEDURE [dbo].[SP_PV_ConsultarUsuarios] 
+	@IdProveedor INT ,
+/*--------------------parametros contrato  --------------------*/
+	@IdContrato INT = NULL, @IdUsuario INT = NULL ,
+	@FechaRegistro DATETIME = NULL
+/*-------------------------------------------------------------*/
 AS
- 
-
 BEGIN
+		SELECT
+			U.IdUsuario,
+			U.Nombre,
+			STUFF((SELECT CAST(' ' AS VARCHAR(MAX)) + CONVERT ( NVARCHAR(MAX), UR.IdRol )
+							FROM dbo.S_UsuarioRol AS UR
+							WHERE UR.IdUsuario = U.IdUsuario
+									AND UR.Activo = 1
+							ORDER BY UR.IdRol
+							FOR XML PATH ('')),1,1, '') AS IdRol
+		FROM dbo.S_UsuarioProveedor AS UP WITH (NOLOCK)
+			JOIN dbo.S_Usuario AS U WITH (NOLOCK) ON UP.IdUsuario = U.IdUsuario 
+				AND UP.IdProveedor = @IdProveedor
+				AND U.Activo = 1
+				AND (u.IsEliminado = 0 OR u.IsEliminado IS NULL)
+				AND UP.IdContrato = @IdContrato
+			JOIN dbo.S_Proveedor AS P WITH (NOLOCK) ON UP.IdProveedor = P.IdProveedor
+		GROUP BY U.IdUsuario, U.Nombre, P.IdProveedor, UP.IdUsuario
 
-SELECT U.[IdUsuario],U.[Activo],U.[IdTipoUsuario],U.[Correo],U.[Contrasena],U.[Nombre]
-FROM S_Usuario AS U
-INNER JOIN S_USUARIOPROVEEDOR AS UP ON UP.IDUSUARIO= U.IdUsuario
-INNER JOIN S_Proveedor AS P ON P.IDPROVEEDOR = UP.IDPROVEEDOR
-WHERE U.IdUsuario = @IdUsuario
-	
+
 END
-
-
