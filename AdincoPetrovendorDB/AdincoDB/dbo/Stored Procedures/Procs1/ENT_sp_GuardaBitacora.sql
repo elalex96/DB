cdfc0ad99e609c4ba154aa5eeb4c7a39cd321536
David De La Cruz <david.cruz@adinco.mx>
2021-12-17 08:27:27 -0600

    tabla y sp guarda archivos bitacora

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/ENT_sp_GuardaBitacora.sql
@@ -0,0 +1,26 @@
+﻿DROP PROCEDURE IF EXISTS ENT_sp_GuardaBitacora
+go
+CREATE PROCEDURE ENT_sp_GuardaBitacora
+--===========================================
+--===========================================
+--Creado Por: Luis David
+--Creado El: 17/12/2021
+--===========================================
+--===========================================
+	@Ruta varchar(300),
+	@Archivo varchar(500),
+	@AWSArchivoId int,
+	@AWSIdentificador varchar(max),	
+	@UsuarioId int,
+	@IdContrato int,
+	@Accion varchar(300)
+AS
+BEGIN
+	INSERT INTO ENT_BitacoraArchivos(
+	Fecha,				Ruta,			Archivo,	
+	AWSIdentificador,	UsuarioId,		IdContrato,
+	Accion,				AWSArchivoId) VALUES
+	(GETDATE(),			@Ruta,			@Archivo,
+	@AWSIdentificador,	@UsuarioId,		@IdContrato,
+	@Accion,			@AWSArchivoId)
+END
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/ENT_BitacoraArchivos.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ENT_BitacoraArchivos]
+(
+	Id Int primary key not null identity (1,1),
+	ModuloId int,
+	Fecha datetime,
+	Ruta varchar(300),
+	Archivo varchar(500),
+	AWSArchivoId int,
+	AWSIdentificador varchar(max) NOT NULL,	
+	UsuarioId int,
+	IdContrato int,
+	Accion varchar(300),
+	FOREIGN KEY (UsuarioId) REFERENCES AP_Usuario(UsuarioId),
+	FOREIGN KEY (IdContrato) REFERENCES CO_Contrato(IdContrato)
+)
