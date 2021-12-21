a2d0765e69125aa69ec6392dec498b96264a34c4
RamonPortales <ramon.portales@adinco.mx>
2021-12-21 17:30:55 -0600

    Se agregó la tabla CaroulseImages y dos sp's para su manejo

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/spCaroulseImagesLst.sql
@@ -0,0 +1,24 @@
+﻿if exists (select * from sys.procedures where name = 'spCaroulseImagesLst')
+begin
+	drop proc spCaroulseImagesLst
+end
+
+go
+
+create proc spCaroulseImagesLst
+(
+	@Carpeta	varchar(100)
+)
+as
+begin
+	select	IdCarouselImage,
+			Carpeta,
+			Nombre,
+			Activo
+	from	CaroulseImages 
+	where	Carpeta			like	@Carpeta
+	and		Activo			=		1
+			
+end
+
+go
