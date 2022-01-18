599a89eb08ef21844fdf80ed155803b56c7a5a06
Daniel Antonio Cruz <daniel.antonio@adinco.mx>
2022-01-18 17:47:14 -0600

    Se agrego actualizacion de sps para enviar notificacion al siguiente aprobador de pedido serial

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Mobile_sp_RegistroBitacora_Aprobacio.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[Mobile_sp_RegistroBitacora_Aprobacio]
+	@IdTarea Int,
+	@IdContrato Int,
+	@IdEstatus Int,
+	@Comentario varchar(max),
+	@AprobadorPetrovendor Int,
+	@AprobadorAdinco Int,
+	@FechaAprobacion datetime
+as
+begin	
+	if not exists(select 1 from AM_BitacoraAprobaciones where IdTarea = @IdTarea)
+	begin
+		insert Into AM_BitacoraAprobaciones(IdTarea ,
+								IdContrato ,
+								IdEstatus ,
+								Comentario ,
+								AprobadorPetrovendor ,
+								AprobadorAdinco ,
+								FechaAprobacion ) values (@IdTarea,
+								@IdContrato,
+								@IdEstatus,
+								@Comentario,
+								@AprobadorPetrovendor,
+								@AprobadorAdinco,
+								@FechaAprobacion
+								)
+	end
+end
