5114c11cf8d95a28b16d40f5bc57103d922f9e53
Barbara Arrañaga <37219356+barbaraarranaga@users.noreply.github.com>
2022-01-19 18:27:30 -0600

    Update fn_ObtenSubtotalPedido.sql
    Se corrije el formato

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Functions/fn_ObtenSubtotalPedido.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Functions/fn_ObtenSubtotalPedido.sql
@@ -1,9 +1,9 @@
-﻿-- =============================================
+-- =============================================
 -- Author:		Luis David De La Cruz Bautista
 -- Create date: 29/21/2021
 -- Description:	Función para obtener el subtotal para issue 1489(Petrovendor)
 -- =============================================
-CREATE FUNCTION fn_ObtenSubtotalPedido
+ALTER FUNCTION fn_ObtenSubtotalPedido
 (
 	@Moneda int,
 	@IdPedido int ,
@@ -11,11 +11,11 @@ CREATE FUNCTION fn_ObtenSubtotalPedido
 )
 returns VARCHAR(MAX)
 begin 
-DECLARE @Subtotal varchar(max) = FORMAT(0, '#,#0.000'); --SELECT DBO.fn_ObtenSubtotalPedido(1,24339,10047) AS SUBTOTAL
+DECLARE @Subtotal varchar(max) = FORMAT(0, '###,###,###,###.000'); --SELECT DBO.fn_ObtenSubtotalPedido(1,24339,10047) AS SUBTOTAL
 	IF @Moneda = 1
 	BEGIN
 		set @Subtotal = (SELECT  
-		FORMAT(SUM(PD.Subtotal), '#,#0.000') AS SUBTOTAL
+		FORMAT(SUM(PD.Subtotal), '###,###,###,###.000') AS SUBTOTAL
 		FROM Petrovendor.dbo.MM_PedidoDetalle PD 
 		JOIN Petrovendor.dbo.MM_Pedido AS P   
 		ON PD.IdPedido = P.IdPedido  
