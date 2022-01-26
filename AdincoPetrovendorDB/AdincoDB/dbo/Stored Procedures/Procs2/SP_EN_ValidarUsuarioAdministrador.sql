945a0aaaf329d9949cc62c1c6f637eb9731ccb93
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2022-01-26 14:02:04 -0600

    Update SP_EN_ValidarUsuarioAdministrador.sql

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_EN_ValidarUsuarioAdministrador.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_EN_ValidarUsuarioAdministrador.sql
@@ -1,6 +1,6 @@
-﻿USE [Adinco]
+USE [Adinco]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_EN_ValidarUsuarioAdministrador]    Script Date: 25/01/2022 03:45:47 p. m. ******/
+/****** Object:  StoredProcedure [dbo].[SP_EN_ValidarUsuarioAdministrador]    Script Date: 26/01/2022 12:34:19 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,10 +10,11 @@ GO
 -- Create date: 25/01/2022
 -- Description:	Validar si el usuario que va reiniciar es admin
 -- =============================================
-CREATE PROCEDURE [dbo].[SP_EN_ValidarUsuarioAdministrador]
+ALTER PROCEDURE [dbo].[SP_EN_ValidarUsuarioAdministrador]
 	-- Add the parameters for the stored procedure here
 	@IdUsuario INT,
-	@IdContrato INT
+	@IdContrato INT,
+	@IdInstanciaEntregable INT
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -21,9 +22,14 @@ BEGIN
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
-	DECLARE @IsAdmin INT;
+	DECLARE @Admins INT;
+	DECLARE @Aprobadors INT;
+	DECLARE @EsAdmin BIT = 0;
+	DECLARE @EsAprobador BIT = 0;
 
-	SELECT @IsAdmin = COUNT(1)
+	--BUSQUEDA DE PERMISOS DE ADMINISTRADOR
+	SELECT 
+		@Admins = COUNT(1)
     FROM dbo.AP_PerfilUsuario PU
         JOIN dbo.AP_Perfil P
             ON PU.PerfilID = P.IdPerfil
@@ -33,17 +39,42 @@ BEGIN
           AND P.IdContrato = @idContrato
           AND (R.Rol LIKE '%Admin%' OR R.ROL LIKE '%SASISOPA%SHELL%');
 
-	IF ISNULL(@IsAdmin,0) > 0
+	--BUSQUEDA DE PERMISOS DE APROBADOR
+	SELECT
+          @Aprobadors = CASE ISNULL(EXAR.idUsuario, 0)
+						   WHEN 0 THEN A.idUsuario
+						   ELSE EXAR.idUsuario
+					  END
+    FROM dbo.EN_InstanciasEntregable I
+        JOIN dbo.EN_Actividad A
+            ON I.ActividadID = A.ActividadID
+        LEFT JOIN dbo.EN_ExcepcionesActividad EXAR
+            ON A.ActividadID = EXAR.ActividadIDExcepcion
+               AND I.idInstanciaEntregable = EXAR.IdInstanciasEntregables
+    WHERE idInstanciaEntregable = @IdInstanciaEntregable;
+
+	IF ISNULL(@Admins,0) > 0
 	BEGIN
 		--ES USUARIO ADMINISTRADOR
-		SELECT 1 AS ISADMIN
+		SET @EsAdmin = 1;
 
-	END
-	ELSE
-	BEGIN 
-		--NO ES ADMINISTRADOR
-		SELECT 0 AS ISADMIN
+	END;
 
-	END
+	IF ISNULL(@Aprobadors,0) > 0
+	BEGIN
+
+		IF (@Aprobadors = @IdUsuario)
+		BEGIN
+			
+			--ES USUARIO APROBADOR
+			SET @EsAprobador = 1;
+
+		END;
+
+	END;
+
+	SELECT 
+		@EsAdmin AS EsAdmin,
+		@EsAprobador AS EsAprobador;
 
 END
