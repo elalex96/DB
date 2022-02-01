d8a8274f097038f873d072b1203ab1a109503961
Pedro Pouchoulen <pedro.pouchoulen@adinco.mx>
2022-02-01 16:45:49 -0600

    Issue/Adinco/1724-ActualizarCertificadoGE

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SP_CO_ConsultaMesPresentacion.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SP_CO_ConsultaMesPresentacion.sql
@@ -1,8 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[SP_CO_ConsultaMesPresentacion]
 AS
 BEGIN
-    SELECT DISTINCT MesPresentacion,
-                    CONVERT(VARCHAR(10), MesPresentacion, 105) AS 'Mes'
+    SELECT DISTINCT CONVERT(VARCHAR(10), MesPresentacion, 105) AS MesPresentacion,
+                    MONTH (MesPresentacion) AS 'Mes',
+					Year (MesPresentacion) AS 'Year'
       FROM CO_Registro
      ORDER BY MesPresentacion ASC;
 END;
