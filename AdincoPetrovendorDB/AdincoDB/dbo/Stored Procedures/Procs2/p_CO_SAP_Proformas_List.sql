e87b20f534f53264300b64dc581aff8f05b9167c
Daniel Moreno <daniel.moreno@adinco.mx>
2022-02-04 16:06:43 -0600

    ajsute sp

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/p_CO_SAP_Proformas_List.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/p_CO_SAP_Proformas_List.sql
@@ -1,5 +1,5 @@
-﻿
-CREATE PROC p_CO_SAP_Proformas_List
+﻿-- p_CO_SAP_Proformas_List 10023,1
+create PROC p_CO_SAP_Proformas_List
 @pIdCotratista INT,
 @pEstatus INT
 AS
@@ -21,12 +21,13 @@ SELECT pr.IdPRESES,
 	from CO_SAPPRESES pr
 	INNER JOIN [dbo].[CO_SAPVendor] ven on ven.VendorIDSAP = pr.SAPVendorNumber
 	INNER JOIN Petrovendor..S_Proveedor prov on prov.RFC COLLATE SQL_Latin1_General_CP1_CI_AS = ven.TaxID COLLATE SQL_Latin1_General_CP1_CI_AS	
-	INNER JOIN Petrovendor..S_Usuario usuP on usuP.IdUsuario = pr.CreadoPor
-	INNER JOIN CO_Contrato CON on CON.IdContrato = ven.IdContrato AND
-								CON.IdContratista = @pIdCotratista
-	INNER JOIN CO_Contratista CI ON CI.IdContratista = CON.IdContratista
+	INNER JOIN Petrovendor..S_Usuario usuP on usuP.IdUsuario = pr.CreadoPor	
+	INNER JOIN CO_SAPPO PO ON PO.SAPPONumber = pr.SAPPONumber
+	INNER JOIN CO_SAPContratista_Planta CP ON CP.Planta = PO.Plant
+	INNER JOIN CO_Contratista CI ON CI.IdContratista = CP.IdContratista
     LEFT JOIN CO_SAPSES SES ON SES.SESReferenceNumber = pr.SAPSESNumber AND
 								SES.SESNumber = pr.SESN
 	where	pr.IdEstatus = @pEstatus
 
 END
+
