cb88ddd3c1fe876965952953b8a6b75699cea3e4
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2022-02-23 17:37:41 -0600

    SP_TA_ConsultarFlujoTareaAprobadores
    se agrega la columna usuario adinco id

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_TA_ConsultarFlujoTareaAprobadores.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_TA_ConsultarFlujoTareaAprobadores.sql
@@ -1,4 +1,8 @@
-﻿-- =============================================
+﻿USE PETROVENDOR
+GO
+DROP PROCEDURE IF EXISTS SP_TA_ConsultarFlujoTareaAprobadores
+GO
+-- =============================================
 -- Author:		Daniel Cruz
 -- Create date: 27-03-17
 -- Description:	Regresa los aprobadores de un flujo de tarea				
@@ -7,7 +11,12 @@
 -- Create date: 16-08-2018
 -- Description:	Se agrega la consulta del telefono				
 -- =============================================
-	CREATE  PROCEDURE SP_TA_ConsultarFlujoTareaAprobadores
+--************************************************************** 
+-- Modified:    LUIS DAVID
+-- Updated date: 22/02/2022
+-- Description: Se agrega el idusuarioAdinco para notificaciones push
+--************************************************************** 
+CREATE  PROCEDURE SP_TA_ConsultarFlujoTareaAprobadores
 	-- Add the parameters for the stored procedure here
 	 @IdFlujoTarea int 
 AS
@@ -18,7 +27,7 @@ BEGIN
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
-	 SELECT A.IdUsuario, IdFlujoTarea, NoSecuencia, Nombre, Correo, ISNULL(U.Telefono, '')
+	 SELECT A.IdUsuario, IdFlujoTarea, NoSecuencia, Nombre, Correo, ISNULL(U.Telefono, ''),U.IdUsuarioADINCO
 	 FROM TA_Aprobador AS A
 	 INNER JOIN S_Usuario AS U on U.IdUsuario = A.IdUsuario
 	 WHERE A.IdFlujoTarea = @IdFlujoTarea
