4c578b45e97fcb5cf818c53a9d299df944458c96
David De La Cruz <david.cruz@adinco.mx>
2022-02-25 00:36:18 -0600

    se agrega el nuevo sp para obtener urls a partir de una descripción

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/WS_sp_ObtenWSURLPorDescripcion.sql
@@ -0,0 +1,11 @@
+﻿USE Petrovendor
+GO
+DROP PROCEDURE IF EXISTS WS_sp_ObtenWSURLPorDescripcion
+go
+CREATE PROCEDURE WS_sp_ObtenWSURLPorDescripcion
+@descripcion varchar(50)
+as
+BEGIN 
+	SELECT * FROM WS_URLS 
+	WHERE DESCRIPCION = @descripcion
+END
