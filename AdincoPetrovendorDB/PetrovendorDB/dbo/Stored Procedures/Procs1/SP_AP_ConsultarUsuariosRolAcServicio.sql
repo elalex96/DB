1b0d10f6571a312bf102b1c53e35020e67a14e89
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2022-03-01 15:51:33 -0600

    Update SP_AP_ConsultarUsuariosRolAcServicio.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_AP_ConsultarUsuariosRolAcServicio.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_AP_ConsultarUsuariosRolAcServicio.sql
@@ -1,6 +1,6 @@
 USE [Petrovendor]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_AP_ConsultarUsuariosRolAcServicio]    Script Date: 28/02/2022 08:46:36 p. m. ******/
+/****** Object:  StoredProcedure [dbo].[SP_AP_ConsultarUsuariosRolAcServicio]    Script Date: 01/03/2022 03:51:00 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,10 +10,6 @@ GO
 -- Create date: <Consulta la lista de usuarios con rol de "Aceptación de servicio">
 -- Description:	<Description,,>
 -- =============================================
--- Author:		<Alexander Gomez>
--- Create date: <28/02/2022>
--- Description:	<validacion para usuarios dea>
--- =============================================
 ALTER PROCEDURE [dbo].[SP_AP_ConsultarUsuariosRolAcServicio] --420,3
 @IdProveedor INT,
 @IdContrato INT
@@ -22,31 +18,12 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-	DECLARE @RFC VARCHAR(100) = (SELECT TOP 1 RFC FROM S_Proveedor WHERE IdProveedor = @IdProveedor AND Activo = 1);
-
-	--VALIDACION PARA DEA
-	IF EXISTS (SELECT * FROM DEA_Proveedor WHERE RFC = @RFC)
-	BEGIN
 		
 		SELECT U.IdUsuario AS Asignado,U.Nombre FROM dbo.S_UsuarioRol UR
 		LEFT JOIN dbo.S_UsuarioProveedor UP ON UP.IdUsuario = UR.IdUsuario
 		LEFT JOIN dbo.S_Usuario U ON U.IdUsuario = UP.IdUsuario
 		WHERE UP.IdProveedor = @IdProveedor 
 		AND ISNULL(U.IsEliminado,0) = 0
-		AND UP.IdContrato = @IdContrato
-
-	END
-	ELSE
-	BEGIN 
-		
-		SELECT U.IdUsuario AS Asignado,U.Nombre FROM dbo.S_UsuarioRol UR
-		LEFT JOIN dbo.S_UsuarioProveedor UP ON UP.IdUsuario = UR.IdUsuario
-		LEFT JOIN dbo.S_Usuario U ON U.IdUsuario = UP.IdUsuario
-		WHERE UP.IdProveedor = @IdProveedor 
-		AND ISNULL(U.IsEliminado,0) = 0
-		AND UP.IdContrato = @IdContrato
-		AND UR.IdRol = 8 -- aceptación servicio
-
-	END
+		AND UP.IdContrato = @IdContrato;
 
 END
