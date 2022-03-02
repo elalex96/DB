1674c5842f13086caaee1bb5dd41dbd5cd5f1f44
Daniel Moreno <daniel.moreno@adinco.mx>
2022-03-01 18:18:13 -0600

    cambios BD

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/p_AWS_CredencialesDropbox.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/p_AWS_CredencialesDropbox.sql
@@ -6,6 +6,12 @@ as
 begin
 
 	SELECT 
-	'j1kxkzuixjUAAAAAAAAAAbNK3SBh8QNUK-pzOHCCv_ebF52Gbq4wPzfofzxhKaFt' AS AccessTokenValue, -->CTES DE PRUEBA	
-	'~/' AS RootDefault -->CTES DE PRUEBA 	
+	AccessTokenValue, 
+	RootDefault
+	from AWS_DropboxCredenciales
+	where IdCOntrato = @ContratoId
+	
 end
+
+
+
