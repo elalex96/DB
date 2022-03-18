24fc4a30f3faee0183f6897cac25bf57d85e9cf2
Marcos Garcia <marcos.garcia@adinco.mx>
2022-03-18 13:54:21 -0600

    Ajuste en sp obtencion correcta

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_SIPAC_ListaArchivosReporteGastosExtranjero.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_SIPAC_ListaArchivosReporteGastosExtranjero.sql
@@ -2,8 +2,12 @@
 -- Author:		Manuel Cruz
 -- Create date: 07-06-17
 -- Description:	
+
+-- Cambios Por: Neri del Angel
+-- Create date: 18 de Marzo del 2022
+-- Description:	Ajuste para que obtenga los PE y PI correctamente mediante metodos usadoen el la Hoja 24 y 25
 -- =============================================
-CREATE PROCEDURE [dbo].[sp_SIPAC_ListaArchivosReporteGastosExtranjero]
+CREATE PROCEDURE [dbo].[sp_SIPAC_ListaArchivosReporteGastosExtranjero]  
 -- Add the parameters for the stored procedure here
 @Contrato      INT, 
 @Mes           DATE, 
@@ -13,93 +17,104 @@ AS
          -- SET NOCOUNT ON added to prevent extra result sets from
          -- interfering with SELECT statements.
          SET NOCOUNT ON;
-         CREATE TABLE #Documentos
-         (Nombre NVARCHAR(MAX), 
-          Doc    IMAGE
+		 --========================================
+         IF OBJECT_ID('tempdb..#PedimentoComprobante', 'U') IS NOT NULL
+             DROP TABLE #PedimentoComprobante;
+		--========================================
+         CREATE TABLE #PedimentoComprobante
+         (IdPedimentoComprobante INT, 
+          IdContrato             INT, 
+          CvTipoDocFacturacion   INT, 
+          SIPAC                  INT
          );
-         -- Insert statements for procedure here
-
-         INSERT INTO #Documentos
-         (Nombre, 
-          Doc
-         )
-                SELECT REPLACE(PC.IdDocFacturacionSIPAC, '-', '_')+'.pdf', 
-                       doc.DocumentoByte
-                FROM fi_transfer tr
-                     LEFT JOIN FI_transferfactura TF ON TF.IdTransfer = TR.IdTransferencia
-                     LEFT JOIN FI_PedimentoComprobante PC ON TF.IdPedimentoComprobante = PC.IdPedimentoComprobante
-                     LEFT JOIN co_registro r ON r.IdPedimentoComprobante = PC.IdPedimentoComprobante
-                     LEFT JOIN co_lineapresupuestomes lpm ON r.idprograma = lpm.idlineapresupuestomes
-                     LEFT JOIN co_presupuesto p ON p.idpresupuesto = lpm.idpresupuesto
-                     LEFT JOIN Co_anioContractual AC ON AC.idaniocontractual = p.idaniocontractual
-                     LEFT JOIN co_contrato c ON tr.idcontrato = c.idcontrato
-                     LEFT JOIN co_contratista con ON c.idcontratista = con.idcontratista
-                     LEFT JOIN fi_documento doc ON pc.idpedimentocomprobante = doc.idpedimentocomprobante
-                     LEFT JOIN pv_subcontratista subi ON pc.idsubcontratistaimportador = subi.idsubcontratista
-                     LEFT JOIN pv_tipomoneda tm ON pc.idmoneda = tm.idmoneda
-                     LEFT JOIN CO_TipoCambioDiario TCD ON TCD.IdMoneda = TM.IdMoneda
-                                                          AND DAY(TCD.Fecha) = DAY(PC.FechaPago)
-                                                          AND MONTH(TCD.Fecha) = MONTH(PC.FechaPago)
-                                                          AND YEAR(TCD.Fecha) = YEAR(PC.FechaPago)
-                     LEFT JOIN fi_pedimentocomprobantedetalle pcd ON pc.idpedimentocomprobante = pcd.idpedimentocomprobante
-                     LEFT JOIN pv_unidad u ON pcd.idunidadmedida = u.idunidad
-                     LEFT JOIN fi_estudiopreciostransfer ept ON pc.IdEstudioPrecioTransfer = ept.IdEstudioPrecioTransfer
-                     LEFT JOIN CO_RelacionEmpresas RE ON RE.idcontratista = Con.idproveedor
-                                                         AND PC.IdSubcontratistaImportador = RE.IdRelacionada
-                WHERE PC.CvTipoDocFacturacion = 3
+         INSERT INTO #PedimentoComprobante
+                SELECT PC.IdPedimentoComprobante, 
+                       PC.IdContrato, 
+                       PC.CvTipoDocFacturacion, 
+                       ROW_NUMBER() OVER(ORDER BY PC.FechaPago) AS SIPAC
+                FROM dbo.FI_Transfer TR WITH(NOLOCK)
+                     JOIN dbo.FI_TransferFactura TF WITH(NOLOCK) ON TF.IdTransfer = TR.IdTransferencia
+                     JOIN dbo.FI_PedimentoComprobante PC WITH(NOLOCK) ON TF.IdPedimentoComprobante = PC.IdPedimentoComprobante
+                     JOIN dbo.CO_Registro R WITH(NOLOCK) ON R.IdPedimentoComprobante = PC.IdPedimentoComprobante
+                     JOIN dbo.CO_LineaPresupuestoMes LPM WITH(NOLOCK) ON R.IdPrograma = LPM.IdLineaPresupuestoMes
+                     JOIN dbo.CO_Presupuesto P WITH(NOLOCK) ON P.IdPresupuesto = LPM.IdPresupuesto
+                     JOIN dbo.CO_AnioContractual AC WITH(NOLOCK) ON AC.IdAnioContractual = P.IdAnioContractual
+                     JOIN dbo.CO_Contrato C WITH(NOLOCK) ON AC.IdContrato = C.IdContrato
+                     JOIN dbo.CO_Contratista CON WITH(NOLOCK) ON C.IdContratista = CON.IdContratista
+                     JOIN dbo.FI_Documento DOC WITH(NOLOCK) ON PC.IdPedimentoComprobante = DOC.IdPedimentoComprobante
+                     JOIN dbo.PV_Subcontratista SUBE WITH(NOLOCK) ON PC.IdSubcontratistaExportador = SUBE.IdSubcontratista
+                     JOIN dbo.PV_TipoMoneda TM WITH(NOLOCK) ON PC.IdMoneda = TM.IdMoneda
+                     JOIN dbo.CO_TipoCambioDiario TCD WITH(NOLOCK) ON TCD.IdMoneda = TM.IdMoneda
+                                                                      AND DAY(TCD.Fecha) = DAY(PC.FechaPago)
+                                                                      AND MONTH(TCD.Fecha) = MONTH(PC.FechaPago)
+                                                                      AND YEAR(TCD.Fecha) = YEAR(PC.FechaPago)
+                     JOIN dbo.FI_PedimentoComprobanteDetalle PCD WITH(NOLOCK) ON PC.IdPedimentoComprobante = PCD.IdPedimentoComprobante
+                     JOIN dbo.PV_MetodoPago PVM WITH(NOLOCK) ON PVM.idMetodoPago = TR.IdMetodoPago
+                     JOIN dbo.CO_Servicio SER WITH(NOLOCK) ON SER.IdServicio = LPM.IdServicio
+                WHERE R.CvTipoDocFacturacion = 3
                       AND C.IdContrato = @Contrato
-                      AND DATEFROMPARTS(YEAR(r.MesPresentacion), MONTH(r.MesPresentacion), 1) = @Mes
-                      AND r.idestado = 10004
-                      AND ISNULL(CONVERT(INT, PC.ProcesadoSIPAC), 0) = 0;
-         --AND p.idpresupuesto = @IdPresupuesto;
-         INSERT INTO #Documentos
-         (Nombre, 
-          Doc
-         )
-                SELECT REPLACE(EPT.IdDocFacturacionSIPAC, '-', '_')+'.pdf', 
-                       EPT.Archivo
-                FROM fi_transfer tr(NOLOCK)
-                     JOIN fi_transferfactura tf ON tr.idtransferencia = tf.idtransfer
-                     JOIN FI_Factura F ON tf.IdFactura = F.IdFactura
-                     JOIN co_registro r ON r.IdFactura = F.IdFactura
-                     JOIN co_lineapresupuestomes lpm ON r.idprograma = lpm.idlineapresupuestomes
-                     JOIN co_presupuesto p ON p.idpresupuesto = lpm.idpresupuesto
-                     JOIN Co_anioContractual AC ON AC.idaniocontractual = p.idaniocontractual
-                     JOIN co_contrato c ON AC.idcontrato = c.idcontrato
-                     JOIN co_contratista con ON c.idcontratista = con.idcontratista
-                     JOIN FI_EstudioPreciosTransfer EPT ON F.IdEstudioPrecioTransfer = EPT.IdEstudioPrecioTransfer
-                WHERE C.IdContrato = @Contrato
-                      AND R.IdEstado = 10004
                       AND DATEFROMPARTS(YEAR(R.MesPresentacion), MONTH(R.MesPresentacion), 1) = @Mes
-                      AND r.CvTipoDocFacturacion = 1
-                      AND isnull(CONVERT(INT, F.ProcesadoSIPAC), 0) = 0
-                      --AND p.idpresupuesto = @IdPresupuesto
-                      AND F.IdEstudioPrecioTransfer IS NOT NULL;
-         INSERT INTO #Documentos
-         (Nombre, 
-          Doc
-         )
-                SELECT REPLACE(EPT.IdDocFacturacionSIPAC, '-', '_')+'.pdf', 
-                       EPT.Archivo
-                FROM fi_transfer tr(NOLOCK)
-                     JOIN fi_transferfactura tf ON tr.idtransferencia = tf.idtransfer
-                     JOIN FI_PedimentoComprobante PC ON tf.IdPedimentoComprobante = PC.IdPedimentoComprobante
-                     JOIN co_registro r ON r.IdPedimentoComprobante = pc.IdPedimentoComprobante
-                     JOIN co_lineapresupuestomes lpm ON r.idprograma = lpm.idlineapresupuestomes
-                     JOIN co_presupuesto p ON p.idpresupuesto = lpm.idpresupuesto
-                     JOIN Co_anioContractual AC ON AC.idaniocontractual = p.idaniocontractual
-                     JOIN co_contrato c ON AC.idcontrato = c.idcontrato
-                     JOIN co_contratista con ON c.idcontratista = con.idcontratista
-                     JOIN FI_EstudioPreciosTransfer EPT ON PC.IdEstudioPrecioTransfer = EPT.IdEstudioPrecioTransfer
-                WHERE C.IdContrato = @Contrato
                       AND R.IdEstado = 10004
+                      AND ISNULL(CONVERT(INT, PC.ProcesadoSIPAC), 0) = 0
+                      AND SER.NombreServicio NOT LIKE '%No elegibles%'
+                      AND ISNULL(PC.EsnotaCredito, 0) <> 1
+                      AND P.IdPresupuesto = CASE
+                                                WHEN @IdPresupuesto = 0
+                                                THEN LPM.IdPresupuesto
+                                                ELSE @IdPresupuesto
+                                            END
+                GROUP BY PC.IdPedimentoComprobante, 
+                         PC.IdContrato, 
+                         PC.CvTipoDocFacturacion, 
+                         PC.FechaPago
+				UNION
+                SELECT PC.IdPedimentoComprobante, 
+                       PC.IdContrato, 
+                       PC.CvTipoDocFacturacion, 
+                       ROW_NUMBER() OVER(ORDER BY PC.FechaPago) AS SIPAC
+                FROM dbo.FI_Transfer TR WITH(NOLOCK)
+                     JOIN dbo.FI_TransferFactura TF WITH(NOLOCK) ON TF.IdTransfer = TR.IdTransferencia
+                     JOIN dbo.FI_PedimentoComprobante PC WITH(NOLOCK) ON TF.IdPedimentoComprobante = PC.IdPedimentoComprobante
+                     JOIN dbo.CO_Registro R WITH(NOLOCK) ON R.IdPedimentoComprobante = PC.IdPedimentoComprobante
+                     JOIN dbo.CO_LineaPresupuestoMes LPM WITH(NOLOCK) ON R.IdPrograma = LPM.IdLineaPresupuestoMes
+                     JOIN dbo.CO_Presupuesto P WITH(NOLOCK) ON P.IdPresupuesto = LPM.IdPresupuesto
+                     JOIN dbo.CO_AnioContractual AC WITH(NOLOCK) ON AC.IdAnioContractual = P.IdAnioContractual
+                     JOIN dbo.CO_Contrato C WITH(NOLOCK) ON AC.IdContrato = C.IdContrato
+                     JOIN dbo.CO_Contratista CON WITH(NOLOCK) ON C.IdContratista = CON.IdContratista
+                     JOIN dbo.FI_PedimentoComprobanteDetalle PCD WITH(NOLOCK) ON PC.IdPedimentoComprobante = PCD.IdPedimentoComprobante
+                     JOIN dbo.PV_Subcontratista SUBI WITH(NOLOCK) ON PC.IdSubcontratistaImportador = SUBI.IdSubcontratista
+                     JOIN dbo.PV_TipoMoneda TM WITH(NOLOCK) ON PC.IdMoneda = TM.IdMoneda
+                     JOIN dbo.FI_CFDIMetodoPago CDIM WITH(NOLOCK) ON CDIM.IdCFDIMetodoPago = TR.IdMetodoPago
+                     JOIN dbo.PV_Subcontratista SUBE WITH(NOLOCK) ON PC.IdSubcontratistaExportador = SUBE.IdSubcontratista
+                     JOIN dbo.FI_ClavesPedimento CV WITH(NOLOCK) ON PC.ClavePedimento = CV.IdPedimento
+                     JOIN dbo.CO_Servicio SER WITH(NOLOCK) ON SER.IdServicio = LPM.IdServicio
+                     LEFT JOIN dbo.CO_TipoCambioDiario TCD WITH(NOLOCK) ON TCD.IdMoneda = TM.IdMoneda
+                                                                           AND DAY(TCD.Fecha) = DAY(PC.FechaPago)
+                                                                           AND MONTH(TCD.Fecha) = MONTH(PC.FechaPago)
+                                                                           AND YEAR(TCD.Fecha) = YEAR(PC.FechaPago)
+                WHERE R.CvTipoDocFacturacion = 2
+                      AND PC.IdContrato = @Contrato
                       AND DATEFROMPARTS(YEAR(R.MesPresentacion), MONTH(R.MesPresentacion), 1) = @Mes
-                      AND r.CvTipoDocFacturacion IN(2, 3)
-                     AND isnull(CONVERT(INT, PC.ProcesadoSIPAC), 0) = 0
-                     --AND p.idpresupuesto = @IdPresupuesto
-                     AND PC.IdEstudioPrecioTransfer IS NOT NULL;
-         SELECT Nombre, 
-                Doc
-         FROM #Documentos
-         WHERE Nombre IS NOT NULL;
+                      AND R.IdEstado = 10004
+                      AND ISNULL(CONVERT(INT, PC.ProcesadoSIPAC), 0) = 0
+                      AND SER.NombreServicio NOT LIKE '%No elegibles%'
+                      AND P.IdPresupuesto = CASE
+                                                WHEN @IdPresupuesto = 0
+                                                THEN LPM.IdPresupuesto
+                                                ELSE @IdPresupuesto
+                                            END
+                GROUP BY PC.IdPedimentoComprobante, 
+                         PC.IdContrato, 
+                         PC.CvTipoDocFacturacion, 
+                         PC.FechaPago;
+		--========================================
+		SELECT 
+			REPLACE(PC.IdDocFacturacionSIPAC, '-', '_')+'.pdf' AS Nombre, 
+				FD.DocumentoByte AS Doc
+		FROM #PedimentoComprobante FPC
+		JOIN FI_PedimentoComprobante PC ON 
+			FPC.IdPedimentoComprobante = PC.IdPedimentoComprobante 
+		JOIN dbo.FI_Documento FD ON 
+			FD.IdTipoDocumento IN(4, 5) AND FPC.IdPedimentoComprobante = FD.IdPedimentoComprobante 
+		ORDER BY IdDocumento DESC
      END;
