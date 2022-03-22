9529bd9f5dfdae71326de75994d1410741e32ff0
Marcos Garcia <marcos.garcia@adinco.mx>
2022-03-22 15:51:29 -0600

    Ajuste tabla y agregar procedimientos nuevos

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_PedimentoComprobanteHASHNoProcesado.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROC [dbo].[SP_FI_PedimentoComprobanteHASHNoProcesado]
+AS
+SELECT TOP 10 
+	PC.IdPedimentoComprobante,
+	PC.HashSHA256, 
+	FD.DocumentoByte,
+	FD.IdTipoDocumento
+FROM FI_PedimentoComprobante PC  
+	INNER JOIN dbo.FI_Documento FD ON  
+		PC.CreadoEn BETWEEN '2019-01-01' AND '2020-12-31' AND 
+		ISNULL(PC.ProcesadoHash, 0) = 0 AND 
+		FD.IdTipoDocumento IN(4, 5) AND 
+		PC.IdPedimentoComprobante = FD.IdPedimentoComprobante 
