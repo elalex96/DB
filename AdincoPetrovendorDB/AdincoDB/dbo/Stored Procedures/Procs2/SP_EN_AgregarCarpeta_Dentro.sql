e381c04326b79e5f1aa84e8f4169810fd91de210
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2022-03-25 03:34:39 -0600

    Se agrego nueva función para recortar tamañao de las carpetas de las rutas de archivos de la tabla del visor

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_EN_AgregarCarpeta_Dentro.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_EN_AgregarCarpeta_Dentro.sql
@@ -1,6 +1,14 @@
 USE [Adinco]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_EN_AgregarCarpeta]    Script Date: 09/03/2022 12:28:37 p. m. ******/
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_EN_AgregarCarpeta_Dentro'
+)
+    DROP PROCEDURE SP_EN_AgregarCarpeta_Dentro;
+GO
+/****** Object:  StoredProcedure [dbo].[SP_EN_AgregarCarpeta_Dentro]    Script Date: 24/03/2022 08:56:15 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,21 +18,34 @@ GO
 -- Create date: <11/01/2022>
 -- Description:	<Agregado de carpetas en el visor de archivos V2>
 -- =============================================
-ALTER PROCEDURE [dbo].[SP_EN_AgregarCarpeta]
+-- =============================================
+-- Author:		DANIEL AC
+-- Create date: <25/03/2022>
+-- Description:	Se agrego parametro de Nivel y CarpetaId del padre de la nueva carpeta
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_EN_AgregarCarpeta_Dentro]
 	-- Add the parameters for the stored procedure here
-	@IdPadre INT,
-	@Nombre VARCHAR(500),
-	@Nivel INT,
+	@Ruta VARCHAR(MAX),
+	@Nombre NVARCHAR(500),
 	@IdUsuario INT,
+	@NivelPadre INT,
+	@CarpetaPadreId INT,
 	@IdContrato INT
+
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
+	DECLARE @IdPadre INT,
+			@Nivel INT,
+			@Frecuencia INT,
+			@IdNuevaCarpeta INT;
+			
+
     -- Insert statements for procedure here
-	INSERT INTO EN_CarpetasArchivosVisor 
+	INSERT INTO EN_CarpetasArchivosVisor
 	(
 		[IsCarpeta],
 		[IsArchivo],
@@ -41,14 +62,28 @@ BEGIN
 		1,
 		0,
 		@Nombre,
-		@IdPadre,
+		@CarpetaPadreId,
 		@IdUsuario,
 		GETDATE(),
-		@Nivel,
+		@NivelPadre,
 		1,
 		@IdContrato
 	);
 
-	SELECT SCOPE_IDENTITY() AS IDCARPETA;
+	SET @IdNuevaCarpeta = (SELECT SCOPE_IDENTITY());
+
+	SELECT TOP 1
+		IdCarpeta,
+		Frecuencia,
+		IsCarpetaUsuario,
+		IdReceptorEntregable,
+		IsPozo,
+		Etapa,
+		AnioMes,
+		Nivel
+	FROM EN_SecuenciaCarpetas
+	WHERE IdCarpeta = @CarpetaPadreId
+	AND Nivel = @NivelPadre
+	AND Activo = 1;
 
 END
