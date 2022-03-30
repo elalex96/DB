8f5451b409bd9631461958d4e001b318b8984b23
Alexander Gomez <alexander.gomez@adinco.mx>
2022-03-30 13:36:26 -0600

    Agregado al sqlproj

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_DEA_GetDocumento_SolicitudAceptacionServicio_1.sql
@@ -0,0 +1,100 @@
+﻿USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_DEA_GetDocumento_SolicitudAceptacionServicio]    Script Date: 30/03/2022 12:09:49 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		ALEXANDER GOMEZ
+-- Create date: <29/03/2022>
+-- Description:	<obtiene la informacion del documento de la solicitud de aceptacion se servicio>
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_DEA_GetDocumento_SolicitudAceptacionServicio] --907,0,17166
+@IdProveedor INT,
+@IdUsuario INT,
+@IdAceptacion INT
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	DECLARE @DOCUMENTO_TIPO_PROFORMA INT = (SELECT TOP 1 IdTipoDocumento FROM S_TipoDocumento WHERE NombreTipoDocumento = 'PROFORMA');
+	DECLARE @DOCUMENTO_TIPO_FIELD_TICKET INT = (SELECT TOP 1 IdTipoDocumento FROM S_TipoDocumento WHERE NombreTipoDocumento = 'FIELD TICKET');
+	DECLARE @DOCUMENTO_TIPO_OTROS_DOCUMENTOS INT = (SELECT TOP 1 IdTipoDocumento FROM S_TipoDocumento WHERE NombreTipoDocumento = 'Aceptacion Pedido');
+
+	CREATE TABLE #DOCUMENTOS_SOLICITUD_ACEPTACION(
+		NombreDocumento NVARCHAR(1000),
+		Extension NVARCHAR(30),
+		Mime NVARCHAR(100),
+		Carpeta NVARCHAR(1000),
+		Identificador NVARCHAR(200),
+		Bucket NVARCHAR(200)
+	);
+
+	--INSERTAR PROFORMAS
+	INSERT INTO #DOCUMENTOS_SOLICITUD_ACEPTACION
+	SELECT
+		'PROFORMA_ ' + SUBSTRING(REPLACE(DS3.NombreDocumento,DS3.Extension,''),0,20) + DS3.Extension,
+		DS3.Extension,
+		DS3.Mime,
+		DS3.Carpeta,
+		DS3.Identificador,
+		DS3.Bucket
+	FROM MM_SolicitudAceptacionPedido AS SAP
+	JOIN MM_AceptacionPedido AS AP
+		ON SAP.IdAceptacionPedido = AP.IdAceptacionPedido
+		AND AP.IdAceptacionPedido = @IdAceptacion
+	JOIN S_Documento_S3 AS DS3
+		ON DS3.IdDocumentoTabla = SAP.IdSolicitudAceptacionPedido
+		AND DS3.IdTipoDocumento = @DOCUMENTO_TIPO_PROFORMA
+		AND DS3.Activo = 1;
+
+	--INSERTAR FIELD TICKET
+	INSERT INTO #DOCUMENTOS_SOLICITUD_ACEPTACION
+	SELECT
+		'FIELDTICKET_' + SUBSTRING(REPLACE(DS3.NombreDocumento,DS3.Extension,''),0,20) + DS3.Extension,
+		DS3.Extension,
+		DS3.Mime,
+		DS3.Carpeta,
+		DS3.Identificador,
+		DS3.Bucket
+	FROM MM_SolicitudAceptacionPedido AS SAP
+	JOIN MM_AceptacionPedido AS AP
+		ON SAP.IdAceptacionPedido = AP.IdAceptacionPedido
+		AND AP.IdAceptacionPedido = @IdAceptacion
+	JOIN S_Documento_S3 AS DS3
+		ON DS3.IdDocumentoTabla = SAP.IdSolicitudAceptacionPedido
+		AND DS3.IdTipoDocumento = @DOCUMENTO_TIPO_FIELD_TICKET
+		AND DS3.Activo = 1;
+
+	--INSERTAR OTROS DOCUMENTOS
+	INSERT INTO #DOCUMENTOS_SOLICITUD_ACEPTACION
+	SELECT
+		SUBSTRING(REPLACE(DS3.NombreDocumento,DS3.Extension,''),0,20) + DS3.Extension,
+		DS3.Extension,
+		DS3.Mime,
+		DS3.Carpeta,
+		DS3.Identificador,
+		DS3.Bucket
+	FROM MM_SolicitudAceptacionPedido AS SAP
+	JOIN MM_AceptacionPedido AS AP
+		ON SAP.IdAceptacionPedido = AP.IdAceptacionPedido
+		AND AP.IdAceptacionPedido = @IdAceptacion
+	JOIN S_Documento_S3 AS DS3
+		ON DS3.IdDocumentoTabla = SAP.IdSolicitudAceptacionPedido
+		AND DS3.IdTipoDocumento = @DOCUMENTO_TIPO_OTROS_DOCUMENTOS
+		AND DS3.Activo = 1;
+
+	SELECT
+		NombreDocumento,
+		Extension,
+		Mime,
+		Carpeta,
+		Identificador,
+		Bucket
+	FROM #DOCUMENTOS_SOLICITUD_ACEPTACION;
+
+END
