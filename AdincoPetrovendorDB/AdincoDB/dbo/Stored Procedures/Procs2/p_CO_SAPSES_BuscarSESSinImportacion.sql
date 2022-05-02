1ccf559c8c2e500b10255bf5c8f4c2c510f7ca8b
Daniel Moreno <daniel.moreno@adinco.mx>
2022-05-02 13:11:18 -0500

    p_CO_SAPSES_BuscarSESSinImportacion

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/p_CO_SAPSES_BuscarSESSinImportacion.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROC p_CO_SAPSES_BuscarSESSinImportacion
+@pIdContrato INT,
+@pSESListado VARCHAR(MAX)
+AS
+
+	SELECT *
+	INTO #TMP_List
+	FROM [dbo].[fnSplitString](@pSESListado,',')
+
+	SELECT TOP 1 TMP.splitdata
+	FROM #TMP_List TMP
+	LEFT JOIN CO_SAPSES SES ON SES.SESNumber = TMP.splitdata AND
+								SES.IdContrato = @pIdContrato
+	WHERE SES.SESNumber IS NULL
+
+
+	
