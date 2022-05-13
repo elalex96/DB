dbb303c778254a3503ecf24dd899d4706d9f2844
Alexander Gomez <alexander.gomez@adinco.mx>
2022-05-13 14:48:59 -0500

    topo de dato tabla EN_IMP_CONFIGRACION_REPSOL para importacion de entregables de repsol

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/User Defined Types/EN_IMP_CONFIGURACION_REPSOL.sql
@@ -0,0 +1,20 @@
+﻿USE [Adinco]
+GO
+
+/****** Object:  UserDefinedTableType [dbo].[Entregables_Importacion]    Script Date: 12/05/2022 01:53:36 p. m. ******/
+CREATE TYPE [dbo].[EN_IMP_CONFIGURACION_REPSOL] AS TABLE(
+	[IdEntregable] [nvarchar](100) NULL,
+	[Area] [nvarchar](1000) NULL,
+	[DiasAlertaPrevia] [nvarchar](100) NULL,
+	[DiasElaboracion] [nvarchar](100) NULL,
+	[DiasRevicion] [nvarchar](100) NULL,
+	[DiasAprobacion] [nvarchar](100) NULL,
+	[Activo] [nvarchar](10) NULL,
+	[Elaborador] [nvarchar](1000) NULL,
+	[ReceptorAlerta] [nvarchar](1000) NULL,
+	[LiderArea] [nvarchar](1000) NULL,
+	[ElaboradorInterno] [nvarchar](1000) NULL
+)
+GO
+
+
