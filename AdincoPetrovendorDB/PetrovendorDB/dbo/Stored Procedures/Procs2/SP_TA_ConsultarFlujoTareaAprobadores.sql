f4880129df8f2279816435e54f732e8ba5965ae2
Alexander Gomez <alexander.gomez@adinco.mx>
2022-05-17 18:25:05 -0500

    modificacion en consulta para obtener aprobadores activos en el flujo de aprobacion del pedimento comprobante

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_TA_ConsultarFlujoTareaAprobadores.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_TA_ConsultarFlujoTareaAprobadores.sql
@@ -1,6 +1,9 @@
-﻿USE PETROVENDOR
+﻿USE [Petrovendor]
 GO
-DROP PROCEDURE IF EXISTS SP_TA_ConsultarFlujoTareaAprobadores
+/****** Object:  StoredProcedure [dbo].[SP_TA_ConsultarFlujoTareaAprobadores]    Script Date: 16/05/2022 10:17:37 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
 -- Author:		Daniel Cruz
@@ -16,7 +19,12 @@ GO
 -- Updated date: 22/02/2022
 -- Description: Se agrega el idusuarioAdinco para notificaciones push
 --************************************************************** 
-CREATE  PROCEDURE SP_TA_ConsultarFlujoTareaAprobadores
+-- =============================================
+-- Author:		Alexander Gomez
+-- Create date: 16/05/2022
+-- Description:	 Se descartan en la aprobacion los usuarios eliminados
+-- =============================================
+ALTER  PROCEDURE [dbo].[SP_TA_ConsultarFlujoTareaAprobadores]
 	-- Add the parameters for the stored procedure here
 	 @IdFlujoTarea int 
 AS
@@ -27,9 +35,19 @@ BEGIN
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here
-	 SELECT A.IdUsuario, IdFlujoTarea, NoSecuencia, Nombre, Correo, ISNULL(U.Telefono, ''),U.IdUsuarioADINCO
+	 SELECT 
+		A.IdUsuario, 
+		IdFlujoTarea, 
+		NoSecuencia, 
+		Nombre, 
+		Correo, 
+		ISNULL(U.Telefono, ''),
+		U.IdUsuarioADINCO
 	 FROM TA_Aprobador AS A
-	 INNER JOIN S_Usuario AS U on U.IdUsuario = A.IdUsuario
+	 INNER JOIN S_Usuario AS U 
+		ON U.IdUsuario = A.IdUsuario 
+			AND U.Activo = 1 
+			AND ISNULL(U.IsEliminado,0) = 0
 	 WHERE A.IdFlujoTarea = @IdFlujoTarea
 	 ORDER BY  NoSecuencia ASC
 
