97f63e1fa64624881e52f5e3bde846179af0cb53
Reyna Olvera <reyna.olvera@adinco.mx>
2022-05-19 06:47:27 -0500

    cambios

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/EPT_RegistraImportaLayout.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE PROCEDURE [dbo].[EPT_RegistraImportaLayout]--10007,10,10000,'20220401',12.3333,0
+	@IdContrato INT,
+	@IdUsuario INT,
+	@ArchivoImportado VARCHAR(150)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	INSERT INTO	EPT_ImportacionLayout (ArchivoImportado,UsuarioId,ContratoId,CreadoEn)
+								VALUES (@ArchivoImportado,@IdUsuario,@IdContrato,GETDATE());
+	SELECT SCOPE_IDENTITY() AS ImportacionLayoutId
+END;
+
+go
+END;
