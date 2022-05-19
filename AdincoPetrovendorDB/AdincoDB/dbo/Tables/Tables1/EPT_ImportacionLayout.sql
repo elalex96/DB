429b51345ea511d301e910f8a06a95bfdea6c67e
Reyna Olvera <reyna.olvera@adinco.mx>
2022-05-19 06:47:33 -0500

    cambios

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/EPT_ImportacionLayout.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[EPT_ImportacionLayout](
+	Id int IDENTITY(1,1) NOT NULL,
+	ArchivoImportado varchar(max) NULL,
+	UsuarioId INT NOT NULL,
+	ContratoId INT NOT NULL,
+	CreadoEn DATETIME NOT NULL,
+	CONSTRAINT [PK_EPT_ImportacionLayout] PRIMARY KEY(Id),
+	CONSTRAINT [FK_EPT_ImportacionLayout_Usuario] FOREIGN KEY(UsuarioId) REFERENCES AP_USUARIO (UsuarioID),
+	CONSTRAINT [FK_EPT_ImportacionLayout_Contratos] FOREIGN KEY(ContratoId) REFERENCES CO_CONTRATO (IdContrato)
+);
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/EPT_ImportacionLayoutDetalle.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[EPT_ImportacionLayoutDetalle](
+	Id int IDENTITY(1,1) NOT NULL,
+	ImportacionLayoutId INT NOT NULL,
+	Contratista varchar(max) NULL,
+	Contrato varchar(max) NULL,
+	IdentificadorDocumento varchar(max) NULL,
+	NombreDocumento varchar(max) NULL,
+	Mes INT,
+	Anio INT,
+	CreadoEn DATETIME NOT NULL,
+	CONSTRAINT [PK_EPT_ImportacionLayoutDetalle] PRIMARY KEY(Id),
+	CONSTRAINT [FK_EPT_ImportacionLayoutDetalle_Importacion] FOREIGN KEY(ImportacionLayoutId) REFERENCES EPT_ImportacionLayout (Id)
+)
