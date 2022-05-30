b6cc944a52192a028885c4fe0ff0e628379dc808
Alexander Gomez <alexander.gomez@adinco.mx>
2022-05-30 09:16:54 -0500

    Sp y tablas  necesarios para funcionalidad de descarga masiva de archivos

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_EN_CF_GenerarSolicitudDescarga.sql
@@ -0,0 +1,43 @@
+﻿USE [Adinco]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_EN_CF_GenerarSolicitudDescarga]    Script Date: 30/05/2022 08:23:21 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Alexander Gomez
+-- Create date: 25/05/2022
+-- Description:	Registro de solicitudes de descarga de rutas en contract files
+-- =============================================
+ALTER PROCEDURE [dbo].[SP_EN_CF_GenerarSolicitudDescarga]
+	-- Add the parameters for the stored procedure here
+	@Ruta NVARCHAR(MAX),
+	@IdUsuario INT,
+	@IdContrato INT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	INSERT INTO EN_CF_SolicitUDescargaCarpetas(
+		SolicitadoPor,
+		SolicitadoEl,
+		ContratoId,
+		RutaDescargada,
+		Procesado
+	)
+	VALUES
+	(	
+		@IdUsuario,
+		GETDATE(),
+		@IdContrato,
+		@Ruta,
+		0
+	);
+
+	SELECT SCOPE_IDENTITY();
+
+END
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables2/EN_CF_SolicitUDescargaCarpetas.sql
@@ -0,0 +1,33 @@
+﻿USE [Adinco]
+GO
+
+/****** Object:  Table [dbo].[EN_CF_SolicitUDescargaCarpetas]    Script Date: 30/05/2022 08:24:05 a. m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[EN_CF_SolicitUDescargaCarpetas](
+	[IdSolicitud] [int] IDENTITY(1,1) NOT NULL,
+	[SolicitadoPor] [int] NULL,
+	[SolicitadoEl] [datetime] NULL,
+	[ContratoId] [int] NULL,
+	[RutaDescargada] [nvarchar](max) NULL,
+	[Procesado] [int] NULL,
+	[UltimaDescarga] [datetime] NULL,
+	[Bucket] [nvarchar](1000) NULL,
+	[Folder] [nvarchar](1000) NULL,
+	[UUIDAmazon] [nvarchar](1000) NULL,
+	[NombreArchivo] [nvarchar](1000) NULL,
+	[Size] [float] NULL,
+	[Meta] [nvarchar](1000) NULL,
+	[FechaProcesado] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[IdSolicitud] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
