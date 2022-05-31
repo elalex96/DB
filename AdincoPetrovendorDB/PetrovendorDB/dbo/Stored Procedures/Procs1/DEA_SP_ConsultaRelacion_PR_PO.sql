4d765a8b514f3ddc15285d07ee22df4113866ca2
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2022-05-31 15:56:36 -0500

    Se realiza cambio de jerarquia de llamadas a laas tablas y  se remueve relación donde se compara el po entre las tablas DEA_Relacion_PR_PO y DEA_AdjuntoPO

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/DEA_SP_ConsultaRelacion_PR_PO.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/DEA_SP_ConsultaRelacion_PR_PO.sql
@@ -1,7 +1,27 @@
-﻿-- =============================================
+﻿USE [Petrovendor]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'DEA_SP_ConsultaRelacion_PR_PO'
+)
+    DROP PROCEDURE DEA_SP_ConsultaRelacion_PR_PO;
+	GO
+/****** Object:  StoredProcedure [dbo].[DEA_SP_ConsultaRelacion_PR_PO]    Script Date: 30/05/2022 06:12:12 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		Daniel AC
 -- Create date: <14/08/2019>
 -- Description:	<Consulta las relaciones de PR - PO>
+-- =============================================
+-- =============================================
+-- Author:		Daniel AC
+-- Create date: <31/05/2022>
+-- Description:	<Se agrega filtro de tipos de pedidos y se elimina relación de  PO.ID_PO = R.PO >
 -- =============================================
 CREATE PROCEDURE [dbo].[DEA_SP_ConsultaRelacion_PR_PO]
 	-- Add the parameters for the stored procedure here
@@ -30,17 +50,16 @@ BEGIN
 		Contrato = c.NumeroContrato
 	FROM dbo.DEA_Relacion_PR_PO AS R
 		INNER JOIN dbo.DEA_AdjuntoPO AS PO 
-			ON PO.ID_PO = R.PO --> PO DEBE SER IGUAL A LA DE LA RELACIÓN
-			AND  R.IdAdjuntoPO=PO.IdAdjuntoPO --> LA DIFERENCIA ES EL IDADJUNTOPO
-		INNER JOIN dbo.MM_Pedido AS P ON P.IdPedido = R.IdPedido
-		INNER JOIN dbo.MM_SolicitudPedido AS SP ON SP.IdSolicitudPedido = P.IdSolicitudPedido
-		LEFT JOIN dbo.DEA_AdjuntoPR AS PR ON PR.IdSolicitudPedido = SP.IdSolicitudPedido AND P.IdSolicitudPedido=PR.IdSolicitudPedido	
-		LEFT JOIN dbo.DEA_Documento_S3 DR ON DR.IdDocumentoTabla=PR.IdAjuntoPr AND DR.IdTipoDocumento=1 --> Documento de tipo PR
-		LEFT JOIN dbo.MM_Pedidos AS PS ON PS.IdIdentificador = P.IdPedido 
-		LEFT JOIN dbo.S_Proveedor AS PRS ON PRS.IdProveedor = P.IdSubcontratista
-		LEFT JOIN dbo.MM_PedidoDetalle AS PD ON PD.IdPedido = P.IdPedido
-		LEFT JOIN PV_TipoMoneda AS TM ON TM.IdMoneda = P.IdMoneda		
-		LEFT JOIN dbo.S_Usuario AS US ON US.IdUsuario = R.CreadoPor
+			ON R.IdAdjuntoPO=PO.IdAdjuntoPO --> LA DIFERENCIA ES EL IDADJUNTOPO
+		INNER JOIN dbo.MM_Pedido AS P ON R.IdPedido = P.IdPedido 
+		INNER JOIN dbo.MM_SolicitudPedido AS SP ON P.IdSolicitudPedido = SP.IdSolicitudPedido 
+		LEFT JOIN dbo.DEA_AdjuntoPR AS PR ON SP.IdSolicitudPedido = PR.IdSolicitudPedido AND PR.IdSolicitudPedido	 = P.IdSolicitudPedido
+		LEFT JOIN dbo.DEA_Documento_S3 DR ON PR.IdAjuntoPr = DR.IdDocumentoTabla AND DR.IdTipoDocumento=1 --> Documento de tipo PR
+		LEFT JOIN dbo.MM_Pedidos AS PS ON P.IdPedido  = PS.IdIdentificador AND PS.IdTipoPedido IN (2,4,6) AND P.IdProveedorCompras= PS.IdProveedorCliente  --> CTES TIPOS DE PEDIDO
+		LEFT JOIN dbo.S_Proveedor AS PRS ON P.IdSubcontratista = PRS.IdProveedor 
+		LEFT JOIN dbo.MM_PedidoDetalle AS PD ON P.IdPedido = PD.IdPedido 
+		LEFT JOIN PV_TipoMoneda AS TM ON P.IdMoneda		 = TM.IdMoneda 
+		LEFT JOIN dbo.S_Usuario AS US ON R.CreadoPor = US.IdUsuario 
 		left JOIN	Adinco.dbo.CO_Contrato	AS	C 	ON	P.IdContrato	=	C.IdContrato 
 	WHERE SP.IdProveedor = @IdProveedor 
 		AND PS.IdProveedorCliente = @IdProveedor
