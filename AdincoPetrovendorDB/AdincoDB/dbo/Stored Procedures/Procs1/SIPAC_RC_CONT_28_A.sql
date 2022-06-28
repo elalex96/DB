91865f2e63db18a76486e60d5d9e9d27b47827f1
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2022-06-28 18:40:47 -0500

    Stored procedure 27: se elimina apartados comentados,, sin embargo se agrega en el pull el respando
    Stored procedure 28: se elimina apartados comentados, Se actualiza en el github desde el stored procedure de productivo y  se agrega en el pull el respando

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SIPAC_RC_CONT_28_A.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SIPAC_RC_CONT_28_A.sql
@@ -1,8 +1,4 @@
 ﻿CREATE PROCEDURE [dbo].[SIPAC_RC_CONT_28_A]
---SIPAC_RC_CONT_28_A 3,'2019-06-01',1
---SIPAC_RC_CONT_28_A 10005,'2017-11-01',1
---SIPAC_RC_CONT_28_A 10039,'2019-07-01',1
--- Add the parameters for the stored procedure here
 @Contrato      INT, 
 @Mes           DATE, 
 @IdPresupuesto INT
@@ -13,9 +9,11 @@ AS
          -- Create date: 28-12-17
          -- Description:	
          -- =============================================
-         -- SET NOCOUNT ON added to prevent extra result sets from
+         -- Author:		 Reyna Olvera
+         -- Create date: 28062022
+         -- Description: Se elimina texto comentado
+         -- =============================================
          SET NOCOUNT ON;
-         --
          SELECT DISTINCT 
                 ResultUnion.RF_00, 
                 ResultUnion.RI_00, 
@@ -28,6 +26,7 @@ AS
                 ResultUnion.RC28_05
          FROM
          (
+			 --EPT ligadas a facturas PUE
              SELECT LTRIM(RTRIM(CC.IDSIPAC)) AS [RF_00], 
                     LTRIM(RTRIM(C.IDRegFiducidiario)) AS [RI_00], 
                     C.NumeroContrato AS [RC11_01], 
@@ -53,7 +52,6 @@ AS
                    AND EPT.FechaCargaSIPAC = @Mes
                    AND R.CvTipoDocFacturacion = 1
                    AND ISNULL(CONVERT(INT, EPT.ProcesadoSIPAC), 0) = 0
-             --AND P.IdPresupuesto = @IdPresupuesto
              GROUP BY LTRIM(RTRIM(CC.IDSIPAC)), 
                       LTRIM(RTRIM(C.IDRegFiducidiario)), 
                       EPT.IdDocFacturacionSIPAC, 
@@ -62,47 +60,7 @@ AS
                       REPLACE(CONCAT(F.IdDocFacturacionSIPAC, '.xml'), '-', '_'), 
                       MONTH(R.MesPresentacion), 
                       YEAR(R.MesPresentacion)
-             --
-             UNION
-             --
-             SELECT LTRIM(RTRIM(CC.IDSIPAC)) AS [RF_00], 
-                    LTRIM(RTRIM(C.IDRegFiducidiario)) AS [RI_00], 
-                    C.NumeroContrato AS [RC11_01], 
-                    EPT.IdDocFacturacionSIPAC AS [RF01_01], 
-                    ISNULL(F.UUID, 'NA') AS [RC28_01], 
-                    REPLACE(CONCAT(F.IdDocFacturacionSIPAC, '.xml'), '-', '_') AS [RC28_02], 
-                    MONTH(R.MesPresentacion) AS [RC28_03], 
-                    YEAR(R.MesPresentacion) AS [RC28_04], 
-                    2 AS [RC28_05]
-             FROM dbo.FI_EstudioPreciosTransfer EPT
-                  JOIN dbo.FI_Factura F ON F.IdEstudioPrecioTransfer = EPT.IdEstudioPrecioTransfer
-                  JOIN dbo.FI_CPDocRelacionado CPDR ON F.UUID = CPDR.IdDocumento
-                  JOIN dbo.FI_ComplementoDePago CP ON CP.IdComplementoDePago = CPDR.IdComplementoDePago
-                  JOIN dbo.FI_Factura FCP ON FCP.IdFactura = CP.IdFactura
-                  JOIN dbo.CO_Contrato C ON C.IdContrato = EPT.IdContrato
-                  JOIN dbo.CO_Contratista CC ON CC.IdContratista = C.IdContratista
-                  JOIN dbo.CO_Registro R ON R.IdFactura = F.IdFactura
-                  JOIN dbo.CO_LineaPresupuestoMes L ON L.IdLineaPresupuestoMes = R.IdPrograma
-                  JOIN dbo.CO_Presupuesto P ON P.IdPresupuesto = L.IdPresupuesto
-                  JOIN dbo.FI_TransferFactura TF ON TF.IdFactura = FCP.IdFactura
-                  JOIN dbo.FI_Transfer T ON T.IdTransferencia = TF.IdTransfer
-                                            AND FCP.IdContrato = T.IdContrato
-                                            AND F.IdDocFacturacionSIPAC IS NOT NULL
-             WHERE C.IdContrato = @Contrato
-                   AND R.IdEstado = 10004
-                   AND EPT.FechaCargaSIPAC = @Mes
-                   AND R.CvTipoDocFacturacion = 1
-                   AND ISNULL(CONVERT(INT, EPT.ProcesadoSIPAC), 0) = 0
-             --AND P.IdPresupuesto = @IdPresupuesto
-             GROUP BY LTRIM(RTRIM(CC.IDSIPAC)), 
-                      LTRIM(RTRIM(C.IDRegFiducidiario)), 
-                      EPT.IdDocFacturacionSIPAC, 
-                      C.NumeroContrato, 
-                      ISNULL(F.UUID, 'NA'), 
-                      REPLACE(CONCAT(F.IdDocFacturacionSIPAC, '.xml'), '-', '_'), 
-                      MONTH(R.MesPresentacion), 
-                      YEAR(R.MesPresentacion)
-             --
+             --EPT ligado a Pedimentos de Importación o Complemento de Proveedor Extranjero
              UNION
              --
              SELECT LTRIM(RTRIM(CC.IDSIPAC)) AS [RF_00], 
@@ -138,7 +96,6 @@ AS
                    AND EPT.FechaCargaSIPAC = @Mes
                    AND R.CvTipoDocFacturacion IN(2, 3)
                   AND ISNULL(CONVERT(INT, EPT.ProcesadoSIPAC), 0) = 0
-             --AND P.IdPresupuesto = @IdPresupuesto
              GROUP BY LTRIM(RTRIM(CC.IDSIPAC)), 
                       LTRIM(RTRIM(C.IDRegFiducidiario)), 
                       EPT.IdDocFacturacionSIPAC, 
@@ -155,7 +112,7 @@ AS
                       END, 
                       MONTH(R.MesPresentacion), 
                       YEAR(R.MesPresentacion)
-             --
+             --EPT ligado al Complemento de Pago de la factura PPD principal relacionado al gasto
              UNION
              --
              SELECT LTRIM(RTRIM(CC.IDSIPAC)) AS [RF_00], 
