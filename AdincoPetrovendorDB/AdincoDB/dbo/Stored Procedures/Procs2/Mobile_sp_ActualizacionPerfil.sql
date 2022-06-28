f024a14fcbca50a682e042c82bcd1a0cbe99ca11
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2022-06-28 13:33:06 -0500

    Se agrega sp Mobile_sp_ActualizacionPerfil

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/Mobile_sp_ActualizacionPerfil.sql
@@ -0,0 +1,39 @@
+﻿USE [Adinco]
+GO
+
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'Mobile_sp_ActualizacionPerfil'
+)
+    DROP PROCEDURE Mobile_sp_ActualizacionPerfil;
+
+/****** Object:  StoredProcedure [dbo].[sp_CentroCostoFiltro_Grd]    Script Date: 13/07/2021 01:17:22 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE proc [dbo].[Mobile_sp_ActualizacionPerfil]
+@idusuario INT,
+@nombre VARCHAR(1000),
+@telefono VARCHAR(100),
+@tfa BIT 
+AS
+BEGIN
+
+	UPDATE AP_Usuario
+	SET Nombre = @nombre,	
+	NumeroCelular = @telefono,
+	TFAuthentication = @tfa,
+	ModificadoEl =GETDATE(),
+	ModificadoPor = @idusuario
+	WHERE UsuarioID = @idusuario
+
+	SELECT *
+	from AP_Usuario
+	WHERE UsuarioID = @idusuario
+			
+END
+
