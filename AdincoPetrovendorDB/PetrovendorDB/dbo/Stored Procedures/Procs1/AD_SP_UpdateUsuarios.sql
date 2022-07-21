99ca0b1d0a685f21d540ecd61a5139649d5bf5ec
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2022-07-21 11:21:39 -0500

    agregado de condiciones para el eliminado del usuario

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/AD_SP_UpdateUsuarios.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/AD_SP_UpdateUsuarios.sql
@@ -1,10 +1,16 @@
-﻿
-CREATE procedure [dbo].[AD_SP_UpdateUsuarios]
+﻿USE [Petrovendor]
+GO
+/****** Object:  StoredProcedure [dbo].[AD_SP_UpdateUsuarios]    Script Date: 21/07/2022 11:00:26 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER procedure [dbo].[AD_SP_UpdateUsuarios]
 	@IdUsuario INT,
 	@Correo NVARCHAR(max),
 	@Activo BIT,
 	@IdTipoUsuario INT,
-	@IsEliminado BIT,
 	@IdUsuarioADINCO INT = NULL,
 	@Nombre NVARCHAR(MAX),
 	@IdUsuarioProveedor INT,
@@ -17,9 +23,25 @@ BEGIN
 			Activo = @Activo,
 			Nombre = @Nombre,
 			IdTipoUsuario = @IdTipoUsuario,
-			IsEliminado = @IsEliminado,
 			IdUsuarioADINCO = @IdUsuarioADINCO,
 			CorreoVerificado = @CorreoVerificado
 		WHERE IdUsuario = @IdUsuario
+
+	IF @Activo = 0
+	BEGIN
+		
+		UPDATE dbo.S_Usuario
+		SET IsEliminado = 1
+		WHERE IdUsuario = @IdUsuario
+		
+	END
+	ELSE
+	BEGIN
+		UPDATE dbo.S_Usuario
+		SET IsEliminado = 0
+		WHERE IdUsuario = @IdUsuario
+	END
+	
+
 END
 
