deba1d8d2623bc03b10976ff6411b40dc2259e48
Pedro Pouchoulen <pedro.pouchoulen@adinco.mx>
2022-08-12 11:51:43 -0500

    se agrega no lock faltante

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/p_OT_ConsultarProgramaAdjunto.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/p_OT_ConsultarProgramaAdjunto.sql
@@ -12,7 +12,7 @@ begin
            UUIDAmazon,
            Bucket
     from OT_ProgramaAdjunto pa (NOLOCK)
-        inner join AWS_Documentos d
+        inner join AWS_Documentos d (NOLOCK)
             on pa.AWSDocumentoId = d.AWSDocumentoId
     where IdOTSolicitud = @pIdOTSolicitud
 end
