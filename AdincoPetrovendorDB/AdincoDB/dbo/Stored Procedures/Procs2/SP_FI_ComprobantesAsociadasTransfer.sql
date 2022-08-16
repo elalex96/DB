01a0e801f25df8171e7a24b26d9608149dc38c96
Neri del Angel <48134542+MarcosGarcia93@users.noreply.github.com>
2022-08-16 17:27:06 -0500

    Issue 2162 | Ajuste de sps relacionados a la pantalla de MisTranferencias-aspx

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_ComprobantesAsociadasTransfer.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_ComprobantesAsociadasTransfer.sql
@@ -3,70 +3,71 @@
 -- Create date: 14-09-2017
 -- Description:	
 -- =============================================
+-- Modificado Por:	Neri Garcia
+-- Fecha:			16 de Agosto del 2022
+-- Descripción:		Eliminación de código comentado, agregado de (NOLOCK), ajustado de orden en los join
+-- =============================================
 CREATE PROCEDURE [dbo].[SP_FI_ComprobantesAsociadasTransfer] 
--- Add the parameters for the stored procedure here
-@IdTran     INT, 
-@IdUsuario  INT, 
-@IdContrato INT
+    @IdTran INT,
+    @IdUsuario INT,
+    @IdContrato INT
 AS
-     BEGIN
-         -- SET NOCOUNT ON added to prevent extra result sets from
-         -- interfering with SELECT statements.
-         SET NOCOUNT ON;
-
-/*DECLARE @CONTRATISTA INT;
-             SELECT @CONTRATISTA = C.IdContratista
-             FROM FI_Transfer T
-                  JOIN CO_Contrato C ON T.IdContrato = C.IdContrato
-                  JOIN CO_Contratista CC ON C.IdContratista = CC.IdContratista
-             WHERE T.IdTransferencia = @IdTran;*/
-
-         --SELECT @CONTRATISTA
-         -- Insert statements for procedure here
-         SELECT PC.IdPedimentoComprobante AS IdComprobante, 
-                PC.FolioComprobante, 
-                PC.FechaPago, 
-                SE.RazonSocial AS Exportador, 
-                PCD.NumeroSerieMercancia, 
-                PCD.ClaseBienServicio, 
-                MU.UMB+' - '+MU.Unidad AS UnidadMedida, 
-                TM.TipoMonedaCorto, 
-                SUM(CASE
-                        WHEN PCD.ImporteTotal IS NOT NULL
-                        THEN PCD.ImporteTotal
-                        ELSE PCD.PrecioUnitario
-                    END) AS 'PrecioUnitario', --SUBTOTAL
-                PCD.Cantidad, 
-                SUM(CASE
-                        WHEN PCD.ImporteTotal IS NOT NULL
-                        THEN PCD.ImporteTotal
-                        ELSE PCD.PrecioUnitario
-                    END) AS 'ImporteTotal', 
-                L.Nombre AS FormaDePago, 
-                TF.MontoPagado
-         FROM FI_Transfer T
-              JOIN FI_TransferFactura TF ON T.IdTransferencia = TF.IdTransfer
-              JOIN FI_PedimentoComprobante PC ON TF.IdPedimentoComprobante = PC.IdPedimentoComprobante
-              LEFT JOIN FI_PedimentoComprobanteDetalle AS PCD ON PC.IdPedimentoComprobante = PCD.IdPedimentoComprobante
-              LEFT JOIN PV_Subcontratista SE ON PC.IdSubcontratistaExportador = SE.IdSubcontratista
-              LEFT JOIN PV_TipoMoneda TM ON PC.IdMoneda = TM.IdMoneda
-              LEFT JOIN AP_Lista L ON PC.IdFormaPago = L.IdClave
-                                      AND IdGrupo = 10001
-              LEFT JOIN PV_MM_MaterialUnidad MU ON PCD.IdUnidadMedida = MU.IdUnidad
-         --LEFT JOIN CO_Contrato C ON PC.IdContrato = C.IdContrato
-         WHERE T.IdTransferencia = @IdTran   --21682 
-               AND PC.CvTipoDocFacturacion = 3
-               AND T.IdContrato = @IdContrato   --10016--TM01
-         GROUP BY PC.IdPedimentoComprobante, 
-                  PC.FolioComprobante, 
-                  PC.FechaPago, 
-                  SE.RazonSocial, 
-                  PCD.NumeroSerieMercancia, 
-                  PCD.ClaseBienServicio, 
-                  MU.UMB+' - '+MU.Unidad, 
-                  TM.TipoMonedaCorto, 
-                  PCD.Cantidad, 
-                  L.Nombre, 
-                  TF.MontoPagado; --AND C.IdContratista = @CONTRATISTA
-         --SP_FI_ComprobantesAsociadasTransfer 686,1,3
-     END;
+BEGIN
+    SET NOCOUNT ON;
+    --
+    SELECT FI_PedimentoComprobante.IdPedimentoComprobante AS IdComprobante,
+           FI_PedimentoComprobante.FolioComprobante,
+           FI_PedimentoComprobante.FechaPago,
+           PV_Subcontratista.RazonSocial AS Exportador,
+           FI_PedimentoComprobanteDetalle.NumeroSerieMercancia,
+           FI_PedimentoComprobanteDetalle.ClaseBienServicio,
+           PV_MM_MaterialUnidad.UMB + ' - ' + PV_MM_MaterialUnidad.Unidad AS UnidadMedida,
+           PV_TipoMoneda.TipoMonedaCorto,
+           SUM(   CASE
+                      WHEN FI_PedimentoComprobanteDetalle.ImporteTotal IS NOT NULL THEN
+                          FI_PedimentoComprobanteDetalle.ImporteTotal
+                      ELSE
+                          FI_PedimentoComprobanteDetalle.PrecioUnitario
+                  END
+              ) AS 'PrecioUnitario',
+           FI_PedimentoComprobanteDetalle.Cantidad,
+           SUM(   CASE
+                      WHEN FI_PedimentoComprobanteDetalle.ImporteTotal IS NOT NULL THEN
+                          FI_PedimentoComprobanteDetalle.ImporteTotal
+                      ELSE
+                          FI_PedimentoComprobanteDetalle.PrecioUnitario
+                  END
+              ) AS 'ImporteTotal',
+           AP_Lista.Nombre AS FormaDePago,
+           FI_TransferFactura.MontoPagado
+    FROM FI_Transfer (NOLOCK)
+        JOIN FI_TransferFactura (NOLOCK)
+            ON FI_Transfer.IdTransferencia = @IdTran
+               AND FI_Transfer.IdContrato = @IdContrato
+               AND FI_Transfer.IdTransferencia = FI_TransferFactura.IdTransfer
+        JOIN FI_PedimentoComprobante (NOLOCK)
+            ON FI_PedimentoComprobante.CvTipoDocFacturacion = 3
+               AND FI_TransferFactura.IdPedimentoComprobante = FI_PedimentoComprobante.IdPedimentoComprobante
+        LEFT JOIN FI_PedimentoComprobanteDetalle (NOLOCK)
+            ON FI_PedimentoComprobante.IdPedimentoComprobante = FI_PedimentoComprobanteDetalle.IdPedimentoComprobante
+        LEFT JOIN PV_Subcontratista (NOLOCK)
+            ON FI_PedimentoComprobante.IdSubcontratistaExportador = PV_Subcontratista.IdSubcontratista
+        LEFT JOIN PV_TipoMoneda (NOLOCK)
+            ON FI_PedimentoComprobante.IdMoneda = PV_TipoMoneda.IdMoneda
+        LEFT JOIN AP_Lista (NOLOCK)
+            ON AP_Lista.IdGrupo = 10001
+               AND FI_PedimentoComprobante.IdFormaPago = AP_Lista.IdClave
+        LEFT JOIN PV_MM_MaterialUnidad (NOLOCK)
+            ON FI_PedimentoComprobanteDetalle.IdUnidadMedida = PV_MM_MaterialUnidad.IdUnidad
+    GROUP BY FI_PedimentoComprobante.IdPedimentoComprobante,
+             FI_PedimentoComprobante.FolioComprobante,
+             FI_PedimentoComprobante.FechaPago,
+             PV_Subcontratista.RazonSocial,
+             FI_PedimentoComprobanteDetalle.NumeroSerieMercancia,
+             FI_PedimentoComprobanteDetalle.ClaseBienServicio,
+             PV_MM_MaterialUnidad.UMB + ' - ' + PV_MM_MaterialUnidad.Unidad,
+             PV_TipoMoneda.TipoMonedaCorto,
+             FI_PedimentoComprobanteDetalle.Cantidad,
+             AP_Lista.Nombre,
+             FI_TransferFactura.MontoPagado;
+END;
