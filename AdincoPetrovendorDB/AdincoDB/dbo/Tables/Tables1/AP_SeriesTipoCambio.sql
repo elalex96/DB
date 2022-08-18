9ef60f8f62f4e361be450faee213a1207eb1222e
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2022-08-18 11:50:36 -0500

    CAMBIOS PARA NUEVOS SPS Y TABLAS SIN CONFLICTOS DE ELIMINACIONES

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/AP_SeriesTipoCambio.sql
@@ -0,0 +1,15 @@
+﻿	CREATE TABLE AP_SeriesTipoCambio(
+	Id INT IDENTITY(1,1),
+	IdMoneda INT,
+	Pediodicidad VARCHAR(150),
+	SerieBanxico VARCHAR(150),
+	Tipo VARCHAR(150),
+	Descripcion VARCHAR(300),
+	CreadoPor INT,
+	CreadoEl DATETIME
+	CONSTRAINT PK_SeriesTipoCambio PRIMARY KEY (Id),
+	CONSTRAINT AP_SeriesTipoCambioPV_TipoMoneda FOREIGN KEY (IdMoneda)
+	REFERENCES PV_TipoMoneda(IdMoneda),
+	CONSTRAINT AP_SeriesTipoCambioCreadoPor FOREIGN KEY (CreadoPor)
+	REFERENCES AP_Usuario(UsuarioID)
+	)
