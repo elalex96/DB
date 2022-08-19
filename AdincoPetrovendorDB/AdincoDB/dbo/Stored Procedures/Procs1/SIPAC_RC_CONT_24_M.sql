7f0930fcddaaffaa30817a6aac3053977ceaa3b0
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2022-08-19 12:25:10 -0500

    MODI

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SIPAC_RC_CONT_24_M.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SIPAC_RC_CONT_24_M.sql
@@ -19,7 +19,7 @@ GO
 -- Fecha Modificado: 2022-08-18
 -- Description:      SE MODIFICA LA CONSULTA POR DEUDA TECNICA, SE MODIFICA LOS JOINS Y LEFT JOIS DE UBICACIÓN, SE QUITAN ALGUNOS ALIAS
 -- =============================================
-ALTER PROCEDURE [dbo].[SIPAC_RC_CONT_24_M] 
+CREATE PROCEDURE [dbo].[SIPAC_RC_CONT_24_M] 
     @Contrato      INT,
     @Mes           DATE,
     @IdPresupuesto INT          = 0,
