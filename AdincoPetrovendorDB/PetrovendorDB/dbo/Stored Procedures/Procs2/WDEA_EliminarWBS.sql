2ac1d889a7810ef78343edb288c72d80ec182c93
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2022-08-31 16:45:52 -0500

    WDEA_EliminarWBS

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/WDEA_EliminarWBS.sql
@@ -0,0 +1,34 @@
+use petrovendor
+go
+DROP PROCEDURE IF EXISTS WDEA_EliminarWBS
+GO
+CREATE PROC WDEA_EliminarWBS
+@Id Int,
+@IdUsuario int,
+@IdContrato int,
+@Desactivar varchar(300)
+AS
+BEGIN
+IF @Desactivar = 'WBS'
+BEGIN
+	Update WDEA_WBS
+	set Activo = 0,
+	ModificadoPor = @IdUsuario,
+	ModificadoEl = GETDATE()
+	where Id = @Id
+
+	UPDATE WDEA_WBSLineaPresupuesto
+	SET Activo = 0,
+	ModificadoPor = @IdUsuario,
+	ModificadoEl = GETDATE()
+	WHERE IdWBS = @Id
+END
+IF @Desactivar = 'WBSLINEAPRESUPUESTO'
+BEGIN
+	UPDATE WDEA_WBSLineaPresupuesto
+	SET Activo = 0,
+	ModificadoPor = @IdUsuario,
+	ModificadoEl = GETDATE()
+	WHERE Id = @Id
+END
+END
