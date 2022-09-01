369dccefc48e1275519cbda6cd92b7d833a84198
Neri del Angel <48134542+MarcosGarcia93@users.noreply.github.com>
2022-08-31 21:57:37 -0500

    Issue 2203 | Agregado de nuevo sp

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_CO_EsDocumentoFacturacionDePetrovendor.sql
@@ -0,0 +1,30 @@
+﻿CREATE PROCEDURE [dbo].[SP_CO_EsDocumentoFacturacionDePetrovendor]
+    @IdContrato INT,
+    @IdUsuario INT,
+    @Id INT,
+    @EsFactura BIT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SET LANGUAGE spanish;
+
+    DECLARE @EsDePetrovendor BIT = 0;
+
+    IF (@EsFactura = 1)
+    BEGIN
+        SELECT @EsDePetrovendor = ISNULL(FI_FacturaAdincoPetrovendor.IdFacturaAdinco, 0)
+        FROM FI_FacturaAdincoPetrovendor
+        WHERE FI_FacturaAdincoPetrovendor.Activo = 1
+              AND FI_FacturaAdincoPetrovendor.IdFacturaAdinco = @Id
+
+    END;
+    ELSE
+    BEGIN
+        SELECT @EsDePetrovendor = ISNULL(FI_PedimentoComprobante.IdPedimentoComprobantePetrovendor, 0)
+        FROM FI_PedimentoComprobante
+        WHERE FI_PedimentoComprobante.IdPedimentoComprobantePetrovendor IS NOT NULL
+              AND FI_PedimentoComprobante.IdPedimentoComprobante = @Id
+    END;
+
+    SELECT @EsDePetrovendor
+END;
