48af44e852ce2fa7268b570d716f28aac1e9bddb
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2022-09-02 14:59:42 -0500

    Update SP_CartaNombresRepresentanteLegal.sql
    No Locks en tablas

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_CartaNombresRepresentanteLegal.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_CartaNombresRepresentanteLegal.sql
@@ -1,9 +1,16 @@
-﻿-- =============================================
+﻿use Petrovendor
+go
+drop procedure if exists SP_CartaNombresRepresentanteLegal
+go
+-- =============================================
 -- Author:		<Pedro Acuña>
 -- Create date: <08/03/2019>
 -- Description:	<Store para retornar los representanes legales que tienen dados de alta en la empresa>
 -- =============================================
-
+-- Author:		Luis David
+-- Create date: <02/09/2022>
+-- Description:	<Se optimiza para el Issue #1986>
+-- =============================================
 CREATE PROCEDURE SP_CartaNombresRepresentanteLegal @IdProveedor INT, @IdTipoRegimen INT
 AS
 	BEGIN
@@ -11,8 +18,8 @@ AS
 			BEGIN
 				SELECT	P.IdProveedor AS IdRepresentanteLegal, P.RazonSocial AS RepLegal
 				  FROM	S_Proveedor AS P
-						JOIN dbo.S_Documento_S3 AS DOC
-							 ON DOC.IdProveedor = P.IdProveedor
+						JOIN dbo.S_Documento_S3 (NOLOCK) AS DOC
+							 ON P.IdProveedor = DOC.IdProveedor
 								AND DOC.Activo = 1
 				 WHERE
 						P.IdProveedor = @IdProveedor
@@ -26,9 +33,9 @@ AS
 				  FROM	dbo.DG_RepresentanteLegal legal
 						INNER JOIN dbo.S_Documento_S3 s3
 								   ON legal.IdDocumento = s3.IdDocumento
+									  AND	legal.IdProveedor = s3.IdProveedor
 									  AND	s3.IdProveedor = @IdProveedor
 									  AND	s3.Activo = 1
-									  AND	legal.IdProveedor = s3.IdProveedor
 				 WHERE
 						legal.IdProveedor = @IdProveedor
 						AND legal.IsActivo = 1
@@ -39,9 +46,9 @@ AS
 				SELECT	TOP 1
 						U.IdUsuario AS IdRepresentanteLegal, U.Nombre AS RepLegal
 				  FROM	S_Proveedor AS P
-						JOIN S_UsuarioProveedor UP
+						JOIN S_UsuarioProveedor (NOLOCK) UP
 							 ON P.IdProveedor = UP.IdProveedor
-						JOIN S_Usuario U
+						JOIN S_Usuario (NOLOCK) U
 							 ON UP.IdUsuario = U.IdUsuario
 				 WHERE
 						U.IdTipoUsuario = 3
