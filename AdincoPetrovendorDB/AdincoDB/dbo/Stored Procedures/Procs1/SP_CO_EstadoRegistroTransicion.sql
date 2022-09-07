8da354ecbc0eda91a14213855a2fc9e19834a250
Neri del Angel <48134542+MarcosGarcia93@users.noreply.github.com>
2022-09-07 13:05:55 -0500

    Issue 2205 | Ajustes en sps de pantallas y vistas solicitadas

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SP_CO_EstadoRegistroTransicion.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SP_CO_EstadoRegistroTransicion.sql
@@ -1,37 +1,51 @@
-﻿-- =============================================
+﻿-- ========================================================================
 -- Author:		Manuel CD
 -- Create date: 27-09-17
 -- Description:	
--- =============================================
+-- ========================================================================
+-- Modificado Por:		Neri del Angel
+-- Fecha Modificación:	06 de Septiembre del 2022
+-- Descripción:			Se agrega validacion del nuevo campo idestadopemex,
+--						de la tabla [CO_RegistroMarkup]
+--						en vez de la tabla CO_Registro.IdEstado
+-- ========================================================================
 CREATE PROCEDURE [dbo].[SP_CO_EstadoRegistroTransicion] 
-	-- Add the parameters for the stored procedure here
-@IdUsuario        INT,
-@EstadoRegistroID INT,
-@IdContrato INT
+    @IdUsuario INT,
+    @EstadoRegistroID INT,
+    @IdContrato INT
 AS
-     BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-         SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
-
-         SELECT --R.IdRegistro,
-         ERT.IdEstadoDestino,
-         ERT.IdEstadoOrigen,
-         ER.NombreEstado AS EstadoActual,
-         ERT.Descripcion,
-         ERD.NombreEstado AS CambiarA
-         FROM CO_EstadoRegistroTransicion ERT
-              JOIN CO_EstadoRegistroUsuario ERU ON ERT.IdEstadoOrigen = ERU.IdClvEstado
-              JOIN CO_EstadoRegistro_V2 ER ON ERT.IdEstadoOrigen = ER.IdClvEstado
-              JOIN CO_EstadoRegistro_V2 ERD ON ERT.IdEstadoDestino = ERD.IdClvEstado
-              JOIN CO_Registro R ON ERT.IdEstadoOrigen = R.IdEstado
-         WHERE ERU.IdUsuario = @IdUsuario
-               AND R.IdRegistro = @EstadoRegistroID
-			AND ER.IdContrato = @IdContrato
-			AND ERD.IdContrato = @IdContrato
-     END;
-	--SP_CO_EstadoRegistroTransicion 2, 7107
-	--SP_CO_EstadoRegistroTransicion 10002, 1631, 3
-
+BEGIN
+    SET NOCOUNT ON;  
+	DECLARE @IdEstadoBase INT;
+	/**/
+    IF NOT EXISTS
+    (
+        SELECT *
+        FROM CO_RegistroMarkup
+        WHERE GastoId = @EstadoRegistroID
+              AND IdEstadoPemex IS NOT NULL
+    )
+    BEGIN
+      SELECT TOP 1 @IdEstadoBase = CO_EstadoRegistro_V2.IdClvEstado FROM  CO_EstadoRegistro_V2 WHERE IdContrato = @IdContrato AND Descripcion= 'En revisión' AND NombreEstado = 'Revisión'
+     UPDATE CO_RegistroMarkup SET IdEstadoPemex = @IdEstadoBase  WHERE GastoId = @EstadoRegistroID
+    END;
+   
+     SELECT CO_EstadoRegistroTransicion.IdEstadoDestino,
+               CO_EstadoRegistroTransicion.IdEstadoOrigen,
+               CO_EstadoRegistro_V2.NombreEstado AS EstadoActual,
+               CO_EstadoRegistroTransicion.Descripcion,
+               CO_EstadoRegistro_V2_Destino.NombreEstado AS CambiarA
+        FROM CO_EstadoRegistroTransicion
+            JOIN CO_EstadoRegistroUsuario
+                ON CO_EstadoRegistroTransicion.IdEstadoOrigen = CO_EstadoRegistroUsuario.IdClvEstado
+            JOIN CO_EstadoRegistro_V2
+                ON CO_EstadoRegistroTransicion.IdEstadoOrigen = CO_EstadoRegistro_V2.IdClvEstado
+            JOIN CO_EstadoRegistro_V2 CO_EstadoRegistro_V2_Destino
+                ON CO_EstadoRegistroTransicion.IdEstadoDestino = CO_EstadoRegistro_V2_Destino.IdClvEstado
+            JOIN CO_RegistroMarkup
+                ON CO_EstadoRegistroTransicion.IdEstadoOrigen = CO_RegistroMarkup.IdEstadoPemex
+        WHERE CO_EstadoRegistroUsuario.IdUsuario = @IdUsuario
+              AND CO_RegistroMarkup.GastoId = @EstadoRegistroID
+              AND CO_EstadoRegistro_V2.IdContrato = @IdContrato
+              AND CO_EstadoRegistro_V2_Destino.IdContrato = @IdContrato
+END;
