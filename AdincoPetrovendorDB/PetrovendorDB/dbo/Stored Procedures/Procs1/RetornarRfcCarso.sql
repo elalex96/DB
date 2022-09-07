d3e77d5045a09bb544443ea1bbe9d1e9fd7ae589
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2022-09-07 14:52:48 -0500

    Update RetornarRfcCarso.sql
    uso de nolocks y acomodo de tablas

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/RetornarRfcCarso.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/RetornarRfcCarso.sql
@@ -1,14 +1,18 @@
-﻿CREATE PROCEDURE RetornarRfcCarso
+USE Petrovendor
+GO
+DROP PROCEDURE IF EXISTS RetornarRfcCarso
+GO
+-- =============================================
+-- Author: Luis David
+-- Create date: 06/09/2022
+-- Description: Modificación de optimización Issue #1985 (Petrovendor)
+--===============================================
+CREATE PROCEDURE RetornarRfcCarso
 AS
 BEGIN
     SELECT prov.RFC
     FROM dbo.AX_ComparativaEmpresa comp
-        INNER JOIN dbo.S_Proveedor prov
-            ON prov.IdProveedor = comp.IdProveedor
-			AND prov.Activo = 1
+    INNER JOIN dbo.S_Proveedor (NOLOCK) prov
+            ON comp.IdProveedor = prov.IdProveedor
+			AND 1 = prov.Activo
 END
-
-
-
-
-
