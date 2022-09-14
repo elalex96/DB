a3bc0edd83dedbaff1cc62ec85c6f1f51f9ab815
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2022-09-14 17:21:10 -0500

    Se suben sps´s relacionados a las notificaciones del defautl en petrovendor

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Tables/APP_NotificacionesDefault.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE APP_NotificacionesDefault
+(
+Id INT PRIMARY KEY IDENTITY(1,1),
+ContratoId INT,
+Titulo NVARCHAR(200),
+Mensaje NVARCHAR(MAX),
+CreadoEl DATETIME,
+ModificadoEl DATETIME,
+FechaInicio  DATETIME,
+FechaFinalizacion  DATETIME,
+Activo BIT)
+GO
+CREATE NONCLUSTERED INDEX idx_APP_NotificacionesDefault_ContratoId
+    ON [dbo].[APP_NotificacionesDefault](ContratoId ASC)
+    INCLUDE(ContratoId) WITH (STATISTICS_NORECOMPUTE = ON);
