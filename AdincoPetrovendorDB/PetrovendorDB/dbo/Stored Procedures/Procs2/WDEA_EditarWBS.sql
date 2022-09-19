a87342149fcfc0b0d6ce65cca0034d2ec3a9fc9d
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2022-09-19 10:05:29 -0500

    Update WDEA_EditarWBS.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/WDEA_EditarWBS.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/WDEA_EditarWBS.sql
@@ -1,17 +1,38 @@
-USE Petrovendor
+USE [Petrovendor]
 GO
-DROP PROCEDURE IF EXISTS WDEA_EditarWBS
+/****** Object:  StoredProcedure [dbo].[WDEA_EditarWBS]    Script Date: 19/09/2022 12:21:34 a. m. ******/
+SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE WDEA_EditarWBS
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[WDEA_EditarWBS]
 @WBS varchar(300),
 @IdContrato int,
 @IdUsuario int,
 @Id int
 AS
 BEGIN
-	Update WDEA_WBS
-	set ModificadoEl = GETDATE(),
-	ModificadoPor = @IdUsuario,
-	WBS = @WBS
-	where Id = @Id
+
+	IF exists (SELECT 1
+	FROM petrovendor..WDEA_WBS
+	WHERE 
+	idcontrato = @IdContrato 
+	and LTRIM(RTRIM(WBS)) = LTRIM(RTRIM(@WBS))
+	and activo = 1) 
+	BEGIN
+
+		SELECT 'YA_EXISTE' as Error
+
+	END
+	ELSE
+	BEGIN
+
+		Update WDEA_WBS
+		set ModificadoEl = GETDATE(),
+		ModificadoPor = @IdUsuario,
+		WBS = @WBS
+		where Id = @Id
+
+	END
+	
 END
