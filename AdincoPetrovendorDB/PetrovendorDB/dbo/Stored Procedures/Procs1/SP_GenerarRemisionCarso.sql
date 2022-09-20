c635f6b02e7185eaf6c0cf75ba19a55b486b5da7
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2022-09-20 18:02:32 -0500

    Se agrega join para evitar generar aceptaciones de pedidos eliminados

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_GenerarRemisionCarso.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_GenerarRemisionCarso.sql
@@ -1,8 +1,14 @@
-﻿USE [Petrovendor]
+﻿
+USE Petrovendor
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_GenerarRemisionCarso'
+)
+    DROP PROCEDURE SP_GenerarRemisionCarso;
 GO
-DROP PROCEDURE IF EXISTS SP_GenerarRemisionCarso
-GO
-/****** Object:  StoredProcedure [dbo].[SP_GenerarRemisionCarso]    Script Date: 12/05/2022 07:45:12 a. m. ******/
+/****** Object:  StoredProcedure [dbo].[SP_GenerarRemisionCarso]    Script Date: 20/09/2022 03:40:18 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -121,6 +127,7 @@ BEGIN --EMPIEZA STORE
             ON sp.IdSolicitudPedido = p.IdSolicitudPedido 
                AND  O.NoVersion = p.Version 
                AND  pod.IdPeticionOferta = p.IdPeticionOferta
+			   AND P.IdPedido= R.IdPedido
     WHERE UPPER(r.RECID) = UPPER(@RecId)
           AND r.IdPedido = @IdPedido
           AND UPPER(r.IdOC) = UPPER(@IdOC)
