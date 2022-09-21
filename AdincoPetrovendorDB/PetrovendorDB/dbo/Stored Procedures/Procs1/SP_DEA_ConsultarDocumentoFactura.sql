5a867169ddad7cc19f0d6ee7bb17153418eef82d
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2022-09-21 10:47:57 -0500

    Update SP_DEA_ConsultarDocumentoFactura.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DEA_ConsultarDocumentoFactura.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DEA_ConsultarDocumentoFactura.sql
@@ -1,13 +1,6 @@
-﻿USE [Petrovendor]
-IF EXISTS
-(
-    SELECT 1
-    FROM dbo.sysobjects
-    WHERE name = 'SP_DEA_ConsultarDocumentoFactura'
-)
-    DROP PROCEDURE SP_DEA_ConsultarDocumentoFactura;
+USE [Petrovendor]
 GO
-/****** Object:  StoredProcedure [dbo].[SP_DEA_ConsultarDocumentoFactura]    Script Date: 27/04/2022 09:57:19 a. m. ******/
+/****** Object:  StoredProcedure [dbo].[SP_DEA_ConsultarDocumentoFactura]    Script Date: 21/09/2022 10:43:04 a. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -17,7 +10,11 @@ GO
 -- Create date: 27-04-2022
 -- Description:	Issue #1739  Optimizacion pantallas se ordena y revisa joins 
 -- =============================================
-CREATE PROCEDURE [dbo].[SP_DEA_ConsultarDocumentoFactura]
+-- Author:		Alexander Gomez
+-- Create date: 21/09/2022
+-- Description:	verificar si lo que se consulta es un comprobante
+-- =============================================
+ALTER PROCEDURE [dbo].[SP_DEA_ConsultarDocumentoFactura]
 	-- Add the parameters for the stored procedure here
 @IdAceptacionPedido int,
 @IdDocumento int
@@ -26,30 +23,52 @@ AS
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
          SET NOCOUNT ON;
-		DECLARE @ArchivoPDF NVARCHAR(MAX)  
-		DECLARE @ArchivoXML NVARCHAR(MAX) 
+		DECLARE @ArchivoPDF NVARCHAR(MAX);  
+		DECLARE @ArchivoXML NVARCHAR(MAX);
+		DECLARE @IDPEDIMENTO INT;
 		 
 		  
 		 IF @IdDocumento = 17 ---PDF
 		 BEGIN 
+			--VERIFICAR SI ES UN PEDIMENTO COMPROBANTE
+			IF EXISTS (SELECT * FROM FI_AceptacionPedido_PedimentoComprobante WHERE IdAceptacionPedido = @IdAceptacionPedido AND Activo = 1)
+			BEGIN
 
-		 SET @ArchivoPDF   = ( SELECT F.ArchivoPDF
+				SET @IDPEDIMENTO = (SELECT TOP 1 IdPedimentoComprobante FROM FI_AceptacionPedido_PedimentoComprobante WHERE IdAceptacionPedido = @IdAceptacionPedido AND Activo = 1 ORDER BY CreadoEl DESC );	
+
+				SELECT 
+					DocumentoByte,
+					NombreExtensionArchivo,
+					'.pdf',
+					'',
+					'application/pdf',
+					'' AS ArchivoPDF
+				FROM dbo.FI_Documento 
+				WHERE IdPedimentoComprobante = @IDPEDIMENTO;
+
+			END
+			ELSE
+			BEGIN 
+
+				SET @ArchivoPDF   = ( SELECT F.ArchivoPDF
 							FROM FI_Factura AS F
 							JOIN MM_AceptacionFactura AS AF ON AF.IdFactura = F.IdFactura
 		   					WHERE AF.IdAceptacionPedido =@IdAceptacionPedido)
 
 		   
 
-		  SELECT (SELECT F.ComprobantePDFByte
-				FROM FI_Factura AS F
-				INNER JOIN MM_AceptacionFactura AS AF ON AF.IdFactura = F.IdFactura
-		   		WHERE AF.IdAceptacionPedido =@IdAceptacionPedido),
-				TD.NombreTipoDocumento+'.pdf',
-				'.pdf','', 
-				'application/pdf',
-				ISNULL(@ArchivoPDF, '')  AS ArchivoPDF --> HISTORICO YA QUE LA FACTURA ANTES SE GUARDABA COMO STRING
-		  FROM S_TipoDocumento AS TD
-		  WHERE TD.IdTipoDocumento = 17 ---> DOCUMENTO DE TIPO PDF DE S_TIPODOCUMENTO
+			  SELECT (SELECT F.ComprobantePDFByte
+					FROM FI_Factura AS F
+					INNER JOIN MM_AceptacionFactura AS AF ON AF.IdFactura = F.IdFactura
+		   			WHERE AF.IdAceptacionPedido =@IdAceptacionPedido),
+					TD.NombreTipoDocumento+'.pdf',
+					'.pdf','', 
+					'application/pdf',
+					ISNULL(@ArchivoPDF, '')  AS ArchivoPDF --> HISTORICO YA QUE LA FACTURA ANTES SE GUARDABA COMO STRING
+			  FROM S_TipoDocumento AS TD
+			  WHERE TD.IdTipoDocumento = 17 ---> DOCUMENTO DE TIPO PDF DE S_TIPODOCUMENTO
+
+			END
 		
 		 END 
 
