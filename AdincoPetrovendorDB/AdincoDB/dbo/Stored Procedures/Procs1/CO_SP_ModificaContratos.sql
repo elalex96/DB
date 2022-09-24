dc1586ae917957bb7817d9bec4a7f7c4e5c64cf6
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2022-09-23 23:04:56 -0500

    nuevos sps

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/CO_SP_ModificaContratos.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Reyna Olvera
+-- Create date: 10/10/2017
+-- Description:	Para AppMovil extrae DescripcionAprobacion
+-- =============================================
+CREATE PROCEDURE AM_ExtraeDescripcion
+	-- Add the parameters for the stored procedure here
+	@idUser int,
+	@idEstatus int,
+	@idTipoAprobacion int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+
+ Select descripcion from AM_Aprobacion
+  where idUsuario =@idUser and IdStatusAprobacionM = @idEstatus and idtipoAprobacion=@idTipoAprobacion
+END
+
