9b3c493374a5cba55e2cbec2e6f620d0cd383100
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2022-10-04 13:11:27 -0500

    Se agregan mejoras en las validaciones de los WBS

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/WDEA_GuardarWBSLineaPresupuesto.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/WDEA_GuardarWBSLineaPresupuesto.sql
@@ -1,8 +1,18 @@
-USE Petrovendor
+USE [Petrovendor]
 GO
-DROP PROCEDURE IF EXISTS WDEA_GuardarWBSLineaPresupuesto
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'WDEA_GuardarWBSLineaPresupuesto'
+)
+    DROP PROCEDURE WDEA_GuardarWBSLineaPresupuesto;
+/****** Object:  StoredProcedure [dbo].[WDEA_GuardarWBSLineaPresupuesto]    Script Date: 04/10/2022 11:37:38 a. m. ******/
+SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE WDEA_GuardarWBSLineaPresupuesto
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[WDEA_GuardarWBSLineaPresupuesto]
 @IdWBS int,
 @IdLineaPresupuesto int,
 @IdContrato int,
@@ -11,34 +21,69 @@ AS
 BEGIN
 	DECLARE @MENSAJE VARCHAR (300);
 	DECLARE @RESPONSE VARCHAR (300);
+
+	IF (0 = isnull(@IdLineaPresupuesto,0) OR  0=isnull(@IdWBS,0))
+	BEGIN 
+		SET @MENSAJE = 'El WBS y la Línea Presupuesto son requeridos para continuar'
+		SET @RESPONSE = 'ERROR'
+		SELECT @MENSAJE as MENSAJE,@RESPONSE as RESPONSE
+		RETURN
+	END 
+
 	IF EXISTS (SELECT * FROM WDEA_WBSLineaPresupuesto 
-	WHERE IdLineaPresupuesto = @IdLineaPresupuesto and IdContrato = @IdContrato and Activo = 0)
+	WHERE IdLineaPresupuesto = @IdLineaPresupuesto and IdContrato = @IdContrato AND IdWBS=@IdWBS and Activo = 1)
 	BEGIN
+		SET @MENSAJE = 'Esta relación de WBS y la Línea Presupuesto ya se encuentra en el sistema'
+		SET @RESPONSE = 'SUCCESS'
+		SELECT @MENSAJE as MENSAJE,@RESPONSE as RESPONSE
+		RETURN
+	END
+
+	IF EXISTS (SELECT * FROM WDEA_WBSLineaPresupuesto 
+	WHERE IdLineaPresupuesto = @IdLineaPresupuesto AND IdWBS<>@IdWBS and IdContrato = @IdContrato and Activo = 1)
+	BEGIN
+		SET @MENSAJE = 'La Línea Presupuesto ya está relacionada a un WBS'
+		SET @RESPONSE = 'ERROR'
+		SELECT @MENSAJE as MENSAJE,@RESPONSE as RESPONSE
+		RETURN
+	END
+
+	IF EXISTS (SELECT * FROM WDEA_WBSLineaPresupuesto 
+	WHERE IdLineaPresupuesto <> @IdLineaPresupuesto AND IdWBS=@IdWBS and IdContrato = @IdContrato and Activo = 1)
+	BEGIN
+		SET @MENSAJE = 'El WBS ya está relacionada a otra Línea Presupuesto '
+		SET @RESPONSE = 'ERROR'
+		SELECT @MENSAJE as MENSAJE,@RESPONSE as RESPONSE
+		RETURN
+	END
+
+	IF EXISTS (SELECT * FROM WDEA_WBSLineaPresupuesto 
+	WHERE IdLineaPresupuesto = @IdLineaPresupuesto and IdContrato = @IdContrato AND IdWBS=@IdWBS and Activo = 0)
+	BEGIN
+
 			UPDATE WDEA_WBSLineaPresupuesto
-			SET ACTIVO = 1
+			SET ACTIVO = 1,
+			ModificadoEl=GETDATE(),
+			ModificadoPor=@IdUsuario
 			WHERE 
 			IdContrato = @IdContrato
 			AND IdLineaPresupuesto = @IdLineaPresupuesto
+
 		SET @MENSAJE = 'La información se guardó exitosamente'
 		SET @RESPONSE = 'SUCCESS'
+
 	END
 	ELSE
-	BEGIN
-		IF EXISTS (SELECT * FROM WDEA_WBSLineaPresupuesto 
-		WHERE IdLineaPresupuesto = @IdLineaPresupuesto and IdContrato = @IdContrato and Activo = 1)
-		BEGIN
-			SET @MENSAJE = 'La Línea Presupuesto ya está relacionada a un WBS'
-			SET @RESPONSE = 'ERROR'
-		END
-		ELSE
-		BEGIN
-			INSERT INTO WDEA_WBSLineaPresupuesto
-			(IdWBS,	IdLineaPresupuesto,	IdContrato,Activo,CreadoEl,CreadoPor)
-			VALUES 
-			(@IdWBS,@IdLineaPresupuesto,@IdContrato,1,GETDATE(),@IdUsuario)									
-			SET @MENSAJE =  'La información se guardó exitosamente'
-			SET @RESPONSE = 'SUCCESS'
-		END
+	BEGIN		
+
+		INSERT INTO WDEA_WBSLineaPresupuesto
+		(IdWBS,	IdLineaPresupuesto,	IdContrato,Activo,CreadoEl,CreadoPor)
+		VALUES 
+		(@IdWBS,@IdLineaPresupuesto,@IdContrato,1,GETDATE(),@IdUsuario)									
+		SET @MENSAJE =  'La información se guardó exitosamente'
+		SET @RESPONSE = 'SUCCESS'	
+		
 	END
+
 	SELECT @MENSAJE as MENSAJE,@RESPONSE as RESPONSE
 END
