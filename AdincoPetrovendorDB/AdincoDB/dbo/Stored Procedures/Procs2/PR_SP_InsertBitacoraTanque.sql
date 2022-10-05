719e5615284c857eb1f7c9a34873e2d6516d9e36
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2022-10-05 10:43:47 -0500

    nuevos sps

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/PR_SP_InsertBitacoraTanque.sql
@@ -0,0 +1,20 @@
+USE ADINCO;
+GO
+
+CREATE PROCEDURE PR_SP_InsertBitacoraTanque
+@TanqueId	int,
+@UsuarioId	int,
+@Accion VARCHAR(250)
+AS
+BEGIN
+		INSERT INTO PR_TanqueBitacora(Accion,CreadoPor,CreadoEl,TanqueId,Clave,Nombre,Descripcion,Estatus,Estacion,
+					Capacidad,Producto,Diametro,Altura,Constante,PctNoBombeable,VolNoBombeable,PctMaximo,VolMaximo,PorcentajeAgua,
+					ProductoAlmacenado,IdTipoTanque,MedicionManual,PuntoEntregaID,Activo)
+		SELECT @Accion,@UsuarioId,GETDATE(),Id,Clave,Nombre,Descripcion,Estatus,Estacion,
+				Capacidad,Producto,Diametro,Altura,Constante,PctNoBombeable,VolNoBombeable,PctMaximo,VolMaximo,PorcentajeAgua,
+				ProductoAlmacenado,IdTipoTanque,MedicionManual,PuntoEntregaID ,Activo
+			FROM 
+				PR_Tanque 
+			WHERE 
+				Id = @TanqueId;
+END
