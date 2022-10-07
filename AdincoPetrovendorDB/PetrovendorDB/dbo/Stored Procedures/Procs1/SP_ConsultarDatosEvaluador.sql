71f7589fcd9eec3283e545417149b3fdb41e0e44
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2022-10-06 19:30:21 -0500

    -------

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_ConsultarDatosEvaluador.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_ConsultarDatosEvaluador.sql
@@ -7,9 +7,10 @@ IF EXISTS
 )
     DROP PROCEDURE SP_ConsultarDatosEvaluador;
 GO
+
 /****** Object:  StoredProcedure [dbo].[SP_ConsultarDatosEvaluador]    Script Date: 06/10/2022 03:16:03 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROCEDURE [dbo].[SP_ConsultarDatosEvaluador]	@IdOperacion INT,	    @IdContrato    INT = null,    @IdUsuario     INT = null,    @FechaRegistro DATETIME = null	ASBEGIN		SELECT U.Correo, u.IdUsuario, AP.IdProveedor	FROM dbo.MM_AceptacionFactura AF	JOIN dbo.MM_AceptacionPedido AP 		ON  AF.IdAceptacionPedido =AP.IdAceptacionPedido	JOIN dbo.MM_Pedido P 		ON AP.IdPedido = P.IdPedido 	JOIN dbo.TA_Operacion TAO 		ON AF.IdAceptacionFactura = TAO.IdDocumento 		AND IdTipoOperacion= 10 --> CTE Aprobación Factura 	JOIN dbo.S_Usuario U 		ON  P.CreadoPor	 = U.IdUsuario 	WHERE  TAO.IdOperacion = @IdOperacion	AND U.Activo =1 --> CTE Debe estar activo	GROUP BY U.Correo, u.IdUsuario, AP.IdProveedorEND
+CREATE PROCEDURE [dbo].[SP_ConsultarDatosEvaluador]	@IdOperacion INT,	    @IdContrato    INT = null,    @IdUsuario     INT = null,    @FechaRegistro DATETIME = null	ASBEGIN		SELECT U.Correo, u.IdUsuario, AP.IdProveedor	FROM dbo.MM_AceptacionFactura AF	JOIN dbo.MM_AceptacionPedido AP 		ON  AF.IdAceptacionPedido =AP.IdAceptacionPedido	JOIN dbo.MM_Pedido P 		ON AP.IdPedido = P.IdPedido 	JOIN dbo.TA_Operacion TAO 		ON AF.IdAceptacionFactura = TAO.IdDocumento 		AND IdTipoOperacion= 10 --> CTE Aprobación Factura 	JOIN dbo.S_Usuario U 		ON  P.CreadoPor	 = U.IdUsuario 	WHERE  TAO.IdOperacion = @IdOperacion	AND U.Activo =1 --> CTE Debe estar activo	GROUP BY U.Correo, u.IdUsuario, AP.IdProveedorEND
