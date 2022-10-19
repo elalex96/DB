a50ac3e7c623e58ad641e504cdaaede9f80f957d
Neri del Angel <48134542+MarcosGarcia93@users.noreply.github.com>
2022-10-19 13:12:56 -0500

    Issue 2283 | Ajustes a sp de gastos elegibles

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SP_CO_LayoutGastosElegibles.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/SP_CO_LayoutGastosElegibles.sql
@@ -3,58 +3,85 @@
 -- Create date: 19-10-17
 -- Description:	
 -- =============================================
+-- Modificador:	Neri del Angel
+-- Fecha:		19 de Octubre del 2022
+-- Descripción:	Se agregan no lock faltantes, se ajusta la consulta a lo deseado
+--				Se respeta que el filtrado de mes y año sea por el mes presentación del gasto (CO_registro)
+--				Los campos GE_NO_COMPROBANTE, GE_MONTO, GE_PROVEEDOR y GE_DESCRIPCION se obtienen de vista GastosAmatitlan2020
+--				EL campo de Actividad [ID_ADMON] se obtiene de CO_Instalacion.IdInstalacionPemex
+--				El campo GE_REF_DOCUMENTO se queda en blanco, el campo GE_MES se obtiene del mes en que aprobó Pemex CO_RegistroMarkup.MesEstadoPemex
+-- =============================================
 CREATE PROCEDURE [dbo].[SP_CO_LayoutGastosElegibles]
-	-- Add the parameters for the stored procedure here
-@IdPresupuesto INT,
-@IdActividad   INT,
-@Anio          INT,
-@Mes			INT
+    @IdPresupuesto INT,
+    @IdActividad INT,
+    @Anio INT,
+    @Mes INT
 AS
---SP_CO_LayoutGastosElegibles 1,2016,10000
---SP_CO_LayoutGastosElegibles 5,2016,10000
-     BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-         SET NOCOUNT ON;
-         DECLARE @Etiqueta1 NVARCHAR(50);
-         SELECT @Etiqueta1 = 'ID_ADMON'
-         WHERE @IdActividad = 1;
-         SELECT @Etiqueta1 = 'ID_DUCTO'
-         WHERE @IdActividad = 2;
-         SELECT @Etiqueta1 = 'ID_ESTUDIO'
-         WHERE @IdActividad = 3;
-         SELECT @Etiqueta1 = 'ID_INSTALA'
-         WHERE @IdActividad = 4;
-         SELECT @Etiqueta1 = 'ID_POZO'
-         WHERE @IdActividad = 5;
-    -- Insert statements for procedure here
-
-         SELECT AC.ID_CATACTIV AS ID_CATACTIV,
-                SA.ID_CATSUBACTIV AS ID_CATSUBACTIV,
-                YEAR(LPM.AC_FEC_FIN) AS PR_ANO,
-                P.Version AS PR_VERSION,
-                 @Mes AS AC_MES,
-                'Datos para la etiqueta1' AS Actividad,
-                '' AS GE_NO_COMPROBANTE,
-                '' AS GE_MONTO,
-                '' AS GE_REF_DOCUMENTO,
-                '' AS GE_PROVEEDOR,
-                '' AS GE_DESCRIPCION,
-                '' AS GE_MES,
-                @Etiqueta1 AS Etiqueta1
-         FROM CO_LineaPresupuestoMes LPM
-              LEFT JOIN CO_Presupuesto P ON P.IdPresupuesto = LPM.IdPresupuesto
-              LEFT JOIN CO_Servicio S ON LPM.IdServicio = S.IdServicio
-              LEFT JOIN CO_ActividadCIEP AC ON LPM.IdActividad = AC.IdActividad
-              LEFT JOIN CO_Instalacion I ON LPM.IdInstalacion = I.IdInstalacion
-              LEFT JOIN CO_Registro R ON LPM.IdLineaPresupuestoMes = R.IdPrograma
-              LEFT JOIN CO_SubactividadCIEP SA ON LPM.IdSubactividad = SA.IdSubactividad
-              LEFT JOIN FI_Factura F ON R.IdFactura = F.IdFactura
-              LEFT JOIN CO_TipoCambioMensual TCM ON F.IdMoneda = TCM.IdMoneda
-                                                    AND TCM.IdMes = MONTH(R.MesPresentacion)
-                                                    AND TCM.Anio = YEAR(R.MesPresentacion)
-         WHERE LPM.IdPresupuesto = @IdPresupuesto
-               AND AC.ID_CATACTIV = @IdActividad
-               AND YEAR(R.MesPresentacion) = @Anio;
-     END;
-
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @Contrato INT;
+    /**/
+    SELECT TOP 1
+        @Contrato = CO_PeriodoContrato.IdContrato
+    FROM CO_Presupuesto (NOLOCK)
+        JOIN CO_ProgramaActividad (NOLOCK)
+            ON CO_Presupuesto.IdProgramaActividad = CO_ProgramaActividad.IdProgramaActividad
+        JOIN CO_PeriodoContrato (NOLOCK)
+            ON CO_ProgramaActividad.IdPeriodoContrato = CO_PeriodoContrato.IdPeriodo
+    WHERE CO_Presupuesto.IdPresupuesto = @IdPresupuesto
+    /**/
+    SELECT CO_ActividadCIEP.ID_CATACTIV AS ID_CATACTIV,
+           CO_SubactividadCIEP.ID_CATSUBACTIV AS ID_CATSUBACTIV,
+           YEAR(CO_LineaPresupuestoMes.AC_FEC_FIN) AS PR_ANO,
+           ISNULL(CO_Presupuesto.Version, 1) AS PR_VERSION,
+           @Mes AS AC_MES,
+           ISNULL(CO_Instalacion.IdInstalacionPemex, '') AS Actividad,
+           ISNULL(GastosAmatitlan2020.Numero, '') AS GE_NO_COMPROBANTE,
+           ISNULL(GastosAmatitlan2020.MontoUSDConMarkup, 0.00) AS GE_MONTO,
+           '' AS GE_REF_DOCUMENTO,
+           ISNULL(GastosAmatitlan2020.Subcontratista, '') AS GE_PROVEEDOR,
+           ISNULL(GastosAmatitlan2020.Comentarios, '') AS GE_DESCRIPCION,
+           ISNULL(MONTH(CO_RegistroMarkup.MesEstadoPemex), '') AS GE_MES,
+           CASE
+               WHEN @IdActividad = 1 THEN
+                   'ID_ADMON'
+               WHEN @IdActividad = 2 THEN
+                   'ID_DUCTO'
+               WHEN @IdActividad = 3 THEN
+                   'ID_ESTUDIO'
+               WHEN @IdActividad = 4 THEN
+                   'ID_INSTALA'
+               WHEN @IdActividad = 5 THEN
+                   'ID_POZO'
+               ELSE
+                   ''
+           END AS Etiqueta1
+    FROM CO_LineaPresupuestoMes (NOLOCK)
+        JOIN CO_Instalacion
+            ON CO_LineaPresupuestoMes.IdInstalacion = CO_Instalacion.IdInstalacion
+        JOIN CO_Presupuesto (NOLOCK)
+            ON CO_LineaPresupuestoMes.IdPresupuesto = CO_Presupuesto.IdPresupuesto
+               AND CO_Presupuesto.IdPresupuesto = @IdPresupuesto
+        JOIN CO_Registro (NOLOCK)
+            ON CO_LineaPresupuestoMes.IdLineaPresupuestoMes = CO_Registro.IdPrograma
+               AND CO_Registro.IdRegistro IS NOT NULL
+               AND MONTH(CO_Registro.MesPresentacion) = @Mes
+               AND YEAR(CO_Registro.MesPresentacion) = @Anio
+        JOIN CO_RegistroMarkup (NOLOCK)
+            ON CO_Registro.IdRegistro = CO_RegistroMarkup.GastoId
+        JOIN CO_EstadoRegistro_V2 (NOLOCK)
+            ON ISNULL(CO_RegistroMarkup.IdEstadoPemex, 0) = CO_EstadoRegistro_V2.IdClvEstado
+               AND CO_EstadoRegistro_V2.NombreEstado = 'Certificado GE Aprobado CACI'
+               AND CO_EstadoRegistro_V2.IdContrato = @Contrato
+        JOIN GastosAmatitlan2020 (NOLOCK)
+            ON CO_Registro.IdRegistro = GastosAmatitlan2020.IdRegistro
+        JOIN CO_ActividadCIEP (NOLOCK)
+            ON CO_LineaPresupuestoMes.IdActividad = CO_ActividadCIEP.IdActividad
+               AND CO_ActividadCIEP.ID_CATACTIV = @IdActividad
+        LEFT JOIN CO_SubactividadCIEP (NOLOCK)
+            ON CO_LineaPresupuestoMes.IdSubactividad = CO_SubactividadCIEP.IdSubactividad
+    WHERE CO_LineaPresupuestoMes.IdPresupuesto = @IdPresupuesto
+          AND CO_ActividadCIEP.ID_CATACTIV = @IdActividad
+          AND MONTH(CO_Registro.MesPresentacion) = @Mes
+          AND YEAR(CO_Registro.MesPresentacion) = @Anio;
+END;
