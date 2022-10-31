a36d041bb6598c472d8880c0d3c009cff26c473b
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2022-10-31 11:28:01 -0600

    Update SP_AX_BitacoraLecturaCorreos.sql
    Envia correo cuando el documento no tiene información que procesar

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_AX_BitacoraLecturaCorreos.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_AX_BitacoraLecturaCorreos.sql
@@ -1,9 +1,13 @@
-﻿-- =============================================
+-- =============================================
 -- Author:		<Alexander Gomez>
 -- Create date: <06/12/2019>
 -- Description:	<Registro de bitacora de lectura de correos>
 -- =============================================
-CREATE PROCEDURE [dbo].[SP_AX_BitacoraLecturaCorreos]
+-- Author:		<LUIS DAVID>
+-- Create date: <21/10/2022>
+-- Description:	<Se agrega a bitacora cuando el documento no contenga info>
+-- =============================================
+ALTER PROCEDURE [dbo].[SP_AX_BitacoraLecturaCorreos]
 	-- Add the parameters for the stored procedure here
 	@Asunto NVARCHAR(MAX),
 	@CantidadArchvios INT,
@@ -15,6 +19,7 @@ CREATE PROCEDURE [dbo].[SP_AX_BitacoraLecturaCorreos]
 	@Error BIT
 AS
 BEGIN
+DECLARE @HTML NVARCHAR(MAX),@IdNotificacion int;
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
@@ -42,5 +47,43 @@ BEGIN
 	    GETDATE(), -- FechaRegBitacora - datetime
 		@Para,
 		@Error
-	    )
+	    );
+	if (@ServicioOperadora = 'CorreoError')
+	begin
+	SET @IdNotificacion = (SELECT MAX(IdNotificacion) FROM Adinco.dbo.S_Notificacion);
+	SET @HTML = (SELECT HTML FROM dbo.TA_Correo WHERE Asunto = 'Notificación de Resumen de Lectura de WDEA');
+	SET @HTML = (REPLACE(@HTML,'##MENSAJE_GENERAL##',ISNULL(@Asunto,'')));
+	SET @HTML = (REPLACE(@HTML,'##MENSAJE_CORRECTOS##',ISNULL('','')));
+	SET @HTML = (REPLACE(@HTML,'##MENSAJE_ERRORES##',ISNULL('','')));
+	SET @HTML = (REPLACE(@HTML,'##ANIO_ACTUAL##',YEAR(GETDATE())));
+		INSERT INTO Adinco.dbo.S_Notificacion
+   (
+            IdNotificacion,
+            Para,
+            Asunto,
+            Mensaje,
+            FechaProgramadaEnvio,
+            Enviada,
+            FechaEnvio,
+            CreadoPor,
+            CreadoEl,
+            ModificadoPor,
+            ModificadoEl,
+            De
+    )
+	SELECT
+			(@IdNotificacion + ROW_NUMBER() over( order by Destinatario desc)), 
+			Destinatario, 
+			CAST(CAST(GETDATE() AS DATE) AS nvarchar) + ' Reporte de interfase ADINCO SAP',
+			REPLACE(@HTML,'##NOMBRE_USUARIO##',ISNULL(Nombre,'Usuario de ADINCO')), 
+			DATEADD(MINUTE, 1, GETDATE()), 
+			0, 
+			NULL, 
+			3, 
+			GETDATE(), 
+			NULL, 
+			NULL,
+			'notificaciones@adinco.mx'
+	FROM dbo.WDEA_CorreosResumenProcesamiento;
+	end
 END
