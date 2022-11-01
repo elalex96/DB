bdc750b0d6e01337f52a0b4d6be5373a03c78839
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2022-10-31 20:40:01 -0600

    Actualización de sps

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_PO_ConsultarProveedoresPedido.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_PO_ConsultarProveedoresPedido.sql
@@ -1,26 +1,54 @@
-﻿-- Author:		Luis David De La Cruz Bautista
--- Create date: 03/02/2021
--- Description:	Optimización por issue 955
--- =============================================
-CREATE PROCEDURE SP_PO_ConsultarProveedoresPedido @IdSolicitudPedido INT, @Version INT, @IdPedido INT
+﻿USE [Petrovendor]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_PO_ConsultarProveedoresPedido'
+)
+    DROP PROCEDURE SP_PO_ConsultarProveedoresPedido;
+GO
+/****** Object:  StoredProcedure [dbo].[SP_PO_ConsultarProveedoresPedido]    Script Date: 31/10/2022 05:41:12 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- DAC- No retornar usuarios inactivos 
+CREATE PROCEDURE [dbo].[SP_PO_ConsultarProveedoresPedido] 
+@IdSolicitudPedido 
+INT, @Version INT, 
+@IdPedido INT
 AS
 BEGIN
 	
-	SELECT PR.IdProveedor, (SELECT TOP 1 RazonSocial +' '+ RegimenCapital from s_proveedor where IdProveedor= P.IdProveedorCompras) AS Proveedor, U.IdUsuario,  U.Nombre, U.Correo, P.IdPedido,H.HorasVigencia, PG.IdPedido AS IdPedidoGeneral
-	FROM MM_Pedido AS P 
-	INNER JOIN S_Proveedor AS PR 
+	SELECT PR.IdProveedor, 
+	(ISNULL(PC.RazonSocial,'') +' '+ ISNULL(PC.RegimenCapital,'')) AS Proveedor,
+	U.IdUsuario,
+	U.Nombre,
+	U.Correo,
+	P.IdPedido,
+	H.HorasVigencia,
+	PG.IdPedido AS IdPedidoGeneral
+	FROM MM_Pedido AS P (NOLOCK)
+	INNER JOIN S_Proveedor AS PR  (NOLOCK)
 		ON P.IdSubcontratista = PR.IdProveedor 
-    INNER JOIN S_UsuarioProveedor AS UP 
+	INNER JOIN S_Proveedor PC
+		ON P.IdProveedorCompras = PC.IdProveedor
+    INNER JOIN S_UsuarioProveedor AS UP  (NOLOCK)
 		ON PR.IdProveedor = UP.IdProveedor 
-	INNER JOIN S_Usuario AS U 
-		ON UP.IdUsuario = U.IdUsuario 
-	INNER JOIN MM_HorasVigenciaPedido AS H 
-		ON P.IdPedido = H.IdPedido
-	INNER JOIN MM_Pedidos AS PG 
+	INNER JOIN S_Usuario AS U  (NOLOCK)
+		ON UP.IdUsuario  = U.IdUsuario 
+	INNER JOIN MM_HorasVigenciaPedido AS H  (NOLOCK)
+		ON P.IdPedido  = H.IdPedido 
+	INNER JOIN MM_Pedidos AS PG  (NOLOCK)
 		ON P.IdPedido = PG.IdIdentificador 
-		AND PG.IdTipoPedido = 2 
 		AND P.IdProveedorCompras = PG.IdProveedorCliente 
-	WHERE P.IdSolicitudPedido = @IdSolicitudPedido AND P.Version = @Version AND (U.IdTipoUsuario = 4 OR U.IdTipoUsuario = 3) AND PG.IdPedido = @IdPedido
+		AND PG.IdTipoPedido IN (2,4,6) --> CTES MER, DIRECTA, ORDEN 		
+	WHERE P.IdSolicitudPedido = @IdSolicitudPedido 
+	AND P.Version = @Version 
+	AND PG.IdPedido = @IdPedido
+	AND (U.IdTipoUsuario = 4 OR U.IdTipoUsuario = 3) 	
+	AND U.Activo = 1 --> Solo usuarios activos
 	ORDER BY P.IdPedido
 
 END
