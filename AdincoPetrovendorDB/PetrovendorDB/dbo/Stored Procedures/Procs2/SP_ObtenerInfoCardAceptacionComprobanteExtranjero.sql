6c72b37cbf069ab72ca815606b5727d5e8b237af
Daniel Antonio <37602019+danielantoniocruz@users.noreply.github.com>
2023-01-10 14:00:19 -0600

    Se cambia cte por variable
    Se cambia cte por variable

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_ObtenerInfoCardAceptacionComprobanteExtranjero.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_ObtenerInfoCardAceptacionComprobanteExtranjero.sql
@@ -95,7 +95,7 @@ BEGIN
 			ON AP_PC.IdPedimentoComprobante = PC.IdPedimentoComprobante
 		JOIN TA_Operacion O 
 			ON PC.IdPedimentoComprobante  = O.IdDocumento 
-			AND O.IdTipoOperacion = 16
+			AND O.IdTipoOperacion = @TipoOperacionId
 		JOIN TA_ESTATUS AS E
 			ON E.IdEstatus = O.IdEstatusOperacion
 		WHERE		ISNULL ( AP.IdEstatusEliminado, 0 ) <> 1 --> MOSTRAR ACEPTACIONES NO ELIMINADAS						
