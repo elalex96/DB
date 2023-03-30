9e74da261861eb7f8811b5fa6b3fa6c1982a3ba7
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2023-03-30 12:56:01 -0600

    alter stored

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/p_GeneracionTransferenciaConLigueDeComplemento.sql
@@ -0,0 +1,21 @@
+﻿-- =============================================
+-- Author:		Reyna Olvera 
+-- Create date: 11/01/2018
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE AlteMenuRol
+	@bit int,
+	@idMenuRol int 
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	Update ap_MenuporRol
+	set visible=@bit 
+	where idMenuRol=@idMenuRol
+END
+
