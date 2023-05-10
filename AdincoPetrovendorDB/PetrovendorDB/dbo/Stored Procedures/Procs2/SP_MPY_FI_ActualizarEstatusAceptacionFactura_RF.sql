e64325a47051348ffec543dbe0c0b251d9c9666b
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2023-05-10 14:31:54 -0600

    se agrega el filtrado por usuario activo, nolocks y reacomodo de joins para sps del proceso de murphy

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MPY_FI_ActualizarEstatusAceptacionFactura_RF.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MPY_FI_ActualizarEstatusAceptacionFactura_RF.sql
@@ -1,4 +1,18 @@
-﻿-- =============================================
+﻿USE [Petrovendor]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_MPY_FI_ActualizarEstatusAceptacionFactura_RF'
+)
+    DROP PROCEDURE SP_MPY_FI_ActualizarEstatusAceptacionFactura_RF;
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		Alexander Gomez
 -- Create date: 16-08-17
 -- Description:	Actualiza el estatus de aprobador y de la parobación general  
@@ -6,6 +20,10 @@
 -- Author:		Jose Roman
 -- Create date: 19-09-2018
 -- Description:	Se modifica la aprobacion de factura
+-- =============================================
+-- Author:		Alexander Gomez
+-- Create date: 10/05/2023
+-- Description:	se agrega el filtrado por usuario activo, nolocks y reacomodo de joins
 -- =============================================
 CREATE PROCEDURE [dbo].[SP_MPY_FI_ActualizarEstatusAceptacionFactura_RF]
     -- Add the parameters for the stored procedure here
@@ -56,19 +74,31 @@ BEGIN
 		SES.SESNumber,
 		PSES.IdPRESES,
 		SES.SESReferenceNumber
-	FROM dbo.MPY_MM_AceptacionFactura AS AF 
-		LEFT JOIN dbo.TA_Estatus AS E ON E.IdEstatus = AF.IdEstatus
-		LEFT JOIN dbo.MPY_MM_AceptacionPedido AS AP ON AP.IdAceptacionPedido = AF.IdAceptacionPedido
-		LEFT JOIN Adinco.dbo.CO_SAPVendor AS SV ON SV.VendorIDSAP COLLATE SQL_Latin1_General_CP1_CI_AS = AP.IdSubContratista COLLATE SQL_Latin1_General_CP1_CI_AS
-		LEFT JOIN dbo.S_Proveedor AS PR ON PR.RFC COLLATE SQL_Latin1_General_CP1_CI_AS = SV.TaxID COLLATE SQL_Latin1_General_CP1_CI_AS
-		LEFT JOIN dbo.S_UsuarioProveedor AS UP ON UP.IdProveedor = PR.IdProveedor
-		LEFT JOIN dbo.S_Usuario AS US ON US.IdUsuario = UP.IdUsuario AND (US.IdTipoUsuario = 3 OR US.IdTipoUsuario = 4)
-		LEFT JOIN Adinco.dbo.CO_Contratista AS CO ON CO.IdContratista = AP.IdProveedor
-		LEFT JOIN Adinco.dbo.CO_SAPSES AS SES ON SES.PO_SAPNumer COLLATE Modern_Spanish_CI_AS = AP.IdPedido COLLATE Modern_Spanish_CI_AS AND SES.SESReferenceNumber COLLATE Modern_Spanish_CI_AS = AP.ReferenceNumber COLLATE Modern_Spanish_CI_AS
-		LEFT JOIN Adinco.dbo.CO_SAPPRESES AS PSES ON PSES.SAPPONumber = SES.PO_SAPNumer AND PSES.SAPSESNumber = SES.SESReferenceNumber
-	WHERE AF.IdAceptacionPedido = @IdAceptacionPedido
-	AND US.IdUsuario IS NOT NULL
-	
+	FROM dbo.MPY_MM_AceptacionFactura AS AF (NOLOCK)
+		LEFT JOIN dbo.TA_Estatus AS E (NOLOCK)
+			ON AF.IdEstatus = E.IdEstatus
+			AND AF.IdAceptacionPedido = @IdAceptacionPedido
+		LEFT JOIN dbo.MPY_MM_AceptacionPedido AS AP (NOLOCK)
+			ON AF.IdAceptacionPedido = AP.IdAceptacionPedido
+		LEFT JOIN Adinco.dbo.CO_SAPVendor AS SV (NOLOCK)
+			ON AP.IdSubContratista COLLATE SQL_Latin1_General_CP1_CI_AS = SV.VendorIDSAP COLLATE SQL_Latin1_General_CP1_CI_AS
+		LEFT JOIN dbo.S_Proveedor AS PR (NOLOCK)
+			ON SV.TaxID COLLATE SQL_Latin1_General_CP1_CI_AS = PR.RFC COLLATE SQL_Latin1_General_CP1_CI_AS
+		LEFT JOIN dbo.S_UsuarioProveedor AS UP (NOLOCK)
+			ON PR.IdProveedor = UP.IdProveedor
+		LEFT JOIN dbo.S_Usuario AS US (NOLOCK)
+			ON UP.IdUsuario = US.IdUsuario  
+			AND (US.IdTipoUsuario = 3 OR US.IdTipoUsuario = 4) 
+			AND US.Activo = 1
+			AND US.IdUsuario IS NOT NULL
+		LEFT JOIN Adinco.dbo.CO_Contratista AS CO (NOLOCK)
+			ON CO.IdContratista = AP.IdProveedor
+		LEFT JOIN Adinco.dbo.CO_SAPSES AS SES (NOLOCK)
+			ON AP.IdPedido COLLATE Modern_Spanish_CI_AS = SES.PO_SAPNumer COLLATE Modern_Spanish_CI_AS 
+				AND AP.ReferenceNumber COLLATE Modern_Spanish_CI_AS = SES.SESReferenceNumber COLLATE Modern_Spanish_CI_AS
+		LEFT JOIN Adinco.dbo.CO_SAPPRESES AS PSES (NOLOCK)
+			ON SES.PO_SAPNumer = PSES.SAPPONumber
+			AND SES.SESReferenceNumber = PSES.SAPSESNumber;
 END;
 
 
--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MPY_UsuarioPreFactura.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MPY_UsuarioPreFactura.sql
@@ -1,9 +1,27 @@
-﻿-- =============================================
+﻿USE [Petrovendor]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_MPY_UsuarioPreFactura'
+)
+    DROP PROCEDURE SP_MPY_UsuarioPreFactura;
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		Alexander Gomez
 -- Create date: 06/11/2018
 -- Description:	Consultas de usuario de la carga 
 -- =============================================
-create procedure [dbo].[SP_MPY_UsuarioPreFactura]
+-- Author:		Alexander Gomez
+-- Create date: 10/05/2023
+-- Description:	se agrega el filtrado por usuario activo, nolocks y reacomodo de joins
+-- =============================================
+CREATE procedure [dbo].[SP_MPY_UsuarioPreFactura]
 	-- Add the parameters for the stored procedure here
 	@IdPRESES INT
 AS
@@ -17,7 +35,9 @@ BEGIN
 		US.IdUsuario,
 		US.Nombre,
 		US.Correo
-	FROM Adinco.dbo.CO_SAPPRESES AS PSES
-	LEFT JOIN dbo.S_Usuario AS US ON US.IdUsuario = PSES.CreadoPor
-	WHERE PSES.IdPRESES = @IdPRESES
+	FROM Adinco.dbo.CO_SAPPRESES AS PSES (NOLOCK)
+	LEFT JOIN dbo.S_Usuario AS US (NOLOCK) ON PSES.CreadoPor = US.IdUsuario 
+		AND US.Activo = 1
+	WHERE PSES.IdPRESES = @IdPRESES;
+
 END
