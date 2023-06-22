c9c76bbdd5eeef1dfa419ef3d1a3a4631c813a6b
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2023-06-22 00:00:48 -0600

    correccion de sp que creaban y enviaban correos, se actualiza el formato con el logo y color

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/EN_sp_GuardaRelacionDocumentoS3Bitacora.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/EN_sp_GuardaRelacionDocumentoS3Bitacora.sql
@@ -1,4 +1,18 @@
-﻿CREATE PROC EN_sp_GuardaRelacionDocumentoS3Bitacora
+﻿USE [Adinco]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'EN_sp_GuardaRelacionDocumentoS3Bitacora'
+)
+    DROP PROCEDURE EN_sp_GuardaRelacionDocumentoS3Bitacora;
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[EN_sp_GuardaRelacionDocumentoS3Bitacora]
 @IdBitacora int,
 @IdDocumento int,
 @IdUsuario int,
@@ -11,153 +25,15 @@ DECLARE
 @pIdNotificacion int,
 @Mensaje varchar(1000),
 @NombreUsuario varchar(1000) = (SELECT TOP 1
-								US.Nombre
-								FROM AP_Usuario AS US
+								US.Nombre 
+								FROM AP_Usuario AS US (NOLOCK)
 								WHERE UsuarioID = @IdUsuario), 
 @para varchar(1000) = (SELECT TOP 1
 								US.Usuario
-								FROM AP_Usuario AS US
+								FROM AP_Usuario AS US (NOLOCK)
 								WHERE UsuarioID = @IdUsuario),
-@HTML varchar(max) = 
- '<p></p>
-<table class="full" border="0" width="100%" cellspacing="0" cellpadding="0" align="center">
-<tbody>
-<tr>
-<td height="25">&nbsp;</td>
-</tr>
-</tbody>
-</table>
-<table class="full" border="0" width="100%" cellspacing="0" cellpadding="0">
-<tbody>
-<tr>
-<td align="center">
-<table class="devicewidth" border="0" width="600" cellspacing="0" cellpadding="0" align="center">
-<tbody>
-<tr>
-<td>
-<table class="full" style="border-radius: 6px 6px 0 0;" width="100%" cellspacing="0" cellpadding="0" align="center" bgcolor="#5e5f5e">
-<tbody>
-<tr>
-<td height="3">&nbsp;</td>
-</tr>
-<tr>
-<td>
-<table class="inner" style="border-collapse: collapse;" border="0" align="left">
-<tbody>
-<tr>
-<td class="inner" valign="middle" height="45"><a><img class="logo" style="padding-left: 2em;" src="https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/41/ee/c9/41eec948-00b3-5528-87b1-d989929f6ec4/source/60x60bb.jpg" width="75" height="75" /></a></td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-<tr>
-<td height="3">&nbsp;</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-<!--CUERPO DEL MENSAJE DE CORREO-->
-<table class="devicewidth" border="0" width="600" cellspacing="0" cellpadding="0" align="center">
-<tbody>
-<tr>
-<td>
-<table class="full" style="background-repeat: repeat-x; background-position: left top; height: 259px; width: 100%;" border="0" width="100%" cellpadding="0" align="center" bgcolor="#FFFFFF">
-<tbody>
-<tr style="height: 25px;">
-<td style="height: 25px;" height="25">&nbsp;</td>
-</tr>
-<tr style="height: 36px;">
-<td class="smallfont" style="color: black; height: 36px;" align="center">Estimado(a) ##NombreUsuario##</td>
-</tr>
-<tr style="height: 18px;">
-<td class="smallfont" style="color: black; height: 18px;" align="center">&nbsp;</td>
-</tr>
-<tr style="height: 18px;">
-<td class="smallfont" style="color: red; height: 18px;" align="center">&nbsp;</td>
-</tr>
-<tr style="height: 108px;">
-<td class="smallfont" style="color: black; height: 108px;" align="center">
-<br />##Mensaje##
-<br />
-</td>
-<tr>
-<td class="smallfont" style="font: 600 12px "Open Sans", Arial, Helvetica, sans-serif; color: #5e5f5e;" align="center">Ingrese a Adinco para descargar su documento. 
-<br /><br /><a style="font-size: 12px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; line-height: 10px; text-decoration: none; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; padding: 10px 10px; display: inline-block; letter-spacing: 1px; text-align: center; font-weight: bold; text-transform: uppercase; width: 8em; background: #2DB360;" 
-href="##URL_TAREA##/2/Entregables/ReporteSasisopa.aspx" target="_blank" name="btnDetalle"> Ver listado de Documentos</a> <br /><br /></td>
-</tr>
-</tr>
-<tr style="height: 18px;">
-<td style="height: 18px;" height="16">&nbsp;</td>
-</tr>
-<tr style="height: 18px;">
-<td class="smallfont" style="color: #5e5f5e; height: 18px;" align="center">&nbsp;</td>
-</tr>
-<tr style="height: 18px;">
-<td style="height: 18px;" height="10">&nbsp;</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-<table class="full" border="0" width="100%" cellspacing="0" cellpadding="0" align="center">
-<tbody>
-<tr>
-<td align="center">
-<table class="devicewidth" border="0" width="600" cellspacing="0" cellpadding="0" align="center">
-<tbody>
-<tr>
-<td>
-<table class="full" style="border-radius: 0 0 6px 6px;" width="100%" cellspacing="0" cellpadding="0" align="center" bgcolor="#5e5f5e">
-<tbody>
-<tr>
-<td height="18">&nbsp;</td>
-</tr>
-<tr>
-<td>
-<table class="inner" style="border-collapse: collapse; text-align: center;" border="0" width="230" cellspacing="0" cellpadding="0" align="center">
-<tbody>
-<tr>
-<td width="20">&nbsp;</td>
-<td>
-<table border="0" width="100%" cellspacing="0" cellpadding="0" align="center">
-<tbody>
-<tr>
-<td style="color: #ffffff;">|</td>
-<td style="font: 10px Helvetica,Arial, sans-serif; color: #ffffff;" align="center">&copy; ##YEAR_ACTUAL##, Todos los derechos reservados</td>
-<td style="color: #ffffff;">|</td>
-</tr>
-<tr>
-<td height="15">&nbsp;</td>
-</tr>
-</tbody>
-</table>
-</td>
-<td width="20">&nbsp;</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>';
+@HTML varchar(max) = (SELECT HTML FROM TA_Correo (NOLOCK) WHERE Asunto = 'Descargade Reporte SASISOPA');
+
 	-- SE ACTUALIZA EL PROCESADO
 	UPDATE EN_Documentos_BitacoraReporteSASISOPA
 	SET Procesado = 1
