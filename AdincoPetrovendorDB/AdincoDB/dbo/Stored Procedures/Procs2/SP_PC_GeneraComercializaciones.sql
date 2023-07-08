466b3e1bd02994eca2d4710defc76ad7b96c9540
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2023-07-08 09:49:12 -0600

    se quita las test del sp

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_PC_GeneraComercializaciones.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_PC_GeneraComercializaciones.sql
@@ -66,6 +66,7 @@ IF @Mes IN ( '2022-06-01', '2022-07-01', '2022-08-01', '2022-09-01', '2022-10-01
   
   
  --Calculo del volumen de crudo a vender basado en reparticion preliminar  
+ SELECT @FechaLimite = '20231231';
  IF @FechaLimite >= GETDATE()  
  BEGIN  
   
