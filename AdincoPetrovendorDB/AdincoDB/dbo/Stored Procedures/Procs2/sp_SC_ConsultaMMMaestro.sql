254b1b32526c0950352ffa3027b87574201a8e1b
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2023-07-11 17:45:29 -0600

    Se agrega mejoras en sps para pagina de ConsultaOTConvenio de procura

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_SC_ConsultaMMMaestro.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_SC_ConsultaMMMaestro.sql
@@ -1,50 +1,48 @@
-﻿
--- sp_SC_ConsultaMMMaestro 1,0
-Create Proc sp_SC_ConsultaMMMaestro
+﻿USE [Adinco]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'sp_SC_ConsultaMMMaestro'
+)
+    DROP PROCEDURE sp_SC_ConsultaMMMaestro; 
+	GO 
+/****** Object:  StoredProcedure [dbo].[sp_SC_ConsultaMMMaestro]    Script Date: 10/07/2023 09:36:26 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Daniel AC
+-- Create date: <11/07/202>
+-- Description:	SP SE USA EN PAGINA ConsultaOTConvenio DE PETROVENDOR
+-- =============================================
+CREATE Proc [dbo].[sp_SC_ConsultaMMMaestro]
 @pIdSubContrato int,
 @pIdSubFamilia int
-
 As
-
+SET NOCOUNT ON;
 	SELECT DISTINCT  MM.IdMaestro,
 			MM.IdTipoCatalogoMaestro,
 			MM.IdSubFamilia,
-			--MM.CodSubFamilia,
 			MM.TextoCorto,
 			MM.TextoLargo,
 			IdUnidad =  MM.IdUnidadPreterminada,
-			--MM.UMB,
-			MM.IdMoneda,
-			--MM.Precio,
-			MM.IdTipoMaterial,
-			--MM.NoMatAnti,
-			--MM.Ce,
-			--MM.Material,
-			MM.Prc,
-			--MM.Entre,
-			--MM.CatVa,
-			--MM.UltMod,
-			--MM.CaP,
-			--MM.ClValor,
-			--MM.GCp,
-			--MM.ABC,
-			--MM.Servicio,
-			--MM.TipoServicio,
-			--MM.CatValor,
-			--MM.IdImpuesto,
-			--MM.Denomin,
-			--MM.Creado,
-			--MM.Modificado,
+			MM.IdMoneda,		
+			MM.IdTipoMaterial,		
+			MM.Prc,			
 			MM.IsActivo,
 			MM.IsEliminado,
 			MM.CreadoPor,
 			MM.CreadoEn,
 			MM.ModificadoPor,
 			MM.ModificadoEn
-		FROM Petrovendor.dbo.[MM_Maestro] mm
-		LEFT join SC_Materiales scMAT ON scMAT.IdMaestro = mm.IdMaestro
-		where isactivo = 1 and isEliminado = 0 
+		FROM Petrovendor.dbo.[MM_Maestro] mm (NOLOCK)
+		LEFT JOIN SC_Materiales scMAT (NOLOCK)
+		ON mm.IdMaestro = scMAT.IdMaestro
+		where mm.isactivo = 1 and mm.isEliminado = 0 
 		AND IdTipoCatalogoMaestro = 2 AND		
 		@pIdSubContrato in (0,scMAT.IdSubContrato) and
 		@pIdSubFamilia in (0,mm.IdSubFamilia)
-		order by MM.TextoCorto
+		order by MM.TextoCorto ASC
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_SC_ConsultarSubContrato.sql
@@ -0,0 +1,33 @@
+﻿USE [Adinco]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'sp_SC_ConsultarSubContrato'
+)
+    DROP PROCEDURE sp_SC_ConsultarSubContrato; 
+	GO 
+/****** Object:  StoredProcedure [dbo].[sp_SC_ConsultarSubContrato]    Script Date: 10/07/2023 11:02:16 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Daniel AC
+-- Create date: <11/07/202>
+-- Description:	SP SE USA EN PAGINA ConsultaOTConvenio DE PETROVENDOR
+-- =============================================
+CREATE Proc [dbo].[sp_SC_ConsultarSubContrato]
+	@ContratoId			int,
+	@UsuarioId		int	
+As
+begin
+	SET NOCOUNT ON;
+	SELECT IdSubContrato, NumeroSubContrato
+	FROM [SC_SubContrato]  (NOLOCK)
+	where isActivo = 1 
+	and isEliminado = 0 
+	order by NumeroSubContrato ASC
+
+end
