389598dc7a0aa9944dcf4c1a009017b72278c4ae
Neri del Angel <48134542+MarcosGarcia93@users.noreply.github.com>
2023-07-12 15:37:21 -0600

    Update SIPAC_RC_CONT_21_MConciliacion.sql

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SIPAC_RC_CONT_21_MConciliacion.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SIPAC_RC_CONT_21_MConciliacion.sql
@@ -245,7 +245,7 @@ AS
             )  
                     SELECT  
                         CO_Registro.IdRegistro,  
-                        ISNULL(FI_Factura.UUID, 'NÚMERO NO REGISTRADO ') AS UUID,  
+                        ISNULL(FI_Factura.UUID, 'NÚMERO NO REGISTRADO') AS UUID,  
                         FI_Factura.IdFactura,  
                         CO_Registro.MontoRegistro,  
                         CASE  
@@ -331,7 +331,7 @@ AS
                                                                    END  
                     GROUP BY  
                         CO_Registro.IdRegistro,  
-                        ISNULL(FI_Factura.UUID, 'NÚMERO NO REGISTRADO '),  
+                        ISNULL(FI_Factura.UUID, 'NÚMERO NO REGISTRADO'),  
                         FI_Factura.IdFactura,  
                         CO_Registro.MontoRegistro,  
                         CASE  
