84f1bdd7c6ff1f43bdcd76bbbdcfb4b288559e25
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2023-07-13 13:50:34 -0600

    se descarta amatitlan

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DeshabilitarUnicoCentroCosto.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/SP_DeshabilitarUnicoCentroCosto.sql
@@ -1,7 +1,26 @@
-﻿-- =============================================
+﻿USE [Petrovendor]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_DeshabilitarUnicoCentroCosto'
+)
+    DROP PROCEDURE SP_DeshabilitarUnicoCentroCosto;
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		<Pedro Acuña>
 -- Create date: <27-09-2019>
 -- Description:	<los proveedores que se encuentren en esta tabla se deshabilitara el check unico centro de costo de la solicitud de pedido>
+-- =============================================
+-- =============================================
+-- Author:		<Alexander Gomez>
+-- Create date: <13/07/2023>
+-- Description:	<se descarta amatitlan>
 -- =============================================
 CREATE PROCEDURE [dbo].[SP_DeshabilitarUnicoCentroCosto] @IdProveedor INT
 AS
@@ -12,6 +31,7 @@ BEGIN
         FROM dbo.DEA_Proveedor (NOLOCK)
         WHERE IdProveedor = @IdProveedor
 		AND ISNULL(Activo, 0) = 1 
+		AND RFC <> 'PAM140722DK6'
     )
         SELECT 1
     ELSE
