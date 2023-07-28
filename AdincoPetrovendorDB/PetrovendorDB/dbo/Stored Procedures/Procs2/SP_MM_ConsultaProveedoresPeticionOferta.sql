2b78532e0d181dd5810eb0e1f92afbbc66f97f07
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2023-07-28 10:54:21 -0600

    se agrega limite de caracteres en la razon social y correo de contacto

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_ConsultaProveedoresPeticionOferta.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_ConsultaProveedoresPeticionOferta.sql
@@ -25,6 +25,10 @@ GO
 -- Create date: <10/07/2023>
 -- Description:	<Se agrega el descartado del contratista pico para que aparesca en la lsita de proveedores>
 -- =============================================
+-- Author:		<Alexander Gomez>
+-- Create date: <28/07/2023>
+-- Description:	se agrega limite de caracteres en la razon social y correo de contacto issue: https://github.com/Adinco/petrovendor/issues/2387
+-- =============================================
 CREATE PROCEDURE [dbo].[SP_MM_ConsultaProveedoresPeticionOferta] --0,'',1
 	-- Add the parameters for the stored procedure here
 	@IdProveedor INT,
@@ -95,8 +99,22 @@ BEGIN
 		SELECT 
 			ROW_NUMBER() OVER(PARTITION BY LP.IdProveedor ORDER BY LP.RazonSocial ASC) AS R,
 			LP.IdProveedor,
-			LP.RazonSocial,
-			dbo.FN_MM_ObtenerCorreoProveedor(LP.IdProveedor) AS CorreoEmpresa,
+			CASE 
+				WHEN LEN(LP.RazonSocial) > 23 THEN SUBSTRING(LP.RazonSocial,1,23) + '...'
+				ELSE LP.RazonSocial
+			END AS RazonSocial,
+			 ISNULL((SELECT TOP 1  
+						CASE 
+							WHEN LEN(US.Correo) > 30 THEN SUBSTRING(US.Correo,1,26) + '...'
+							ELSE US.Correo  
+						END
+					FROM dbo.S_UsuarioProveedor AS UPR (NOLOCK) 
+					LEFT JOIN dbo.S_Usuario AS US (NOLOCK) ON US.IdUsuario = UPR.IdUsuario  
+					WHERE UPR.IdProveedor = LP.IdProveedor  
+						AND US.IdTipoUsuario = 3  
+						AND US.Activo = 1  
+					ORDER BY US.FechaRegistro DESC  
+			),'') AS CorreoEmpresa,
 			dbo.ObtenerEstrellasModificado(LP.IdProveedor) AS Estrellas,
 			LP.IsBlackList,
 			IMP.ImagenProveedorThumb AS ImagenProveedor,
@@ -113,3 +131,4 @@ BEGIN
 	ORDER BY R.RazonSocial
 
 END
+
