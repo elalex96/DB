f97553174be56f737ba9ce6379d5bfef0b9b4cb0
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2023-08-09 13:05:02 -0600

    --

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/sp_AP_ActualizaDatosPerfil.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/sp_AP_ActualizaDatosPerfil.sql
@@ -1,7 +1,26 @@
-﻿-- =============================================
+﻿USE [Adinco]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'sp_AP_ActualizaDatosPerfil'
+)
+    DROP PROCEDURE sp_AP_ActualizaDatosPerfil;
+/****** Object:  StoredProcedure [dbo].[sp_AP_ActualizaDatosPerfil]    Script Date: 08/08/2023 05:27:56 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		Miguel Gomez
 -- Create date: 
 -- Description:	
+-- =============================================
+-- =============================================
+-- Author:	Daniel AC
+-- Create date: 08/08/2023
+-- Description:	Actualizar perfil del usuario
 -- =============================================
 CREATE PROCEDURE [dbo].[sp_AP_ActualizaDatosPerfil] 
 	-- Add the parameters for the stored procedure here
@@ -9,7 +28,8 @@ CREATE PROCEDURE [dbo].[sp_AP_ActualizaDatosPerfil]
 @IdContrato INT          = 0,
 @Nombre     VARCHAR(MAX) = '',
 @Foto       IMAGE,
-@NumeroCel VARCHAR(20)
+@NumeroCel VARCHAR(20),
+@IdPais		INT   
 AS
          BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -22,8 +42,9 @@ AS
                    Nombre = @Nombre,
                    Foto = @Foto,
 				   NumeroCelular=@NumeroCel,
-			    ModificadoEl = GETDATE(),
-			    ModificadoPor = @IdUsuario
+				   ModificadoEl = GETDATE(),
+			       ModificadoPor = @IdUsuario,
+				   CodigoPais = CASE WHEN ISNULL(@IdPais,0) = 0 THEN NULL ELSE @IdPais END
              WHERE UsuarioID = @IdUsuario;
              
 		   IF @@ERROR <> 0
@@ -34,3 +55,5 @@ AS
                     'Tu perfil se ha guardado exitosamente' AS MSG;
          END;
 
+
+		 
