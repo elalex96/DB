5905167d54add22076f2dc0ceb195acf549a58e7
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2023-08-09 13:04:57 -0600

    Se agrega cambios para poder actualizar la información del perfil

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/AP_ConsultarInformacionPerfilPorUsuario.sql
@@ -0,0 +1,42 @@
+﻿USE [Adinco]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'AP_ConsultarInformacionPerfilPorUsuario'
+)
+    DROP PROCEDURE AP_ConsultarInformacionPerfilPorUsuario;
+/****** Object:  StoredProcedure [dbo].[AP_ConsultarInformacionPerfilPorUsuario]    Script Date: 08/08/2023 05:27:56 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:	Daniel AC
+-- Create date: 08/08/2023
+-- Description:	Obtener el pais y código del pais por usuario
+-- =============================================
+CREATE PROCEDURE [dbo].[AP_ConsultarInformacionPerfilPorUsuario] 
+	-- Add the parameters for the stored procedure here
+@IdUsuario  INT          = 0,
+@IdContrato INT          = 0
+AS
+         BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+SET NOCOUNT ON;
+
+	 SELECT 
+	 Nombre,
+	 NumeroCelular,
+	 Foto As Foto,  
+	 U.CodigoPais AS IdPais,
+	 P.CodigoPais AS CodigoPais
+	 FROM AP_Usuario U   (NOLOCK)
+	 LEFT JOIN dbo.AP_Paises P (NOLOCK)
+		ON U.CodigoPais = p.idPais
+	 WHERE  U.UsuarioID = @IdUsuario
+
+
+END;
---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/AP_ConsultarPaisesCodigos.sql
@@ -0,0 +1,51 @@
+﻿USE [Adinco]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'AP_ConsultarPaisesCodigos'
+)
+    DROP PROCEDURE AP_ConsultarPaisesCodigos;
+/****** Object:  StoredProcedure [dbo].[sp_AP_ActualizaDatosPerfil]    Script Date: 08/08/2023 05:27:56 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:	Daniel AC
+-- Create date: 08/08/2023
+-- Description:	Consultar código de país
+-- =============================================
+CREATE PROCEDURE [dbo].[AP_ConsultarPaisesCodigos] 
+	-- Add the parameters for the stored procedure here
+@IdUsuario  INT          = 0,
+@IdContrato INT          = 0
+AS
+         BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+SET NOCOUNT ON;
+
+CREATE TABLE #Paises (IdPais INT, NombrePais VARCHAR(MAX))
+ 
+ --> AGREGAR QUE MEXICO SE LA PRIMER OPCIÓN
+ INSERT INTO #Paises(IdPais, NombrePais)
+ SELECT idPais AS IdPais,
+ CONCAT(NombrePais,' (+', CodigoPais,+')') AS NombrePais
+ FROM AP_Paises (NOLOCK)
+ where NombrePais = 'México'
+ ORDER BY NombrePais ASC
+
+ 
+  INSERT INTO #Paises(IdPais, NombrePais)
+ SELECT idPais AS IdPais,
+ CONCAT(NombrePais,' (+', CodigoPais,+')') AS NombrePais
+ FROM AP_Paises (NOLOCK)
+ where NombrePais <> 'México'
+ ORDER BY NombrePais ASC
+
+ SELECT IdPais, NombrePais 
+ FROM #Paises
+
+END;
