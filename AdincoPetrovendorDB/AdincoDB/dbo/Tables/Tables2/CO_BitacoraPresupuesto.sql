f872b6f6c47f522d5ede46f23aea884f06138012
Neri del Angel <48134542+MarcosGarcia93@users.noreply.github.com>
2023-08-28 09:57:16 -0600

    Se corrige el nombre de la columna a el solicitado en el issue

--- AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables2/CO_BitacoraPresupuesto.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables2/CO_BitacoraPresupuesto.sql
@@ -10,7 +10,7 @@
     CreadoPor INT NOT NULL,
     DetalleInsercion VARCHAR(8000),
     IdPresupuesto INT NULL,
-    AdjuntarClaveDeSubtarea BIT NOT NULL, 
+    chkAdjuntaClaveSubTarea BIT NOT NULL, 
     CONSTRAINT PK_CO_BitacoraPresupuesto
         PRIMARY KEY CLUSTERED (IdCarga ASC),
     CONSTRAINT FK_CO_BitacoraPresupuesto_ArchivoAWS
