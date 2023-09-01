484e4bf3fe2642133161ccd808fcb7f66350dac9
Daniel AC <37602019+danielantoniocruz@users.noreply.github.com>
2023-09-01 09:58:19 -0600

    Se actualiza sp con nuevas columnas para mostrar proveedor en correo

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/MM_ACEP_ConsultarUsuarioVentas.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs1/MM_ACEP_ConsultarUsuarioVentas.sql
@@ -1,8 +1,22 @@
-﻿
+﻿USE [Petrovendor]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'MM_ACEP_ConsultarUsuarioVentas'
+)
+    DROP PROCEDURE MM_ACEP_ConsultarUsuarioVentas;
+/****** Object:  StoredProcedure [dbo].[MM_ACEP_ConsultarUsuarioVentas]    Script Date: 31/08/2023 05:40:59 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
 -- =============================================
 -- Author:		Daniel AC
--- Create date: 03-07-17
--- Description:	
+-- Create date: 01-09-2023
+-- Description:	Se agrega columnas para poder enviar nombre del proveedor y clasificar correos 
 -- =============================================
 -- Author:		Pedro Acuña
 -- Create date: 09-03-18
@@ -32,17 +46,31 @@ BEGIN
         U.IdTipoUsuario,
         U.IdUsuario,
         PV.IdProveedor,
-		U.IdTipoUsuario
-    FROM MM_AceptacionPedido AS AP
-        INNER JOIN MM_Pedido AS P
-            ON P.IdPedido = AP.IdPedido
-        INNER JOIN S_Proveedor AS PV
-            ON PV.IdProveedor = P.IdSubcontratista
-        INNER JOIN S_UsuarioProveedor AS UP
-            ON UP.IdProveedor = P.IdSubcontratista --AND UP.IdContrato = P.IdContrato
-        INNER JOIN S_Usuario AS U
-            ON U.IdUsuario = UP.IdUsuario
-    WHERE (U.IdTipoUsuario = 4 OR U.IdTipoUsuario = 3)
+		U.IdTipoUsuario,
+		ISNULL(U.Dominio,'') AS Dominio,
+		PV.RazonSocial AS Proveedor,
+		CASE WHEN UPPER(ISNULL(U.Dominio,'')) = 'ADINCO.MX' THEN 
+			1
+		ELSE 
+			0
+		END IsCorreoAdinco,
+		RC.PedirCarta, 
+		PC.RazonSocial AS Cliente
+    FROM MM_AceptacionPedido AS AP (NOLOCK)
+        JOIN MM_Pedido AS P (NOLOCK)
+            ON AP.IdPedido = P.IdPedido  
+		JOIN RelacionCartaCNPedido RC
+			ON P.IdPedido = RC.IdPedido
+			AND AP.IdAceptacionPedido = RC.IdAceptacionPedido
+        JOIN S_Proveedor AS PV (NOLOCK)
+            ON  P.IdSubcontratista = PV.IdProveedor
+        JOIN S_UsuarioProveedor AS UP (NOLOCK)
+            ON P.IdSubcontratista  = UP.IdProveedor
+        JOIN S_Usuario AS U (NOLOCK)
+            ON UP.IdUsuario = U.IdUsuario 
+		JOIN S_Proveedor AS PC (NOLOCK)
+            ON  P.IdProveedorCompras = PC.IdProveedor
+    WHERE (U.IdTipoUsuario = 4 OR U.IdTipoUsuario = 3) --> CTE USUARIO DE VENTA Y DE ADMISTRADOR
           AND U.Activo = 1
           AND AP.IdAceptacionPedido = @IdAceptacionPedido
           AND P.IdPedido = @IdPedido
@@ -50,6 +78,11 @@ BEGIN
              U.Correo,
              U.IdTipoUsuario,
              U.IdUsuario,
-             PV.IdProveedor;
+             PV.IdProveedor,
+			 PV.RazonSocial,
+			 U.Dominio,
+			 RC.PedirCarta,
+			 PC.RazonSocial;
 		  
 END;
+
