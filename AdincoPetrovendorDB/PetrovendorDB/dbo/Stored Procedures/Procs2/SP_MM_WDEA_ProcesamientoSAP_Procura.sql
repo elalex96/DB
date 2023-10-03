88377d4f7e730dff1deba68bd954002de73ad1c5
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2023-10-03 17:12:20 -0600

    Petrovendor/2469
    Se agrupan los destinatarios para no repetir

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_WDEA_ProcesamientoSAP_Procura.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_WDEA_ProcesamientoSAP_Procura.sql
@@ -105,6 +105,7 @@ BEGIN TRY
 	THEN 1 ELSE 0 END AS IsCorreoAdinco,
 	C.Destinatario
 	FROM WDEA_CorreosResumenProcesamiento AS C
+	GROUP BY C.Destinatario
 	-- SE CONCATENAN Y SE AGRUPAN LOS CORREOS DEPENDIENDO EL DOMINIO
 	INSERT INTO #CorreoConcat(
 	IsCorreoAdinco,
