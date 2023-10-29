3cf6747a526cc80e40704881aaf225e36a189229
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2023-10-29 10:19:03 -0600

    Ajustes

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/FI_RelacionaFacturaPuntoEntrega.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/FI_RelacionaFacturaPuntoEntrega.sql
@@ -1,11 +1,22 @@
-﻿CREATE PROCEDURE [dbo].[FI_RelacionaFacturaPuntoEntrega]
-	-- Add the parameters for the stored procedure here
-	@idFactura int,
-	@idPuntoEntrega int,
-	@idContrato int,
-	@idUsuario int,
-	@fechaMesDiaAnio date,
-	@hidrocarburo int
+﻿IF EXISTS
+    (
+        SELECT
+            1
+        FROM
+            dbo.sysobjects
+        WHERE
+            name = 'FI_RelacionaFacturaPuntoEntrega'
+    )
+    DROP PROCEDURE FI_RelacionaFacturaPuntoEntrega
+GO
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CREATE PROCEDURE [dbo].[FI_RelacionaFacturaPuntoEntrega]
+    @idFactura       int,
+    @idPuntoEntrega  int,
+    @idContrato      int,
+    @idUsuario       int,
+    @fechaMesDiaAnio date,
+    @hidrocarburo    int
 -- =============================================
 -- Author:		Reyna Olvera
 -- Create date: 13/04/2018
@@ -18,166 +29,197 @@
 --					si en la cromatografía hay volumen de n C5, I C5 Y C6+
 -- =============================================	
 AS
-BEGIN
-	SET NOCOUNT ON
+    BEGIN
+        SET NOCOUNT ON
 
-	CREATE TABLE #TipoHidrocarburo
-	(
-		IdTipoHidrocarburo	INT,
-		IdHidrocarburo		INT
-	)
+        CREATE TABLE #TipoHidrocarburo
+            (
+                IdTipoHidrocarburo INT,
+                IdHidrocarburo     INT
+            )
 
-	DECLARE @ContProduct INT
-    DECLARE @cont INT
-    DECLARE @GasNoAsociado as bit
+        DECLARE @ContProduct INT
+        DECLARE @cont INT
+        DECLARE @GasNoAsociado as bit
+        DECLARE
+            @HidrocarburoAceite INT = 1001, -- Aceite
+            @HidrocarburoGas    INT = 1000; -- Gas
 
 
-    select @GasNoAsociado  = GasNoAsociado     from CO_Contrato where IdContrato= @idContrato
+        select
+            @GasNoAsociado = GasNoAsociado
+        from
+            CO_Contrato (NOLOCK)
+        where
+            IdContrato = @idContrato;
 
-	IF @hidrocarburo = 1000	-- Gas
-	BEGIN
-		INSERT INTO #TipoHidrocarburo ( IdTipoHidrocarburo, IdHidrocarburo ) VALUES (10002, 1000)
-		INSERT INTO #TipoHidrocarburo ( IdTipoHidrocarburo, IdHidrocarburo ) VALUES (10003, 1000)
-		INSERT INTO #TipoHidrocarburo ( IdTipoHidrocarburo, IdHidrocarburo ) VALUES (10004, 1000)
-		INSERT INTO #TipoHidrocarburo ( IdTipoHidrocarburo, IdHidrocarburo ) VALUES (10005, 1000)
-		--IF @GasNoAsociado   = 1
-		--begin
-		--INSERT INTO #TipoHidrocarburo ( IdTipoHidrocarburo ) VALUES (10001)
-		--end
+        IF @hidrocarburo = @HidrocarburoGas -- Gas
+            BEGIN
+                INSERT INTO #TipoHidrocarburo
+                    (
+                        IdTipoHidrocarburo,
+                        IdHidrocarburo
+                    )
+                VALUES
+                    (
+                        10002, @HidrocarburoGas
+                    )
+                INSERT INTO #TipoHidrocarburo
+                    (
+                        IdTipoHidrocarburo,
+                        IdHidrocarburo
+                    )
+                VALUES
+                    (
+                        10003, @HidrocarburoGas
+                    )
+                INSERT INTO #TipoHidrocarburo
+                    (
+                        IdTipoHidrocarburo,
+                        IdHidrocarburo
+                    )
+                VALUES
+                    (
+                        10004, @HidrocarburoGas
+                    )
+                INSERT INTO #TipoHidrocarburo
+                    (
+                        IdTipoHidrocarburo,
+                        IdHidrocarburo
+                    )
+                VALUES
+                    (
+                        10005, @HidrocarburoGas
+                    )
+            END
+        ELSE
+            BEGIN
+                IF @hidrocarburo = @HidrocarburoAceite -- Aceite
+                    BEGIN
+                        INSERT INTO #TipoHidrocarburo
+                            (
+                                IdTipoHidrocarburo,
+                                IdHidrocarburo
+                            )
+                        VALUES
+                            (
+                                10000, @HidrocarburoAceite
+                            )
+                    END
+                ELSE
+                    BEGIN
+                        INSERT INTO #TipoHidrocarburo
+                            (
+                                IdTipoHidrocarburo,
+                                IdHidrocarburo
+                            )
+                        VALUES
+                            (
+                                10001, 1002
+                            )
+                    END
+            END
 
-	END
-	ELSE
-	BEGIN
-		IF @hidrocarburo = 1001	-- Aceite
-		BEGIN
-			INSERT INTO #TipoHidrocarburo ( IdTipoHidrocarburo, IdHidrocarburo ) VALUES (10000, 1001)
-        END
-		ELSE
-        BEGIN
-			INSERT INTO #TipoHidrocarburo ( IdTipoHidrocarburo, IdHidrocarburo ) VALUES (10001, 1002)
-        END
-	END
-    
-	-- SE BUSCAN LAS COMERCIALIZACIONES PARA EL PUNTO DE ENTREGA PARA ASIGNARLES LA FACTURA
-	
-	--/*Si el hidrocarburo es 1002 Condensable se actualiza la comercializacion con el bit=1 de EsCondensable*/
-	--IF @hidrocarburo = 1002
-	--BEGIN
-	--	UPDATE C	
-	--	SET IdFactura = @idFactura
-	--	FROM
-	--		COM_OperacionComercializacion	C
-	--	JOIN
-	--		#TipoHidrocarburo	T
-	--		ON	C.IdTipoHidrocarburo	=	T.IdTipoHidrocarburo
-	--	WHERE
-	--		IdContrato	=	@idContrato
-	--		AND	MesReporte	=	@fechaMesDiaAnio
-	--		AND PuntoEntregaID = @idPuntoEntrega
-	--		AND C.EsCondensable = 1
-	--END
-	--ELSE 
-	--BEGIN
-		--UPDATE C	
-		--	SET IdFactura = @idFactura
-		--FROM
-		--	COM_OperacionComercializacion	C
-		--JOIN
-		--	#TipoHidrocarburo	T
-		--	ON	C.IdTipoHidrocarburo	=	T.IdTipoHidrocarburo
-		--WHERE
-		--	IdContrato	=	@idContrato
-		--	AND	MesReporte	=	@fechaMesDiaAnio
-		--	AND PuntoEntregaID = @idPuntoEntrega
-    --END
+        -- SE BUSCAN LAS COMERCIALIZACIONES PARA EL PUNTO DE ENTREGA PARA ASIGNARLES LA FACTURA
+        SELECT
+            @ContProduct = idfacturaPuntoEntrega
+        FROM
+            FI_FacturaPuntoEntrega (NOLOCK)
+        WHERE
+            productoid = @hidrocarburo
+            and MesReporte = @fechaMesDiaAnio
+            and PuntoEntregaid = @idPuntoEntrega
 
-	--IF @hidrocarburo = 1000	-- Gas	SE LIGAN LAS FACTURAS DE CONDENSABLE
-	--BEGIN
-	--	UPDATE C	
-	--		SET IdFactura = @idFactura
-	--	FROM
-	--		COM_OperacionComercializacion	C
-	--	WHERE
-	--		IdContrato	=	@idContrato
-	--		AND	MesReporte	=	@fechaMesDiaAnio
-	--		AND PuntoEntregaID = @idPuntoEntrega
-	--		AND C.IdTipoHidrocarburo = 10001
-	--		AND C.EsCondensable = 1
-	--END
+        IF (@ContProduct >= 1) --Checa si ya hay producto facturado para esa fecha y ese punto de entrega, si no hay puede entrar para insertar con la factura
+            BEGIN
+                SELECT
+                    2
+            END
+        ELSE
+            BEGIN
+                SELECT
+                    @cont = idfacturaPuntoEntrega
+                FROM
+                    FI_FacturaPuntoEntrega FP (NOLOCK)
+                    JOIN
+                        FI_Factura         F (NOLOCK)
+                            on FP.idFactura = f.idFactura
+                WHERE
+                    FP.idFactura = @idFactura
+                    AND f.idContrato = @idContrato
 
-	SELECT @ContProduct=idfacturaPuntoEntrega 
-	FROM  FI_FacturaPuntoEntrega 
-	WHERE productoid=@hidrocarburo and MesReporte=@fechaMesDiaAnio and PuntoEntregaid=@idPuntoEntrega
+                IF (@cont >= 1)
+                    BEGIN
+                        --Checa si la factura ya esta insertada a algunos datos, si ya esta insertada,modifica los datos
+                        UPDATE
+                            FI_FacturaPuntoEntrega
+                        SET
+                            PuntoEntregaId = @idPuntoEntrega,
+                            ModificadoPor = @idUsuario,
+                            ModificadoEl = GETDATE(),
+                            ProductoId = @hidrocarburo,
+                            MesReporte = @fechaMesDiaAnio
+                        WHERE
+                            idFactura = @idFactura
+                        SELECT
+                            1
+                    END
+                ELSE
+                    BEGIN
+                        INSERT INTO FI_FacturaPuntoEntrega
+                            (
+                                idFactura,
+                                PuntoEntregaId,
+                                ProductoId,
+                                MesReporte,
+                                CreadoPor,
+                                CreadoEl,
+                                Activo
+                            )
+                        VALUES
+                            (
+                                @idFactura, @idPuntoEntrega, @hidrocarburo, @fechaMesDiaAnio, @idUsuario, GETDATE(), 1
+                            );
+                        SELECT
+                            1
+                    END
+            END
 
-	IF (@ContProduct>=1)--Checa si ya hay producto facturado para esa fecha y ese punto de entrega, si no hay puede entrar para insertar con la factura
-	BEGIN
-		SELECT 2
-	END
-	ELSE
-	BEGIN
-		SELECT @cont= idfacturaPuntoEntrega
-		FROM
-			FI_FacturaPuntoEntrega FP
-		JOIN
-			FI_Factura F on FP.idFactura =f.idFactura
-		WHERE
-			FP.idFactura	=	@idFactura
-			AND f.idContrato	=	@idContrato
+        -- SE LIGAN LAS FACTURAS CON LAS COMERCIALIZACIONES EN CASO DE QUE EXISTAN PARA GAS, PETROLEO Y CONDENSADO
+        UPDATE
+            C
+        SET
+            IdFactura = FP.idFactura
+        FROM
+            COM_OperacionComercializacion C
+            JOIN
+                #TipoHidrocarburo         T
+                    ON C.IdTipoHidrocarburo = T.IdTipoHidrocarburo
+            JOIN
+                FI_FacturaPuntoEntrega    FP
+                    ON C.PuntoEntregaID = FP.PuntoEntregaId
+                       AND C.MesReporte = FP.MesReporte
+                       AND T.IdHidrocarburo = FP.ProductoId
+        WHERE
+            C.IdContrato = @idContrato
+            AND C.MesReporte = @fechaMesDiaAnio
 
-		IF (@cont>=1)
-		BEGIN
-		--Checa si la factura ya esta insertada a algunos datos, si ya esta insertada,modifica los datos
-			UPDATE  FI_FacturaPuntoEntrega
-				SET PuntoEntregaId	=	@idPuntoEntrega,
-					ModificadoPor	=	@idUsuario,
-					ModificadoEl	=	GETDATE(),
-					ProductoId	=	@hidrocarburo,
-					MesReporte	=	@fechaMesDiaAnio
-			WHERE idFactura=@idFactura 
-				SELECT 1
-		END 
-		ELSE 
-		BEGIN 
-			INSERT INTO FI_FacturaPuntoEntrega (idFactura,PuntoEntregaId,ProductoId,MesReporte,CreadoPor,CreadoEl,Activo )
-			VALUES (@idFactura,@idPuntoEntrega,@hidrocarburo,@fechaMesDiaAnio,@idUsuario,GETDATE(),1);
-				SELECT 1
-		END
-	END
+        -- SE LIGAN LAS FACTURAS DE CONDENSABLE
+        UPDATE
+            C
+        SET
+            IdFactura = FP.idFactura
+        FROM
+            COM_OperacionComercializacion C
+            JOIN
+                FI_FacturaPuntoEntrega    FP
+                    ON C.PuntoEntregaID = FP.PuntoEntregaId
+                       AND C.MesReporte = FP.MesReporte
+                       AND FP.ProductoId = 1000
+        WHERE
+            C.IdContrato = @idContrato
+            AND C.MesReporte = @fechaMesDiaAnio
+            AND C.IdTipoHidrocarburo = 10001
+            AND C.EsCondensable = 1
 
--- SE LIGAN LAS FACTURAS CON LAS COMERCIALIZACIONES EN CASO DE QUE EXISTAN PARA GAS, PETROLEO Y CONDENSADO
-	UPDATE C	
-		SET IdFactura = FP.idFactura
-	FROM
-		COM_OperacionComercializacion	C
-	JOIN
-		#TipoHidrocarburo	T
-		ON	C.IdTipoHidrocarburo	=	T.IdTipoHidrocarburo
-	JOIN
-		FI_FacturaPuntoEntrega	FP
-		ON	C.PuntoEntregaID	=	FP.PuntoEntregaId
-		AND	C.MesReporte	=	FP.MesReporte
-		AND	T.IdHidrocarburo	=	FP.ProductoId
-	WHERE
-		C.IdContrato	=	@idContrato
-		AND	C.MesReporte	=	@fechaMesDiaAnio
---			AND C.PuntoEntregaID = @idPuntoEntrega
-
-	-- SE LIGAN LAS FACTURAS DE CONDENSABLE
-	UPDATE C	
-		SET IdFactura = FP.idFactura
-	FROM
-		COM_OperacionComercializacion	C
-	JOIN
-		FI_FacturaPuntoEntrega	FP
-		ON	C.PuntoEntregaID	=	FP.PuntoEntregaId
-		AND	C.MesReporte	=	FP.MesReporte
-		AND	FP.ProductoId	=	1000
-	WHERE
-		C.IdContrato	=	@idContrato
-		AND	C.MesReporte	=	@fechaMesDiaAnio
-		--AND C.PuntoEntregaID = @idPuntoEntrega
-		AND C.IdTipoHidrocarburo = 10001
-		AND C.EsCondensable = 1
-
-END
-
+    END
