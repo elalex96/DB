30bada3f81f815687355a2a9275557100ba60c53
Daniel Antonio <37602019+danielantoniocruz@users.noreply.github.com>
2023-10-30 15:52:45 -0600

    Se modifica consulta para visualizar aprobadores

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_RPT_OCM_AprobadoresPorPedido.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_RPT_OCM_AprobadoresPorPedido.sql
@@ -7,21 +7,23 @@ IF EXISTS
     WHERE name = 'SP_RPT_OCM_AprobadoresPorPedido'
 )
     DROP PROCEDURE SP_RPT_OCM_AprobadoresPorPedido;
-GO
+	/****** Object:  StoredProcedure [dbo].[sp_ENT_DocumentosUltimaVersion]    Script Date: 20/09/2023 01:36:09 p. m. ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+/****** Object:  StoredProcedure [dbo].[SP_RPT_OCM_AprobadoresPorPedido]    Script Date: 30/10/2023 03:35:28 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
 -- =============================================
 -- Author: Daniel AC
--- Create date: 12-04-2022
--- Description:	 Se cambian los aprobadores mostrados por los aprobadores de pedido, mostrando la columna Estatus
+-- Create date: 30-10-2023
+-- Description:	 Se cambian los aprobadores mostrados por los aprobadores de pedido, mostrando la columna Estatus y que se muestren aunque este en aprobación
 -- =============================================
--- Author:		Alexander Gomez
--- Create date: 10/10/2023
--- Description:	se agregan estandares de desarrollo
--- =============================================
-CREATE  PROCEDURE [dbo].[SP_RPT_OCM_AprobadoresPorPedido]
+CREATE PROCEDURE [dbo].[SP_RPT_OCM_AprobadoresPorPedido]
 	@IdPedido INT
 AS
 BEGIN
@@ -29,26 +31,23 @@ BEGIN
 		'Aprobador ' + CAST(T.NoSecuencia AS NVARCHAR(max)) AS NoAprobador,
 		T.NoSecuencia, 
 		U.Nombre, 
-		t.FechaCambioEstatus, 
-		t.IdTarea,
-		t.IdFirma,
+		T.FechaCambioEstatus, 
+		T.IdTarea,
+		CASE WHEN T.IdEstatus <> 1 THEN T.IdFirma ELSE '' END IdFirma,
 		tae.Nombre as Estatus
 	FROM TA_Tarea AS T		
 		INNER JOIN TA_Operacion AS TOO (NOLOCK)
 			ON T.IdOperacion = TOO.IdOperacion
 		INNER JOIN dbo.MM_SolicitudPedido AS SP (NOLOCK)
 			ON TOO.IdDocumento = SP.IdSolicitudPedido 
-		INNER JOIN dbo.MM_Pedido AS p (NOLOCK)
-			ON sp.IdSolicitudPedido = p.IdSolicitudPedido  
-			AND  too.NoVersion = p.Version
-		INNER JOIN S_Usuario AS U (NOLOCK)
-			on T.IdAprobador = u.IdUsuario 
-		INNER JOIN TA_Estatus AS TAE (NOLOCK)
+		INNER JOIN dbo.MM_Pedido AS P (NOLOCK)
+			ON SP.IdSolicitudPedido = P.IdSolicitudPedido  
+			AND  TOO.NoVersion = P.Version
+		INNER JOIN S_Usuario AS U  (NOLOCK)
+			on T.IdAprobador = U.IdUsuario 
+		INNER JOIN TA_Estatus AS TAE  (NOLOCK)
 			ON T.IdEstatus = TAE.IdEstatus 
-	WHERE p.IdPedido = @IdPedido 
-		AND t.IdEstatus = 2 --> CTE APROBADO
+	WHERE p.IdPedido = @IdPedido 	
 		AND TOO.IdTipoOperacion = 9 --> CTE APROBACION DE PEDIDO
 	ORDER BY NoSecuencia ASC 		
 END
-
-
