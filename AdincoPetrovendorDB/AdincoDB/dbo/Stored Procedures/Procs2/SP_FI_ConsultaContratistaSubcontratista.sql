0733aa02a93b8bd7acaf5dc0b007300e21bc386f
Pedro Pouchoulen <38993356+PedroPou@users.noreply.github.com>
2023-11-04 22:51:27 -0600

    Issue/Adinco/2744-Finanzas

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_ConsultaContratistaSubcontratista.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_ConsultaContratistaSubcontratista.sql
@@ -1,36 +1,29 @@
-﻿-- =============================================
+﻿
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_FI_ConsultaContratistaSubcontratista'
+)
+    DROP PROCEDURE SP_FI_ConsultaContratistaSubcontratista
+GO
+-- =============================================
 -- Author:		Daniel AC
 -- Create date: 24/05/2017
 -- Description:	Obtiene la razon social del subcontratista
 -- =============================================
 CREATE PROCEDURE [dbo].[SP_FI_ConsultaContratistaSubcontratista] 
--- Add the parameters for the stored procedure here
 @IdContratista INT
 AS
      BEGIN
-         -- SET NOCOUNT ON added to prevent extra result sets from
-         -- interfering with SELECT statements.
+
          SET NOCOUNT ON;
-
-         -- Insert statements for procedure here
-         --SELECT S.IdSubcontratista, S.RazonSocial FROM CO_Contratista AS C
-         --INNER JOIN  FI_Factura F   On f.id   PV_Subcontratista AS S ON S.IdSubcontratista = C.IdProveedor
-         ----WHERE IdContratista = @IdContratista
-
-             /*SELECT S.IdSubcontratista,
-                    S.RazonSocial
-             FROM PV_Subcontratista AS S
-             WHERE S.RazonSocial <> ''
-             ORDER BY S.RazonSocial ASC;*/
 
          SELECT DISTINCT 
                 S.IdSubcontratista, 
                 UPPER(S.RazonSocial) AS RazonSocial, 
                 S.RFC
-         FROM PV_Subcontratista AS S
-         --FROM CO_Contrato C
-         --LEFT OUTER JOIN FI_Factura F ON C.IdContrato = f.IdContrato
-         --JOIN PV_Subcontratista AS S ON S.IdSubcontratista = F.IdSubcontratista
+         FROM PV_Subcontratista AS S (NOLOCK)
          WHERE S.RazonSocial <> ''
                AND S.RazonSocial <> '-'
                AND S.RFC IS NOT NULL
@@ -39,5 +32,4 @@ AS
                AND S.IsEliminado = 0
                AND S.IsActivo = 1
          ORDER BY UPPER(S.RazonSocial);
-         ----WHERE IdContratista = @IdContratista
      END;
--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_ConsultaCuentasBancarias.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_ConsultaCuentasBancarias.sql
@@ -1,18 +1,23 @@
-﻿-- =============================================
+﻿
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_FI_ConsultaCuentasBancarias'
+)
+    DROP PROCEDURE SP_FI_ConsultaCuentasBancarias
+GO
+-- =============================================
 -- Author:		Daniel AC
 -- Create date: 24/05/2017
 -- Description:	Obtiene la razon social del subcontratista
 -- =============================================
 CREATE PROCEDURE [dbo].[SP_FI_ConsultaCuentasBancarias] 
-	-- Add the parameters for the stored procedure here
 @IdContratista INT
 AS
          BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
              SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
              SELECT CB.DatoBancarioID,
                     S.RazonSocial,
                     CB.Titular,
@@ -24,10 +29,12 @@ AS
                     CB.IdTipoCuenta,
                     CB.Predeterminado,
                     CB.TipoMonedaID
-             FROM CO_Contratista AS C
-                  INNER JOIN PV_Subcontratista AS S ON S.IdSubcontratista = C.IdProveedor
-                  INNER JOIN PV_CuentaBancaria AS CB ON CB.IdProveedor = S.IdSubcontratista
-             WHERE C.IdContratista = @IdContratista;
+             FROM CO_Contratista AS C (NOLOCK)
+                  INNER JOIN PV_Subcontratista AS S (NOLOCK) 
+					ON S.IdSubcontratista = C.IdProveedor
+                  INNER JOIN PV_CuentaBancaria AS CB (NOLOCK)
+					ON CB.IdProveedor = S.IdSubcontratista
+             WHERE C.IdContratista = @IdContratista
+			 ORDER BY S.RazonSocial
          END;
-
 
