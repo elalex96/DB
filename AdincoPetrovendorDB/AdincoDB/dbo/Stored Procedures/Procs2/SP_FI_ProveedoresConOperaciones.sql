c9ce074ac52e3d9494aa3f430911eef9be84bff5
Neri del Angel <48134542+MarcosGarcia93@users.noreply.github.com>
2023-11-07 14:04:49 -0600

    Ajustes de sps

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_ProveedoresConOperaciones.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FI_ProveedoresConOperaciones.sql
@@ -1,4 +1,13 @@
-﻿-- =============================================
+﻿IF EXISTS
+(
+    SELECT 1
+    FROM sysobjects
+    WHERE name = 'SP_FI_ProveedoresConOperaciones'
+)
+    DROP PROCEDURE SP_FI_ProveedoresConOperaciones;
+GO
+
+-- =============================================
 -- Author:		Marcos Garcia
 -- Create Date: 05-12-2019
 -- Description:	Seleccion de Proveedores con 
@@ -8,88 +17,155 @@
 -- Alter Date:  06-01-2020
 -- Description:	Se agrega RFC en la Razon Social
 -- =============================================
-CREATE PROCEDURE [dbo].[SP_FI_ProveedoresConOperaciones] 
--- [SP_FI_ProveedoresConOperaciones] 3, 10002,0
--- Add the parameters for the stored procedure here
-@IdContrato INT, 
-@IdUsuario  INT, 
-@FactComp   INT
+CREATE PROCEDURE [dbo].[SP_FI_ProveedoresConOperaciones]
+    @IdContrato INT,
+    @IdUsuario INT,
+    @FactComp INT
 AS
-     BEGIN
-         -- SET NOCOUNT ON added to prevent extra result sets from
-         -- interfering with SELECT statements.
-         SET NOCOUNT ON;
-         --==================== IdContratista del Contrato ================
-         DECLARE @IdContratista INT;
-         --=========================
-         SET @IdContratista =
-         (
-             SELECT IdContratista
-             FROM dbo.CO_Contrato
-             WHERE IdContrato = @IdContrato
-         );
-         --============== Contratos Relacionados al Contratista ===========
-         IF OBJECT_ID('tempdb..#ContratosTempo', 'U') IS NOT NULL
-             DROP TABLE #ContratosTempo;
-         --=============================
-         CREATE TABLE #ContratosTempo
-         (IdContrato INT
-         );
-         --=============================
-         --Contratistas Jaguar
-         IF(@IdContratista = 10005
-            OR @IdContratista = 10006)
-		
-             BEGIN
-                 INSERT INTO #ContratosTempo(IdContrato)
-                        SELECT IdContrato
-                        FROM dbo.CO_Contrato
-                        WHERE IdContratista IN(10005, 10006)
-                        AND Activo = 1;
-             END; 
-         --Todos los demas Contratista         
-		ELSE        
-             BEGIN
-                 INSERT INTO #ContratosTempo(IdContrato)
-                        SELECT IdContrato
-                        FROM dbo.CO_Contrato
-                        WHERE IdContratista = @IdContratista
-                              AND Activo = 1;
-             END;
+BEGIN
+    SET NOCOUNT ON;
 
-         --=========== Seleccion de Proveedores por Contratos ============== 
-         --======= Por Facturas =======		
-         IF(@FactComp = 0)
-             BEGIN
-                 SELECT SC.IdSubcontratista, 
-				UPPER(CONCAT(SC.RazonSocial,' - ', SC.RFC)) AS RazonSocial,                         
-                        SC.RFC
+    --
+    IF OBJECT_ID('tempdb..#TemporalContratos', 'U') IS NOT NULL
+        DROP TABLE #TemporalContratos;
 
-                 FROM dbo.FI_Factura F
-                      LEFT JOIN Adinco.dbo.PV_Subcontratista SC ON SC.IdSubcontratista = F.IdSubcontratista
-                      INNER JOIN #ContratosTempo T ON F.IdContrato = T.IdContrato
-                 WHERE ISNULL(SC.IsEliminado, 0) = 0
-                       AND ISNULL(SC.IsActivo, 0) = 1
-                 GROUP BY UPPER(CONCAT(SC.RazonSocial,' - ', SC.RFC)),
-                          SC.IdSubcontratista,
-                          SC.RFC
-                 ORDER BY UPPER(CONCAT(SC.RazonSocial,' - ', SC.RFC));
-             END;
-         --===== Por Complementos =====			
-         IF(@FactComp <> 0)
-             BEGIN
-                 SELECT SC.IdSubcontratista, 
-				 UPPER(CONCAT(SC.RazonSocial,' - ', SC.RFC)) AS RazonSocial,                         
-                        SC.RFC
-                 FROM dbo.FI_Factura F
-                      LEFT JOIN Adinco.dbo.PV_Subcontratista SC ON SC.IdSubcontratista = F.IdSubcontratista
-                      INNER JOIN #ContratosTempo T ON F.IdContrato = T.IdContrato
-                 WHERE ISNULL(SC.IsEliminado, 0) = 0
-                       AND ISNULL(SC.IsActivo, 0) = 1
-                       AND F.TipoComprobante = 'P'
-                GROUP BY UPPER(CONCAT(SC.RazonSocial,' - ', SC.RFC)),
-                         SC.IdSubcontratista,
-                         SC.RFC
-                 ORDER BY UPPER(CONCAT(SC.RazonSocial,' - ', SC.RFC));
-             END;
-     END;
+    --
+    DECLARE @IdContratista INT;
+
+    --
+    CREATE TABLE #TemporalContratos (IdContrato INT);
+
+    --
+    SET @IdContratista =
+    (
+        SELECT IdContratista
+        FROM CO_Contrato (NOLOCK)
+        WHERE IdContrato = @IdContrato
+    );
+
+    /*Contratistas Jaguar*/
+    IF (@IdContratista = 10005 OR @IdContratista = 10006)
+    BEGIN
+        INSERT INTO #TemporalContratos
+        (
+            IdContrato
+        )
+        SELECT IdContrato
+        FROM CO_Contrato (NOLOCK)
+        WHERE IdContratista IN ( 10005, 10006 )
+              AND ISNULL(Activo, 0) = 1;
+    END;
+    /*Todos los demas Contratista*/
+    ELSE
+    BEGIN
+        INSERT INTO #TemporalContratos
+        (
+            IdContrato
+        )
+        SELECT IdContrato
+        FROM CO_Contrato (NOLOCK)
+        WHERE IdContratista = @IdContratista
+              AND ISNULL(Activo, 0) = 1;
+    END;
+
+    /*Selección  de Proveedores por Contratos*/
+    /*Por Facturas*/
+    IF (@FactComp = 0)
+    BEGIN
+        SELECT PV_Subcontratista.IdSubcontratista,
+               CASE
+                   WHEN ISNULL(PV_Subcontratista.RFC, '') = '' THEN
+                       UPPER(ISNULL(PV_Subcontratista.RazonSocial, ''))
+                   WHEN ISNULL(PV_Subcontratista.RazonSocial, '') = '' THEN
+                       UPPER(ISNULL(PV_Subcontratista.RFC, ''))
+                   ELSE
+                       UPPER(CONCAT(ISNULL(PV_Subcontratista.RazonSocial, ''), ' - ', ISNULL(PV_Subcontratista.RFC, '')))
+               END AS RazonSocial,
+               ISNULL(PV_Subcontratista.RFC, '') AS RFC
+        FROM #TemporalContratos
+            INNER JOIN FI_Factura (NOLOCK)
+                ON #TemporalContratos.IdContrato = FI_Factura.IdContrato
+            INNER JOIN PV_Subcontratista (NOLOCK)
+                ON FI_Factura.IdSubcontratista = PV_Subcontratista.IdSubcontratista
+                   AND ISNULL(PV_Subcontratista.IsEliminado, 0) = 0
+                   AND ISNULL(PV_Subcontratista.IsActivo, 0) = 1
+        GROUP BY PV_Subcontratista.IdSubcontratista,
+                 CASE
+                     WHEN ISNULL(PV_Subcontratista.RFC, '') = '' THEN
+                         UPPER(ISNULL(PV_Subcontratista.RazonSocial, ''))
+                     WHEN ISNULL(PV_Subcontratista.RazonSocial, '') = '' THEN
+                         UPPER(ISNULL(PV_Subcontratista.RFC, ''))
+                     ELSE
+                         UPPER(CONCAT(
+                                         ISNULL(PV_Subcontratista.RazonSocial, ''),
+                                         ' - ',
+                                         ISNULL(PV_Subcontratista.RFC, '')
+                                     )
+                              )
+                 END,
+                 PV_Subcontratista.RFC
+        ORDER BY CASE
+                     WHEN ISNULL(PV_Subcontratista.RFC, '') = '' THEN
+                         UPPER(ISNULL(PV_Subcontratista.RazonSocial, ''))
+                     WHEN ISNULL(PV_Subcontratista.RazonSocial, '') = '' THEN
+                         UPPER(ISNULL(PV_Subcontratista.RFC, ''))
+                     ELSE
+                         UPPER(CONCAT(
+                                         ISNULL(PV_Subcontratista.RazonSocial, ''),
+                                         ' - ',
+                                         ISNULL(PV_Subcontratista.RFC, '')
+                                     )
+                              )
+                 END;
+    END;
+    /*Por Complementos*/
+    ELSE
+    BEGIN
+        SELECT PV_Subcontratista.IdSubcontratista,
+               CASE
+                   WHEN ISNULL(PV_Subcontratista.RFC, '') = '' THEN
+                       UPPER(ISNULL(PV_Subcontratista.RazonSocial, ''))
+                   WHEN ISNULL(PV_Subcontratista.RazonSocial, '') = '' THEN
+                       UPPER(ISNULL(PV_Subcontratista.RFC, ''))
+                   ELSE
+                       UPPER(CONCAT(ISNULL(PV_Subcontratista.RazonSocial, ''), ' - ', ISNULL(PV_Subcontratista.RFC, '')))
+               END AS RazonSocial,
+               ISNULL(PV_Subcontratista.RFC, '') AS RFC
+        FROM #TemporalContratos
+            INNER JOIN FI_Factura (NOLOCK)
+                ON #TemporalContratos.IdContrato = FI_Factura.IdContrato
+                   AND FI_Factura.TipoComprobante = 'P'
+            INNER JOIN PV_Subcontratista (NOLOCK)
+                ON FI_Factura.IdSubcontratista = PV_Subcontratista.IdSubcontratista
+                   AND ISNULL(PV_Subcontratista.IsEliminado, 0) = 0
+                   AND ISNULL(PV_Subcontratista.IsActivo, 0) = 1
+        GROUP BY PV_Subcontratista.IdSubcontratista,
+                 CASE
+                     WHEN ISNULL(PV_Subcontratista.RFC, '') = '' THEN
+                         UPPER(ISNULL(PV_Subcontratista.RazonSocial, ''))
+                     WHEN ISNULL(PV_Subcontratista.RazonSocial, '') = '' THEN
+                         UPPER(ISNULL(PV_Subcontratista.RFC, ''))
+                     ELSE
+                         UPPER(CONCAT(
+                                         ISNULL(PV_Subcontratista.RazonSocial, ''),
+                                         ' - ',
+                                         ISNULL(PV_Subcontratista.RFC, '')
+                                     )
+                              )
+                 END,
+                 PV_Subcontratista.RFC
+        ORDER BY CASE
+                     WHEN ISNULL(PV_Subcontratista.RFC, '') = '' THEN
+                         UPPER(ISNULL(PV_Subcontratista.RazonSocial, ''))
+                     WHEN ISNULL(PV_Subcontratista.RazonSocial, '') = '' THEN
+                         UPPER(ISNULL(PV_Subcontratista.RFC, ''))
+                     ELSE
+                         UPPER(CONCAT(
+                                         ISNULL(PV_Subcontratista.RazonSocial, ''),
+                                         ' - ',
+                                         ISNULL(PV_Subcontratista.RFC, '')
+                                     )
+                              )
+                 END;
+    END;
+END;
