ef9432f74932684117ea0295228bbe8918bb51e1
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2023-11-21 14:27:40 -0600

    optimizaciones y correcciones al sp SP_MM_ValidacionEdicionPedido

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_ValidacionEdicionPedido.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_ValidacionEdicionPedido.sql
@@ -1,9 +1,27 @@
-﻿-- =============================================
+﻿USE [Petrovendor]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_MM_ValidacionEdicionPedido'
+)
+    DROP PROCEDURE SP_MM_ValidacionEdicionPedido;
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
 -- Author:		Alexander Gomez
 -- Create date: 13/10/2022
 -- Description: Validacion de edicion de pedido
 -- =============================================
-CREATE PROCEDURE [dbo].[SP_MM_ValidacionEdicionPedido]
+-- Author:		Alexander Gomez
+-- Create date: 21/11/2023
+-- Description: Mejora en validacion
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_MM_ValidacionEdicionPedido] 
 	-- Add the parameters for the stored procedure here
 	 @IdPedido INT
 AS
@@ -15,40 +33,50 @@ BEGIN
     -- Insert statements for procedure here
 	DECLARE @PROCESOS_EN_CURSO INT = 0;
 
+	CREATE TABLE #PROCESOS_PENDIENTES(
+		Id INT PRIMARY KEY IDENTITY(1,1),
+		IdProceso INT,
+		Tipo NVARCHAR(20)
+	);
+
 	--SE OBTIENEN LA CANTIDAD DE CARTAS CN RELACIONADAS AL PEDIDO
-	DECLARE @CN INT = (SELECT
-							COUNT(ACN.IdAceptacionCartaPCN)
-						FROM MM_AceptacionCartaPCN AS ACN
-							JOIN MM_AceptacionPedido AS AP
-								ON ACN.IdAceptacionPedido = AP.IdAceptacionPedido
-							JOIN MM_Pedido AS P
-								ON AP.IdPedido = P.IdPedido
-						WHERE P.IdPedido = @IdPedido
-						GROUP BY ACN.IdAceptacionCartaPCN);
-
 	--SE OBTIENE LA CANTIDAD DE FACTURAS ASOCIADAS AL PEDIDO
-	DECLARE @FACTURAS INT = (SELECT
-								COUNT(AF.IdAceptacionFactura)
-							FROM MM_AceptacionFactura AS AF
-								JOIN MM_AceptacionPedido AS AP
-									ON AF.IdAceptacionPedido = AP.IdAceptacionPedido
-								JOIN MM_Pedido AS P
-									ON AP.IdPedido = P.IdPedido
-							WHERE P.IdPedido = @IdPedido
-							GROUP BY AF.IdAceptacionFactura);
-
 	--SE OBTIENE LA CANTIDAD DE COMPROBANTES EXTRANJEROS
-	DECLARE @COMPROBANTES INT = (SELECT
-								COUNT(AF.IdAceptacionPedidoPedimentoComprobante)
-							FROM FI_AceptacionPedido_PedimentoComprobante AS AF
-								JOIN MM_AceptacionPedido AS AP
-									ON AF.IdAceptacionPedido = AP.IdAceptacionPedido
-								JOIN MM_Pedido AS P
-									ON AP.IdPedido = P.IdPedido
-							WHERE P.IdPedido = @IdPedido
-							GROUP BY AF.IdAceptacionPedidoPedimentoComprobante);
+	INSERT INTO #PROCESOS_PENDIENTES (IdProceso,Tipo)
+	SELECT
+		ACN.IdAceptacionCartaPCN,
+		'CARTACN'
+	FROM MM_AceptacionCartaPCN AS ACN (NOLOCK)
+		JOIN MM_AceptacionPedido AS AP (NOLOCK)
+			ON ACN.IdAceptacionPedido = AP.IdAceptacionPedido
+			AND ACN.Activo = 1
+		JOIN MM_Pedido AS P (NOLOCK)
+			ON AP.IdPedido = P.IdPedido
+	WHERE P.IdPedido = @IdPedido
+	UNION
+	SELECT
+		AF.IdAceptacionFactura,
+		'FACTURA'
+	FROM MM_AceptacionFactura AS AF (NOLOCK)
+		JOIN MM_AceptacionPedido AS AP (NOLOCK)
+			ON AF.IdAceptacionPedido = AP.IdAceptacionPedido
+			AND AF.IdEstatusEliminado IS NULL
+		JOIN MM_Pedido AS P (NOLOCK)
+			ON AP.IdPedido = P.IdPedido
+	WHERE P.IdPedido = @IdPedido
+	UNION
+	SELECT
+		AF.IdAceptacionPedidoPedimentoComprobante,
+		'PEDIMENTO/COMPROBANTE'
+	FROM FI_AceptacionPedido_PedimentoComprobante AS AF (NOLOCK)
+		JOIN MM_AceptacionPedido AS AP (NOLOCK)
+			ON AF.IdAceptacionPedido = AP.IdAceptacionPedido
+			AND AF.Activo = 1
+		JOIN MM_Pedido AS P (NOLOCK)
+			ON AP.IdPedido = P.IdPedido
+	WHERE P.IdPedido = @IdPedido;
 
-	SET @PROCESOS_EN_CURSO = ISNULL(@CN,0) + ISNULL(@FACTURAS,0) + ISNULL(@COMPROBANTES,0);
+	SET @PROCESOS_EN_CURSO = (SELECT COUNT(Id) FROM #PROCESOS_PENDIENTES);
 
 	IF @PROCESOS_EN_CURSO > 0
 	BEGIN
