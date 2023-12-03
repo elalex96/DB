dcb1bcab8897f2e652d4602fa0d81e03e8d7372e
Pedro Pouchoulen <38993356+PedroPou@users.noreply.github.com>
2023-12-03 08:09:11 -0600

    Issue/Adinco/2747-Refactura

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FACTURAS_FACTURASHIJODEFACTURASPADRE.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/SP_FACTURAS_FACTURASHIJODEFACTURASPADRE.sql
@@ -1,11 +1,43 @@
-﻿--Created by: Luis David De La Cruz Bautista
-CREATE PROCEDURE [dbo].[SP_FACTURAS_FACTURASHIJODEFACTURASPADRE]
-@IdFacturaPadre int
+﻿IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'SP_FACTURAS_FACTURASHIJODEFACTURASPADRE'
+)
+    DROP PROCEDURE SP_FACTURAS_FACTURASHIJODEFACTURASPADRE;
+GO
+CREATE PROCEDURE [dbo].[SP_FACTURAS_FACTURASHIJODEFACTURASPADRE] 
+	@IdFacturaPadre INT,
+	@IdSubcontratista INT,
+	@FacturaPedimento VARCHAR(100)
 AS
 BEGIN
-	SELECT 
-	IdFacturahijo,IdFacturaPadre 
-	FROM FI_RelacionRefacturas 
-	WHERE idFacturaPadre = @IdFacturaPadre
-END;
+    SET NOCOUNT ON;
 
+	IF(@FacturaPedimento = 'Factura')
+	BEGIN
+		SELECT IdFacturahijo,
+			   IdFacturaPadre
+		FROM FI_RelacionRefacturas
+		INNER JOIN FI_Factura
+		ON FI_RelacionRefacturas.idFacturaPadre = @idFacturaPadre
+		AND FI_RelacionRefacturas.idFacturaPadre = FI_Factura.IdFactura
+		INNER JOIN FI_Factura Fhijo
+		ON FI_RelacionRefacturas.idFacturaHijo = Fhijo.IdFactura
+		WHERE Fhijo.IdSubcontratista = @IdSubcontratista and idFacturaPadre = @idFacturaPadre
+	END
+
+	IF(@FacturaPedimento = 'Pedimento')
+	BEGIN
+		SELECT IdPedimentoHijo,
+			   IdFacturaPadre
+		FROM FI_RelacionPedimento
+		INNER JOIN FI_Factura
+		ON FI_RelacionPedimento.idFacturaPadre = @idFacturaPadre
+		AND FI_RelacionPedimento.idFacturaPadre = FI_Factura.IdFactura
+		INNER JOIN FI_PedimentoComprobante
+		ON FI_RelacionPedimento.IdPedimentoHijo = FI_PedimentoComprobante.IdPedimentoComprobante
+		AND FI_PedimentoComprobante.IdSubcontratistaExportador = @IdSubcontratista
+		WHERE FI_PedimentoComprobante.IdSubcontratistaExportador = @IdSubcontratista AND FI_RelacionPedimento.IdFacturaPadre = @idFacturaPadre
+	END
+END;
