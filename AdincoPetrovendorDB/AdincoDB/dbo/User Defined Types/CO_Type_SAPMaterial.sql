8145546656bbb567799a64f78c5910a92fcbcb49
Neri del Angel <48134542+MarcosGarcia93@users.noreply.github.com>
2023-12-04 17:30:14 -0600

    Issue/Adinco/2873-Agregado de type con clase de SDAPMAterial, sps con agregado y actualizacion de materiales y sp con update de activo = 0

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/User Defined Types/CO_Type_SAPMaterial.sql
@@ -0,0 +1,10 @@
+﻿CREATE TYPE CO_Type_SAPMaterial AS TABLE
+(
+    [NumeroFila] INT NULL,
+    [MaterialDescription] VARCHAR(150) NULL,
+    [MaterialLongText] VARCHAR(500) NULL,
+    [BaseUnitOfMeasure] VARCHAR(50) NULL,
+    [SAPMaterialNumber] VARCHAR(20) NULL,
+    [Plant] VARCHAR(15) NULL,
+    [KeyLastImport] VARCHAR(50) NULL
+);
