e0e42bdfbb6bcece37d93cdbfc5bb2dcbc463455
Alexander Gomez <38989750+AlexanderGE9@users.noreply.github.com>
2024-01-04 11:36:17 -0600

    Consultar flujo de aprobacion de pedimento comprobante con el idoperacion

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs3/USP_SEL_MOBILE_ConsultaFlujoAprobacion.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs3/USP_SEL_MOBILE_ConsultaFlujoAprobacion.sql
@@ -21,7 +21,11 @@ GO
 -- Create date: <30/11/2023>
 -- Description:	<Consultar flujo de aprobacion de compra directa con el idoperacion>
 -- =============================================
-CREATE PROCEDURE [dbo].[USP_SEL_MOBILE_ConsultaFlujoAprobacion]
+-- Author:		<Alexander Gomez>
+-- Create date: <03/01/2024>
+-- Description:	<Consultar flujo de aprobacion de pedimento comprobante con el idoperacion>
+-- =============================================
+CREATE PROCEDURE [dbo].[USP_SEL_MOBILE_ConsultaFlujoAprobacion] --114332,14
 	-- Add the parameters for the stored procedure here
 	@IdOperacion INT,
 	@TipoOperacion INT,
@@ -90,7 +94,7 @@ BEGIN
 				WHEN T.IdEstatus = 3 THEN '#c0392b'
 				WHEN T.IdEstatus = 4 THEN '#c0392b'
 				ELSE '#ebedef'
-			END AS ColorBackground,
+			END AS Color,
 			T.IdTarea,
 			OP.IdEstatusOperacion,
 			US.Nombre,
@@ -103,7 +107,7 @@ BEGIN
 				WHEN OP.IdEstatusOperacion = 3 THEN '#ed594a'
 				WHEN OP.IdEstatusOperacion = 4 THEN '#c0392b'
 				ELSE '#ebede'
-			END AS Color,
+			END AS ColorBackground,
 			CASE
 				WHEN T.IdEstatus = 1 THEN 'estatus_enaprobacion.png'
 				WHEN T.IdEstatus = 2 THEN 'estatus_aprobado.png'
@@ -125,6 +129,56 @@ BEGIN
 			INNER JOIN Petrovendor..TA_Estatus AS EFT
 				ON OP.IdEstatusOperacion = EFT.IdEstatus
 		ORDER BY T.NoSecuencia ASC;
+
+	END
+
+
+	IF @TipoOperacion IN (19)--PEDIMENTO COMPROBANTE
+	BEGIN
+
+		SELECT
+			EFT.Nombre AS EstatusOperacion,
+			CASE
+				WHEN T.IdEstatus = 1 THEN '#f4d03f'
+				WHEN T.IdEstatus = 2 THEN '#2ecc71'
+				WHEN T.IdEstatus = 3 THEN '#c0392b'
+				WHEN T.IdEstatus = 4 THEN '#c0392b'
+				ELSE '#ebedef'
+			END AS Color,
+			T.IdTarea,
+			OP.IdEstatusOperacion,
+			US.Nombre,
+			US.Correo,
+			T.IdEstatus,
+			E.Nombre AS Estatus,
+			CASE
+				WHEN OP.IdEstatusOperacion = 1 THEN '#e6ca5c'
+				WHEN OP.IdEstatusOperacion = 2 THEN '#36eb83'
+				WHEN OP.IdEstatusOperacion = 3 THEN '#ed594a'
+				WHEN OP.IdEstatusOperacion = 4 THEN '#c0392b'
+				ELSE '#ebede'
+			END AS ColorBackground,
+			CASE
+				WHEN T.IdEstatus = 1 THEN 'estatus_enaprobacion.png'
+				WHEN T.IdEstatus = 2 THEN 'estatus_aprobado.png'
+				WHEN T.IdEstatus = 3 THEN 'estatus_rechazado.png'
+				WHEN T.IdEstatus = 4 THEN 'estatus_cancelado.png"'
+				ELSE 'estatus_enaprobacion.png'
+			END AS ImageEstatus
+			FROM Petrovendor..TA_Tarea AS T
+				JOIN Petrovendor..TA_Operacion AS OP
+					ON T.IdOperacion = OP.IdOperacion
+					AND OP.IdOperacion = @IdOperacion
+					AND OP.IdTipoOperacion = @TipoOperacion
+				JOIN Petrovendor..S_Usuario AS US
+					ON T.IdAprobador = US.IdUsuario
+					AND US.Activo = 1 
+					AND ISNULL(US.IsEliminado,0) = 0
+				INNER JOIN Petrovendor..TA_Estatus AS E
+					ON T.IdEstatus = E.IdEstatus
+				INNER JOIN Petrovendor..TA_Estatus AS EFT
+					ON OP.IdEstatusOperacion = EFT.IdEstatus
+			ORDER BY T.NoSecuencia ASC;
 
 	END
 
