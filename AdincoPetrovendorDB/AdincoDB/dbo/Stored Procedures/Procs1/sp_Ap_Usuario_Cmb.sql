e108c99ca60dcdb0b3c6cde875c9eddc1c7bd7ce
Daniel Antonio <37602019+danielantoniocruz@users.noreply.github.com>
2024-02-01 17:20:39 -0600

    Se agrega consideracón de grupos en importaciones de usuarios

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/sp_Ap_Usuario_Cmb.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/sp_Ap_Usuario_Cmb.sql
@@ -1,19 +1,67 @@
-﻿
-CREATE proc sp_Ap_Usuario_Cmb  
+﻿USE [Adinco]
+GO
+IF EXISTS
+(
+    SELECT 1
+    FROM dbo.sysobjects
+    WHERE name = 'sp_Ap_Usuario_Cmb'
+)
+    DROP PROCEDURE sp_Ap_Usuario_Cmb;
+/****** Object:  StoredProcedure [dbo].[sp_Ap_Usuario_Cmb]    Script Date: 31/01/2024 04:38:05 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author: Daniel AC
+-- Create date: 31/01/2023
+-- Description:	Se agrega consulta para mostrar lista de usuarios 
+-- =============================================
+/****** Object:  StoredProcedure [dbo].[sp_Ap_Usuario_Cmb]    Script Date: 31/01/2024 04:27:44 p. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[sp_Ap_Usuario_Cmb] 
 (  
 	@IdContrato  int  
 )  
-as  
-begin  
-	select		u.UsuarioID,  
+AS  
+BEGIN  
+	
+	CREATE TABLE #UsuariosContratoActual(UsuarioID INT,Usuario NVARCHAR(MAX),Nombre NVARCHAR(MAX))
+
+	INSERT INTO #UsuariosContratoActual(UsuarioID,Usuario,Nombre)
+	SELECT		u.UsuarioID,  
 				u.Usuario,  
 				u.Nombre   
-	from		Ap_Usuario			u  
-	inner join	AP_PerfilUsuario	pu  
-	on			u.UsuarioID			=	pu.UsuarioID
-	and			IsActivo			=	1
-	inner join	AP_Perfil			p  
-	on			p.IdPerfil			=	pu.PerfilID  
-	where		p.IdContrato		=	@IdContrato  
+	FROM		Ap_Usuario			u  
+	INNER JOIN	AP_PerfilUsuario	pu  
+	ON			u.UsuarioID			=	pu.UsuarioID
+	AND			IsActivo			=	1
+	INNER JOIn	AP_Perfil			p  
+	ON			pu.PerfilID			=	p.IdPerfil	
+	WHERE		p.IdContrato		=	@IdContrato  
 	AND			ISNULL(IsGrupo,0)	=	0
+
+
+	INSERT INTO #UsuariosContratoActual(UsuarioID,Usuario,Nombre)
+	SELECT DISTINCT
+	   UG.UsuarioID,
+		UG.Nombre AS Usuario,
+		UG.Nombre
+		FROM	EN_GruposUsuarios GU
+		JOIN	AP_Usuario AS UG
+			ON	GU.IdGrupo	=	UG.UsuarioID
+		WHERE	GU.Activo	=	1
+			AND	UG.IsActivo	=	1
+			AND	GU.IdContrato	= @IdContrato
+
+	SELECT UsuarioID,  
+		   Usuario,  
+		   Nombre   
+	FROM #UsuariosContratoActual
+	ORDER BY Nombre ASC 
+
 end  
