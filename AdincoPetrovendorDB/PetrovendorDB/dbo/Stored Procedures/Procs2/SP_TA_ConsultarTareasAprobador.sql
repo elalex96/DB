948637fa19688b25281b3dd2f0a6ce672389d46c
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2024-02-08 15:22:19 -0600

    Update SP_TA_ConsultarTareasAprobador.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_TA_ConsultarTareasAprobador.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_TA_ConsultarTareasAprobador.sql
@@ -1,4 +1,9 @@
-﻿-- =============================================
+use Petrovendor
+go
+drop proc if exists SP_TA_ConsultarTareasAprobador
+go
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- =============================================
 -- Author:		Daniel Cruz
 -- Create date: 09-04-17
 -- Description:	Regresa la informaci�n de las tareas por aprobar por el usuario actual
@@ -8,39 +13,36 @@
 -- Create date: 15-08-2018
 -- Description:	Se filtran las aprobaciones de tipo serial, donde los aprobadores con numero de secuencia menos aun no han aprobado la operacion			
 -- =============================================
+-- Author:		Luis David
+-- Create date: 8 Febrero 2024
+-- Description:	Se reamocodan los joins y se agrega rango de fechas
+-- =============================================
 CREATE PROCEDURE SP_TA_ConsultarTareasAprobador
     -- Add the parameters for the stored procedure here
     @IdUsuario INT,
     @IdProveedor INT,
-    @IdContrato INT
+    @IdContrato INT,
+	@FechaInicio datetime null,
+	@FechaFin datetime null
 AS
 BEGIN
-	
-	--DECLARE @FlujoSerial TABLE(IdOperacion INT)
-
-	--INSERT INTO @FlujoSerial
-	--(
-	--    IdOperacion
-	--)
-	--SELECT IdOperacion 
-	--FROM dbo.FN_FlujoSerialNoAprobados (@IdUsuario, @IdProveedor, 2)
 
     SELECT O.IdOperacion,
            O.IdDocumento,
            O.FechaRegistro,
            O.Descripcion,           
 		   E.Nombre AS Nombre
-    FROM TA_Operacion AS O 
-	INNER JOIN TA_TipoOperacion AS OT ON OT.IdTipoOperacion = O.IdTipoOperacion
-    INNER JOIN TA_Estatus AS E ON E.IdEstatus = O.IdEstatusOperacion
-    INNER JOIN TA_TareaOperacion AS TTO ON TTO.IdOperacion = O.IdOperacion
-    INNER JOIN TA_Tarea AS T ON T.IdTarea = TTO.IdTarea
-	--LEFT JOIN @FlujoSerial fs ON fs.IdOperacion = O.IdOperacion AND fs.IdOperacion IS NULL -- Se excluyen los flujos de tipo serial que no han sido aprobados por aprobadores superiores
+    FROM TA_Operacion AS O (NOLOCK)
+	INNER JOIN TA_TipoOperacion AS OT (NOLOCK) ON O.IdTipoOperacion = OT.IdTipoOperacion
+    INNER JOIN TA_Estatus AS E (NOLOCK) ON O.IdEstatusOperacion = E.IdEstatus
+    INNER JOIN TA_TareaOperacion (NOLOCK) AS TTO ON O.IdOperacion = TTO.IdOperacion
+    INNER JOIN TA_Tarea AS T (NOLOCK) ON TTO.IdTarea = T.IdTarea
     WHERE T.IdAprobador = @IdUsuario
 			AND O.IdTipoOperacion = 2
 			AND O.IdProveedor = @IdProveedor
 			AND ISNULL(O.IdEstatusEliminado, 0) <> 1  --> MOSTRAR NO ELIMINADAS 
 			AND O.IdOperacion NOT IN (SELECT IdOperacion FROM dbo.FN_FlujoSerialNoAprobados(@IdUsuario,@IdProveedor,2))
+			AND O.FechaRegistro BETWEEN @FechaInicio and @FechaFin
 AND T.idestatus = 1 -->No mostrar si ya se aprobo MG 
     GROUP BY O.IdOperacion,
 			O.IdDocumento,
@@ -49,6 +51,5 @@ AND T.idestatus = 1 -->No mostrar si ya se aprobo MG
 			E.Nombre,
 			O.IdEstatusEliminado
     ORDER BY O.FechaRegistro DESC;
-
 
 END;
