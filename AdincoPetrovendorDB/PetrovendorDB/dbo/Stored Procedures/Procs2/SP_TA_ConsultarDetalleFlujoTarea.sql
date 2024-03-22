e9e3f72d1b81f1dc49a229b863be11702b9a4e84
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2024-03-22 11:27:09 -0600

    Update SP_TA_ConsultarDetalleFlujoTarea.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_TA_ConsultarDetalleFlujoTarea.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_TA_ConsultarDetalleFlujoTarea.sql
@@ -1,4 +1,7 @@
-﻿
+use Petrovendor
+go
+drop proc if exists SP_TA_ConsultarDetalleFlujoTarea
+go
 -- =============================================
 -- Author:		Daniel AC
 -- Create date: 29-03-17
@@ -9,31 +12,63 @@
 -- Create date: 10-01-2018
 -- Description:	Regresa la información de una Tarea Flujo + Parametros de orden de compra IdPedido				
 -- =============================================
-	CREATE  PROCEDURE [dbo].[SP_TA_ConsultarDetalleFlujoTarea] 
-	-- Add the parameters for the stored procedure here
-	 @IdOperacion int 
+-- Author:		David
+-- Create date: marzo 31 24
+-- Description:	Se optimiza sp Issue #2686 petrovendor
+-- =============================================
+-- Author:		David
+-- Create date: marzo 31 24
+-- Description:	Se optimiza sp Issue #2686 petrovendor
+-- =============================================
+	CREATE PROCEDURE [dbo].[SP_TA_ConsultarDetalleFlujoTarea] 
+    @IdOperacion int 
 AS
 BEGIN
-	 
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+    SET NOCOUNT ON;
 
-    -- Insert statements for procedure here
-	  SELECT  FT.Nombre,TAE.Nombre AS EstadoOperacion, TOO.IdEstadoFlujo, TFT.Nombre AS TipoFlujo,EFT.NombreEstado,TOO.IdAsignador,U.Nombre AS [Nombre Asignador], TOO.FechaRegistro, TOO.Descripcion, TP.Nombre, TV.DiaVencimiento, TOO.IdEstatusOperacion, U.Correo AS [Correo Asignador], TCC.Descripcion, TTO.NombreOperacion, TOO.IdDocumento,TOO.IdTipoOperacion, ISNULL(PS.IdPedido,0) AS IdPedido
-	  FROM TA_Operacion AS TOO
-	  INNER JOIN TA_FlujoTarea AS FT ON FT.IdFlujoTarea = TOO.IdFlujoTarea
-	  INNER JOIN TA_TipoFlujoTarea AS TFT ON TFT.IdTipoFlujoTarea = FT.IdTipoFlujo
-	  INNER JOIN TA_EstadoFlujoTarea AS EFT ON TOO.IdEstadoFlujo =  EFT.IdEstado
-	  INNER JOIN TA_TipoOperacion AS TTO ON TTO.IdTipoOperacion = TOO.IdTipoOperacion
-	  INNER JOIN TA_Estatus AS TAE ON TAE.IdEstatus = TOO.IdEstatusOperacion
-	  INNER JOIN S_Usuario AS U ON U.IdUsuario= TOO.IdAsignador
-	  INNER JOIN TA_Prioridad AS TP ON TP.IdPrioridad = TOO.IdPrioridad
-	  INNER JOIN TA_Vencimiento AS TV ON TV.IdVencimiento = TOO.IdVigencia
-	  LEFT JOIN TA_ComentariosTareaCancelada AS TCC ON TCC.IdOperacion = TOO.IdOperacion
-	  LEFT JOIN dbo.MM_Pedidos AS PS ON PS.IdIdentificador= TOO.IdDocumento AND TOO.IdProveedor= PS.IdProveedorCliente AND PS.IdTipoPedido=1 -- ORDEN DE COMPRA DIRECTA 
-	  WHERE  TOO.IdOperacion = @IdOperacion
-	 
+    SELECT  
+        FT.Nombre,
+        TAE.Nombre AS EstadoOperacion, 
+        TOO.IdEstadoFlujo, 
+        TFT.Nombre AS TipoFlujo,
+        EFT.NombreEstado,
+        TOO.IdAsignador,
+        U.Nombre AS [Nombre Asignador], 
+        TOO.FechaRegistro, 
+        TOO.Descripcion, 
+        TP.Nombre, 
+        TV.DiaVencimiento, 
+        TOO.IdEstatusOperacion, 
+        U.Correo AS [Correo Asignador], 
+        TCC.Descripcion, 
+        TTO.NombreOperacion, 
+        TOO.IdDocumento,
+        TOO.IdTipoOperacion, 
+        ISNULL(PS.IdPedido, 0) AS IdPedido
+    FROM 
+        TA_Operacion AS TOO (NOLOCK)
+    INNER JOIN 
+        TA_TipoOperacion AS TTO (NOLOCK) ON TOO.IdTipoOperacion = TTO.IdTipoOperacion
+    INNER JOIN 
+        S_Usuario AS U (NOLOCK) ON TOO.IdAsignador = U.IdUsuario
+    INNER JOIN 
+        TA_Prioridad AS TP (NOLOCK) ON TOO.IdPrioridad = TP.IdPrioridad
+    INNER JOIN 
+        TA_Vencimiento AS TV (NOLOCK) ON TOO.IdVigencia = TV.IdVencimiento
+    INNER JOIN 
+        TA_FlujoTarea AS FT (NOLOCK) ON TOO.IdFlujoTarea = FT.IdFlujoTarea
+    INNER JOIN 
+        TA_TipoFlujoTarea AS TFT (NOLOCK) ON FT.IdTipoFlujo = TFT.IdTipoFlujoTarea
+    INNER JOIN 
+        TA_EstadoFlujoTarea AS EFT (NOLOCK) ON EFT.IdEstado = TOO.IdEstadoFlujo
+    INNER JOIN 
+        TA_Estatus AS TAE (NOLOCK) ON TAE.IdEstatus = TOO.IdEstatusOperacion
+    LEFT JOIN 
+        TA_ComentariosTareaCancelada AS TCC (NOLOCK) ON TOO.IdOperacion = TCC.IdOperacion
+    LEFT JOIN 
+        dbo.MM_Pedidos AS PS (NOLOCK) ON TOO.IdDocumento  = PS.IdIdentificador
+                                            AND TOO.IdProveedor = PS.IdProveedorCliente 
+                                            AND PS.IdTipoPedido = 1 -- ORDEN DE COMPRA DIRECTA 
+    WHERE  
+        TOO.IdOperacion = @IdOperacion;
 END
-
-
