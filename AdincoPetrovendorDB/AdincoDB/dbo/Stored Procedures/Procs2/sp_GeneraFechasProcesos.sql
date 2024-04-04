69d4eb8188aa881e5ab7a085cd9afce8d916ddc0
Barbara Arrañaga <37219356+barbaraarranaga@users.noreply.github.com>
2024-04-04 13:36:41 -0600

    Update sp_GeneraFechasProcesos.sql

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_GeneraFechasProcesos.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_GeneraFechasProcesos.sql
@@ -28,7 +28,6 @@ CREATE TABLE #Actividades
     IdActividadSucesora     INT,
 	IdRegulador INT,
 	PRIMARY KEY (IdActividad)
-
 )
 
 CREATE TABLE #DiasHabiles
@@ -37,6 +36,7 @@ CREATE TABLE #DiasHabiles
     IdFecha DATE,
 	PRIMARY KEY (IdFecha)
 )
+
 CREATE TABLE #SucesorAntecesor
 (
     IdActividad     INT,
@@ -68,7 +68,7 @@ JOIN
 	AND	PA.IdProceso	=	@idProceso 
 	AND	PA.IdContrato	=	@IdContrato
 WHERE  PA.Activo = 1
-	   AND PA.Orden	>=	0
+ AND PA.Orden	>=	0
 ORDER	BY
 	PA.Orden;
 
@@ -115,7 +115,7 @@ BEGIN
 	SELECT
         A.IdFecha
     FROM 
-        dbo.AP_Calendario A (NOLOCK)
+		dbo.AP_Calendario A (NOLOCK)
     LEFT JOIN 
 		AP_CalendarioExcepciones	CE  (NOLOCK)
 		ON	A.IdFecha	=	CE.IdFecha 
@@ -129,7 +129,6 @@ BEGIN
         AND	CE.IdFecha	IS	NULL
     ORDER BY
         A.IdFecha
-
 END
 
 IF @IsSerie = 0 AND @MismoRegulador = 1
@@ -196,7 +195,7 @@ BEGIN
 		A.IdActividad,
 		A.NombreActividad,
 		A.Dias,
-	   A.DiasNaturales,
+		A.DiasNaturales,
 		@FechaInicial AS FechaInicial,
 		CASE WHEN A.DiasNaturales = 1 AND A.Dias <> 1
 			THEN DATEADD(DAY, A.Dias, @FechaInicial)
@@ -208,15 +207,15 @@ BEGIN
 			THEN DH.IdFecha
 		END AS FechaLimite
 	FROM
-		EN_Procesos P
+		EN_Procesos P	(NOLOCK)
 	JOIN
-		EN_ProcesosActividades  PA
+		EN_ProcesosActividades  PA	(NOLOCK)
 		ON  P.IdProceso =   PA.IdProceso
 		AND PA.IdContrato=@IdContrato
 		AND P.IdProceso =   @IdProceso
 		AND PA.Activo   =   1
 	JOIN
-		EN_Actividades  A
+		EN_Actividades  A	(NOLOCK)
 		ON  PA.IdActividad  =   A.IdActividad
 		AND   A.Activo    =   1
 	JOIN
@@ -229,7 +228,8 @@ BEGIN
 	JOIN
 		dbo.AP_Calendario   C2 (NOLOCK)
 		ON  @FechaInicial   <=  C2.IdFecha
-		AND C2.IdFecha  <   DATEADD(YEAR, 5, @FechaInicial)
+		AND C2.IdFecha  <  
+ DATEADD(YEAR, 5, @FechaInicial)
 		AND C2.FinDeSemana  =   0
 		AND C2.DiaLaborable = 1
 	WHERE
@@ -255,28 +255,56 @@ ELSE
 BEGIN
 
 	TRUNCATE TABLE #DiasHabiles
+	IF @IsSerie = 1
+	BEGIN
+		INSERT INTO #DiasHabiles
+		(	IdFecha		)
+		SELECT
+			A.IdFecha
+		FROM 
+			dbo.AP_Calendario A (NOLOCK)
+		LEFT JOIN 
+			AP_CalendarioExcepciones	CE  (NOLOCK)
+			ON	A.IdFecha	=	CE.IdFecha 
+			AND	CE.Activo	=	1 
+			AND	CE.IdRegulador	=	@Regulador
+		WHERE
+			A.IdFecha	>	@FechaInicial		-- CAMBIO PARA SHELL PARA AGREGAR UN DIA A LAS ACTIVIDADES 202007
+			AND	DATEADD(YEAR, 2,@FechaInicial)	>=	A.IdFecha
+			AND	FinDeSemana	=	0
+			AND	DiaLaborable	=	1
+			AND	CE.IdFecha	IS	NULL
+		ORDER BY
+			A.IdFecha
+	END
+	ELSE
+	BEGIN
+		INSERT INTO #DiasHabiles
+		(	IdFecha		)
+		SELECT
+			A.IdFecha
+		FROM 
+			dbo.AP_Calendario A (NOLOCK)
+		LEFT JOIN 
+			AP_CalendarioExcepciones	CE  (NOLOCK)
+			ON	A.IdFecha	=	CE.IdFecha 
+			AND	CE.Activo	=	1 
+			AND	CE.IdRegulador	=	@Regulador
+		WHERE
+			A.IdFecha	>=	@FechaInicial		-- CAMBIO PARA SHELL PARA NO AGREGAR UN DIA A LAS ACTIVIDADES CUANDO ES PARALELO 20230524
+			AND	DATEADD(YEAR, 2,@FechaInicial)	>=	A.IdFecha
+			AND	FinDeSemana	=	0
+			AND	DiaLaborable	=	1
+			AND	CE.IdFecha	IS	NULL
+		ORDER BY
+			A.IdFecha
+	END
 
-	INSERT INTO #DiasHabiles
-	(	IdFecha		)
-	SELECT
-		A.IdFecha
-	FROM 
-		dbo.AP_Calendario A (NOLOCK)
-	LEFT JOIN 
-		AP_CalendarioExcepciones	CE  (NOLOCK)
-		ON	A.IdFecha	=	CE.IdFecha 
-		AND	CE.Activo	=	1 
-		AND	CE.IdRegulador	=	@Regulador
-	WHERE
-		A.IdFecha	>	@FechaInicial		-- CAMBIO PARA SHELL PARA AGREGAR UN DIA A LAS ACTIVIDADES 202007
-		AND	DATEADD(YEAR, 2,@FechaInicial)	>=	A.IdFecha
-		AND	FinDeSemana	=	0
-		AND	DiaLaborable	=	1
-		AND	CE.IdFecha	IS	NULL
-	ORDER BY
-		A.IdFecha
+IF @Debug = 1
+BEGIN
+	SELECT * FROM #DiasHabiles
 
-
+END
 	INSERT INTO #Actividades
 	(
 		IdProceso,
@@ -414,12 +442,14 @@ BEGIN
 	SELECT TOP 1
        @FechaInicialSig = CASE @IsSerie
                               WHEN 1 THEN FechaLimite
-                                  --CASE  WHEN Orden > 0 THEN DATEADD(DAY, 1, FechaLimite)                                  --ELSE  FechaLimite    END
+                                  --CASE  WHEN Orden > 0 THEN DATEADD(DAY, 1, FechaLimite)
+                                  --ELSE  FechaLimite    END
                               ELSE	@FechaInicial
                           END,
        @IdActividad = IdActividadSucesora
 	   --@BitDiasNaturales = DiasNaturales,
 	   --@Regulador = IdRegulador
+
 	FROM #Actividades
 	WHERE FechaLimite IS NOT NULL
 	ORDER BY Orden DESC;
@@ -447,7 +477,8 @@ BEGIN
 	--		IF 10050 = (SELECT IDCONTRATISTA FROM CO_CONTRATO (NOLOCK) WHERE IDCONTRATO = @idContrato)
 	--		BEGIN
 	--			INSERT INTO #DiasHabiles
-	--			(
+	--
+			(
 	--				IdFecha
 	--			)
 	--			SELECT
@@ -461,8 +492,6 @@ BEGIN
 	--				AND DiaLaborable = 1
 	--			ORDER BY
 	--				A.IdFecha;
-		
-
 	--END
 	--		ELSE
 	--		BEGIN
@@ -477,6 +506,7 @@ BEGIN
 				WHERE
 					A.IdFecha >=@FechaInicialSig
 					AND DATEADD(YEAR, 2,@FechaInicialSig) >=    A.IdFecha
+
 					AND FinDeSemana =   0
 					AND DiaLaborable = 1
 				ORDER BY
@@ -487,8 +517,8 @@ BEGIN
 		 BEGIN
 			--IF 10050 = (SELECT IDCONTRATISTA FROM CO_CONTRATO (NOLOCK) WHERE IDCONTRATO = @idContrato)
 			--BEGIN
-			--	INSERT INTO #DiasHabiles
-			--	(
+			--	INSERT INTO #DiasHabiles	
+--	(
 			--		IdFecha
 			--	)
 			--	SELECT
@@ -538,8 +568,7 @@ BEGIN
 	ELSE
 	BEGIN
 		INSERT INTO #DiasHabiles
-		(
-			IdFecha
+		(	IdFecha
 		)
 		SELECT
 			A.IdFecha
@@ -587,6 +616,7 @@ BEGIN
 	--			FROM 
 	--				dbo.AP_Calendario A (NOLOCK)
 	--			LEFT JOIN 
+
 	--				AP_CalendarioExcepciones CE  (NOLOCK)
 	--				ON A.IdFecha=CE.IdFecha 
 	--				AND CE.Activo=1 
@@ -617,28 +647,25 @@ BEGIN
 	--		ORDER BY
 	--			A.IdFecha;
 	--	END
-
-
 	--END
 
     SELECT
         @NvaFechaIni = CASE WHEN A.DiasNaturales = 1
-				  THEN @FechaInicialSig
-                    WHEN A.DiasNaturales = 0 AND	FI.DiaLaborable = 1
-                    THEN @FechaInicialSig
-                    WHEN A.DiasNaturales = 0 AND FI.DiaLaborable = 0
-                    THEN MIN(C2.IdFecha)
-                END,
-  
+				THEN @FechaInicialSig
+                WHEN A.DiasNaturales = 0 AND	FI.DiaLaborable = 1
+                THEN @FechaInicialSig
+                WHEN A.DiasNaturales = 0 AND FI.DiaLaborable = 0
+                THEN MIN(C2.IdFecha)
+            END,
       @NvaFechaFin  = CASE WHEN A.DiasNaturales = 1 AND A.Dias <> 1
-                    THEN DATEADD(DAY, A.Dias, @FechaInicialSig)
-                    WHEN A.DiasNaturales = 1 AND A.Dias = 1
-					THEN @FechaInicialSig	
-					WHEN A.DiasNaturales = 0 AND FI.DiaLaborable = 1
-                    THEN DH.IdFecha 
-                    WHEN A.DiasNaturales = 0 AND FI.DiaLaborable = 0
-                    THEN DH.IdFecha
-		            END
+                THEN DATEADD(DAY, A.Dias, @FechaInicialSig)
+                WHEN A.DiasNaturales = 1 AND A.Dias = 1
+				THEN @FechaInicialSig	
+				WHEN A.DiasNaturales = 0 AND FI.DiaLaborable = 1
+                THEN DH.IdFecha 
+                WHEN A.DiasNaturales = 0 AND FI.DiaLaborable = 0
+                THEN DH.IdFecha
+		        END
     FROM
 	    #Actividades    A
     JOIN
@@ -659,7 +686,7 @@ BEGIN
   GROUP BY
         A.Dias,
 		A.DiasNaturales,
-	  FI.DiaLaborable,
+		FI.DiaLaborable,
        DH.IdFecha;
 
     UPDATE #Actividades 
