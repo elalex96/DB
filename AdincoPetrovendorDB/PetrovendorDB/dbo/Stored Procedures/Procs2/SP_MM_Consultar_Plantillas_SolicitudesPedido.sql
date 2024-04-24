69f2411f1c8356ef9cc81b292135f6b7ed5b83fe
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2024-04-24 13:41:15 -0600

    Update SP_MM_Consultar_Plantillas_SolicitudesPedido.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_Consultar_Plantillas_SolicitudesPedido.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_Consultar_Plantillas_SolicitudesPedido.sql
@@ -1,13 +1,23 @@
-﻿-- =============================================
+use petrovendor
+go
+drop proc if exists SP_MM_Consultar_Plantillas_SolicitudesPedido
+go
+-- =============================================
 -- Author:		<Alexander Gomez>
 -- Create date: <20/11/2019>
 -- Description:	<Consultar plantillas de solicitudes de pedido pendientes de enviar>
+-- =============================================
+-- Author:		<Luis David>
+-- Create date: <02/24/2024>
+-- Description:	<Se agrega el filtro por @FechaInicio y @FechaFin, Petrovendor #2737>
 -- =============================================
 CREATE PROCEDURE [dbo].[SP_MM_Consultar_Plantillas_SolicitudesPedido] 
 	-- Add the parameters for the stored procedure here
 	@IdProveedor INT,
 	@IdUsuario INT,
-	@IdContrato INT
+	@IdContrato INT,
+	@FechaInicio datetime,
+	@FechaFin datetime
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -32,9 +42,6 @@ BEGIN
 		AND SP.IdProveedor = @IdProveedor
 		AND SP.IdContrato = @IdContrato
 		AND ISNULL(SP.Enviada,0) = 0
+		AND CAST(SP.CreadoEl AS date) BETWEEN CAST(@FechaInicio AS date) AND CAST(@FechaFin AS date)
 	ORDER BY SP.CreadoEl DESC
-
-
-
-
 END
