fd094fef1350915b794a6e175518440dc23129e1
Pedro Pouchoulen <38993356+PedroPou@users.noreply.github.com>
2024-04-29 10:10:12 -0600

    se agrega el bit de activo a la cuenta

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/p_CO_GastosActualizar_Ins.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/p_CO_GastosActualizar_Ins.sql
@@ -1,4 +1,6 @@
-﻿IF EXISTS
+﻿USE Adinco;
+GO
+IF EXISTS
 (
     SELECT 1
     FROM dbo.sysobjects
@@ -190,8 +192,10 @@ BEGIN
 			   AND ISNULL(CO_GastosActualizar.CuentaContable, '') <> ''
         LEFT JOIN CO_CatalogoCuentaSH
             ON rtrim(CO_CatalogoCuentaSH.Nivel3) = rtrim(CO_GastosActualizar.CuentaContable)
-    WHERE CO_CatalogoCuentaSH.IdCatalogoCuentasSH IS NULL
-
+		INNER JOIN CO_VersionCatalogoCuentasSH
+			ON CO_CatalogoCuentaSH.IdVersion = CO_VersionCatalogoCuentasSH.IdVersion
+				AND CO_VersionCatalogoCuentasSH.Activo = 1
+    WHERE CO_CatalogoCuentaSH.IdCatalogoCuentasSH IS NULL AND CO_VersionCatalogoCuentasSH.Activo = 1
 
 	UPDATE CO_GastosActualizar
     SET Error = 1,
