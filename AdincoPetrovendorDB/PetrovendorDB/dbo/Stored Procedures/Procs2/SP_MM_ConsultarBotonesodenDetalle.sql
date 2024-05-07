bf200a6c76465e0e1dca9d40911d93c9b66a1069
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2024-05-07 13:25:20 -0600

    Update SP_MM_ConsultarBotonesodenDetalle.sql

--- AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_ConsultarBotonesodenDetalle.sql
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SP_MM_ConsultarBotonesodenDetalle.sql
@@ -1,7 +1,15 @@
-﻿--============================
+USE Petrovendor
+GO
+DROP PROC IF EXISTS SP_MM_ConsultarBotonesodenDetalle
+GO
+--============================
 --creado por: Luis David
 --creado el: 12/10/2021
 --descripcion: se utiliza para validar los botones del detalle de la aceptación
+--============================
+--creado por: Luis David
+--creado el: 06/MAYO/2024
+--descripcion: Se agrega el RFC de DNA para el flujo de aprobación de Aceptación de Servicios
 --============================
 CREATE PROCEDURE SP_MM_ConsultarBotonesodenDetalle
 @IdProveedor int,
@@ -9,12 +17,17 @@ CREATE PROCEDURE SP_MM_ConsultarBotonesodenDetalle
 @IdPedido int
 AS
 BEGIN
-	IF EXISTS (SELECT 1 FROM dbo.MM_Pedido AS AP 
-				JOIN dbo.S_Proveedor AS P ON AP.IdProveedorCompras = P.IdProveedor
-				WHERE AP.IdPedido = @IdPedido
-				AND
-				p.RFC = 'DDE151002QY9')
-			    BEGIN
-					SELECT 'MOSTRAR_BOTONES_DEA'
-				END
+	IF EXISTS (
+    SELECT 1 
+    FROM dbo.MM_Pedido AS AP 
+    JOIN dbo.S_Proveedor AS P ON AP.IdProveedorCompras = P.IdProveedor
+    WHERE AP.IdPedido = @IdPedido
+    AND p.RFC IN 
+	('DDE151002QY9', --WINTERSHALLDEA
+	'LIS130820563' --DNA SERVICIOS(LIS130820563)
+	) 
+	)
+	BEGIN
+		SELECT 'MOSTRAR_BOTONES_SAS'
+	END
 END
