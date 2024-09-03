0c070e9deda00f717c4bc33cc5a003c3e1c8f07d
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2024-09-03 15:06:26 -0600

    Create INS_PV_EnviaInvitacionPetrovendor.sql

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs3/INS_PV_EnviaInvitacionPetrovendor.sql
@@ -0,0 +1,56 @@
+USE Petrovendor
+GO
+DROP PROC IF EXISTS INS_PV_EnviaInvitacionPetrovendor
+GO
+CREATE PROC INS_PV_EnviaInvitacionPetrovendor
+@RFCEmisor varchar(100),
+@CorreoInvitado varchar(100),
+@IdUsuario int = null,
+@IdContrato int = null
+AS
+BEGIN
+	
+DECLARE @HTMLCORREOSINV NVARCHAR(MAX) = (SELECT HTML FROM TA_CORREO WHERE ASUNTO = 'Invitación para unirse a Petrovendor'),
+		@IDCORREO INT = (SELECT IdCorreo FROM TA_CORREO WHERE ASUNTO = 'Invitación para unirse a Petrovendor'),
+		@ASUNTOCORREO varchar(1000) = (SELECT Asunto FROM TA_CORREO WHERE ASUNTO = 'Invitación para unirse a Petrovendor'),
+		@EmpresaEmisora varchar(max) = (Select RazonSocial from S_Proveedor where RFC = @RFCEmisor and Activo = 1),
+		@IdNotificacion BIGINT,
+		@CorreoNotificaciones NVARCHAR(MAX)
+		;
+
+
+		SET @CorreoNotificaciones = (SELECT  TOP 1  CuentaRegistro
+								FROM TA_Correo AS C
+									INNER JOIN TA_CorreoServidor AS S
+										ON C.IdServidor = S.IdServidor
+								WHERE IdCorreo = @IDCORREO) --> CTE NUMERO CORREO (TA_Correo)
+
+        SET @HTMLCORREOSINV
+            = (REPLACE(@HTMLCORREOSINV, '##NombreEmpresa##', @EmpresaEmisora));
+        SET @HTMLCORREOSINV = (REPLACE(@HTMLCORREOSINV, '##ANIO_ACTUAL##', YEAR(GETDATE())));
+        SET @HTMLCORREOSINV
+            = (REPLACE(@HTMLCORREOSINV, '##DOMINIO##', 'https://petrovendor.com.mx/'));
+
+
+        SET @IdNotificacion = ((SELECT MAX(IdNotificacion) FROM Adinco.dbo.S_Notificacion) + 1);
+
+        INSERT INTO Adinco.dbo.S_Notificacion
+        (
+            IdNotificacion,
+            Para,
+            Asunto,
+            Mensaje,
+            FechaProgramadaEnvio,
+            Enviada,
+            FechaEnvio,
+            CreadoPor,
+            CreadoEl,
+            ModificadoPor,
+            ModificadoEl,
+            De
+        )
+        VALUES
+        (@IdNotificacion, @CorreoInvitado, @ASUNTOCORREO,
+         @HTMLCORREOSINV, DATEADD(MINUTE, 1, GETDATE()), 0, NULL, 3, GETDATE(), NULL, NULL,
+         ISNULL(@CorreoNotificaciones,''));
+END
