b75d9ebac13be551968c0c705bcf892bc6ff84f4
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2024-12-03 13:38:32 -0600

    Add files via upload

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs2/SEL_CO_ObtenInformacionContrato.sql
@@ -0,0 +1,13 @@
+USE Petrovendor
+DROP PROC IF EXISTS SEL_CO_ObtenInformacionContrato
+GO
+CREATE PROC SEL_CO_ObtenInformacionContrato
+@IdContrato int,
+@IdUsuario int
+AS 
+BEGIN
+	select * from adinco..co_contrato as C
+	join adinco..CO_TipoContrato as TC
+	on C.IdTipoContrato = tc.IdTipoContrato
+	where c.IdContrato = @IdContrato
+END
