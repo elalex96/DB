985894812049dcf144a46ba477fb1879a9a240c3
Pedro Pouchoulen <38993356+PedroPou@users.noreply.github.com>
2025-03-31 17:39:43 -0600

    Issue/Adinco/3149(Hotfix-Echo)-Estandares

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_OT_ConsultaSolicitudPrograma.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs2/sp_OT_ConsultaSolicitudPrograma.sql
@@ -20,13 +20,7 @@ SELECT @anioIni = MIN(YEAR(FechaProgramaInicio)),
 FROM OT_SolicitudMaterial (NOLOCK)
 WHERE IdOTSolicitud = @pIdOTSolicitud;
 
-;WITH Years
-AS (SELECT @anioIni AS anio
-    UNION ALL
-    SELECT anio + 1
-    FROM Years
-    WHERE anio < @anioFin
-   )
+
 INSERT INTO #tmpMeses
 (
     mes,
@@ -34,32 +28,22 @@ INSERT INTO #tmpMeses
     anio,
     anioMes
 )
-SELECT m.mes,
-       m.nombreMes,
-       y.anio,
-       CAST(y.anio AS VARCHAR(4)) + RIGHT('0' + CAST(m.mes AS VARCHAR(2)), 2) AS anioMes
-FROM Years y
-    CROSS JOIN
-    (
-        VALUES
-            (1, 'Enero'),
-            (2, 'Febrero'),
-            (3, 'Marzo'),
-            (4, 'Abril'),
-            (5, 'Mayo'),
-            (6, 'Junio'),
-            (7, 'Julio'),
-            (8, 'Agosto'),
-            (9, 'Septiembre'),
-            (10, 'Octubre'),
-            (11, 'Noviembre'),
-            (12, 'Diciembre')
-    ) m (mes, nombreMes)
-OPTION (MAXRECURSION 0);
+SELECT 
+	MES,
+	NombreMes,
+	ANIO,
+	CAST(Anio As Varchar(4)) + RIGHT('0' + CAST(Mes AS Varchar(2)), 2)
+FROM
+	AP_CALENDARIO (NOLOCK)
+WHERE ANIO BETWEEN @anioIni AND @anioFin
+GROUP BY 
+	MES, NombreMes, Anio,
+	CAST(Anio AS VARCHAR(4)) + RIGHT('0' + CAST(Mes AS VARCHAR(2)), 2)
 
 
-select IDPrograma = cast(sm.IdOTSolicitudMaterial as varchar) + '-' + cast(meses.Anio as varchar) + '-'
-                    + cast(meses.Mes as varchar),
+
+select IDPrograma = cast(sm.IdOTSolicitudMaterial AS VARCHAR) + '-' + cast(meses.Anio as varchar) + '-'
+                    + cast(meses.Mes AS VARCHAR),
        IdOTSolicitudMaterial = sm.IdOTSolicitudMaterial,
        FechaProgramaInicio = sm.FechaProgramaInicio,
        FechaProgramaFin = sm.FechaProgramaFin,
@@ -74,60 +58,8 @@ from OT_SolicitudMaterial sm (NOLOCK)
     inner join SC_Materiales sc (NOLOCK)
         on sm.IdSCMaterial = sc.IdSCMaterial
     inner join #tmpMeses meses (NOLOCK)
-        on meses.aniomes >= cast(cast((datepart(yy, sm.FechaProgramaInicio)) as varchar)
-                                 + cast(case
-                                            when datepart(mm, sm.FechaProgramaInicio) = 1 then
-                                                '01'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 2 then
-                                                '02'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 3 then
-                                                '03'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 4 then
-                                                '04'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 5 then
-                                                '05'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 6 then
-                                                '06'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 7 then
-                                                '07'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 8 then
-                                                '08'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 9 then
-                                                '09'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 10 then
-                                                '10'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 11 then
-                                                '11'
-                                            when datepart(mm, sm.FechaProgramaInicio) = 12 then
-                                                '12'
-                                        end as varchar) as int)
-           and meses.aniomes <= cast(cast((datepart(yy, sm.FechaProgramaFin)) as varchar)
-                                     + cast(case
-                                                when datepart(mm, sm.FechaProgramaFin) = 1 then
-                                                    '01'
-                                                when datepart(mm, sm.FechaProgramaFin) = 2 then
-                                                    '02'
-                                                when datepart(mm, sm.FechaProgramaFin) = 3 then
-                                                    '03'
-                                                when datepart(mm, sm.FechaProgramaFin) = 4 then
-                                                    '04'
-                                                when datepart(mm, sm.FechaProgramaFin) = 5 then
-                                                    '05'
-                                                when datepart(mm, sm.FechaProgramaFin) = 6 then
-                                                    '06'
-                                                when datepart(mm, sm.FechaProgramaFin) = 7 then
-                                                    '07'
-                                                when datepart(mm, sm.FechaProgramaFin) = 8 then
-                                                    '08'
-                                                when datepart(mm, sm.FechaProgramaFin) = 9 then
-                                                    '09'
-                                                when datepart(mm, sm.FechaProgramaFin) = 10 then
-                                                    '10'
-                                                when datepart(mm, sm.FechaProgramaFin) = 11 then
-                                                    '11'
-                                                when datepart(mm, sm.FechaProgramaFin) = 12 then
-                                                    '12'
-                                            end as varchar) as int)
+        on meses.aniomes >= CONVERT(INT, CONVERT(varchar(6), sm.FechaProgramaInicio, 112))
+           and meses.aniomes <= CONVERT(INT, CONVERT(VARCHAR(6), sm.FechaProgramaFin, 112))
     LEFT JOIN OT_SolicitudPrograma sp (NOLOCK)
         on sm.IdOTSolicitudMaterial = sp.IdOTSolicitudMaterial
            and meses.anio = sp.anio
