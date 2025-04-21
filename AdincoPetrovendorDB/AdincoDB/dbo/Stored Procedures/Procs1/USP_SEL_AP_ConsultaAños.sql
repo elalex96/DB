a6bd51cc0a6b94599265baeffe438468d3eda971
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2025-04-21 12:53:55 -0600

    Stored procedures

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/USP_SEL_AP_ConsultaAños.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/USP_SEL_AP_ConsultaAños.sql
@@ -1,108 +1,19 @@
-﻿
--- =============================================
--- Autor:				Neri Garcia del Angel
--- Fecha de Edición:	02 de Marzo del 2023
--- Descripción:			Se agregan LTRIM y RTRIM correspondientes
--- =============================================
-CREATE PROCEDURE [dbo].[ActualizarInsertarPozosAmat]
-    @IdUsuario INT,
-    @IdAreaContractual INT,
-    @IdInstalacion INT = 0, --> Cuando es 0 es Insert y cuando es mayor a 0 es update
-    @NombreInstalacion VARCHAR(4000),
-    @NombreInstalacionAlterno VARCHAR(4000),
-    @IdCatalogoSCIEP INT,
-    @IdCampo INT,
-    @Activo BIT
+﻿IF EXISTS
+    (
+        SELECT
+            1
+        FROM
+            dbo.sysobjects
+        WHERE
+            name = 'USP_SEL_AP_ConsultaAños'
+    )
+    DROP PROCEDURE USP_SEL_AP_ConsultaAños;
+GO
+CREATE PROCEDURE [dbo].[USP_SEL_AP_ConsultaAños]
+@IdContrato INT = 0, 
+@IdUsuario  INT = 0
 AS
-BEGIN
-    DECLARE @IdActividad INT,
-            @IdEstatus INT,
-            @IdYacimiento INT
-
-    SELECT TOP 1
-        @IdActividad = IdActividad
-    FROM CO_ActividadCIEP
-    WHERE LTRIM(RTRIM(UPPER(NombreActividad))) = 'POZOS'
-
-    SELECT TOP 1
-        @IdEstatus = idEstatus
-    FROM CO_EstadoPozos
-    WHERE LTRIM(RTRIM(UPPER(TipoEstatus))) = 'ACTIVO'
-
-    SELECT TOP 1
-        @IdYacimiento = IdYacimiento
-    FROM CO_AreaContractualYacimiento
-    WHERE IdAreaContractual = @IdAreaContractual
-
-    IF (ISNULL(@IdInstalacion, 0) <> 0)
-    BEGIN
-        UPDATE CO_Instalacion
-        SET NombreInstalacion =  LTRIM(RTRIM(@NombreInstalacion)),
-            IdInstalacionPemex = NULL,
-            EsBolsa = 0,
-            IdActividad = @IdActividad,             --> Asignar el id correspondiente a pozos
-            IdUsuario = @IdUsuario,
-            FecMovto = GETDATE(),
-            NombreInstalacionAlterno =  LTRIM(RTRIM(@NombreInstalacionAlterno)),
-            IdCatalogoSCIEP = @IdCatalogoSCIEP,
-            IdAreaContractual = @IdAreaContractual, --> Dato interno que no se muestra en pantalla
-            Activo = @Activo,
-            CUIP = NULL,
-            IdYacimiento = @IdYacimiento,           --> Id del yacimiento default del area contractual
-            IdCampo = @IdCampo,
-            UTMX = 0,
-            UTMY = 0,
-            IdEstatus = @IdEstatus,                 --> id del estatus activo de la tabla co_estadopozos
-            ModificadoPor = @IdUsuario,
-            ModificadoEn = GETDATE(),
-            ComodinBolsa = 0
-        WHERE IdInstalacion = @IdInstalacion
-    END
-
-    IF (ISNULL(@IdInstalacion, 0) = 0)
-    BEGIN
-        INSERT INTO CO_Instalacion
-        (
-            NombreInstalacion,
-            IdInstalacionPemex,
-            EsBolsa,
-            IdActividad,              --> Asignar el id correspondiente a pozos
-            IdUsuario,                --> Dato interno que no se muestra en pantalla
-            FecMovto,                 --> Dato interno que no se muestra en pantalla
-            NombreInstalacionAlterno, --> Nombre alterno que ingresa el usuario
-            IdCatalogoSCIEP,          --> ID SCIEP que ingresa el usuario
-            IdAreaContractual,        --> Dato interno que no se muestra en pantalla
-            Activo,                   --> Indicador si el pozo esta activo o no
-            CUIP,                     --> NULL
-            WelIID,                   --> Id del pozo en caso de existir, NULL para registros nuevos
-            IdYacimiento,             --> Id del yacimiento default del area contractual
-            IdCampo,                  --> Id de la macropera (mostrar combo)
-            UTMX,                     --> 0
-            UTMY,                     --> 0
-            IdEstatus,                --> id del estatus activo de la tabla co_estadopozos
-            CreadoPor,                --> Dato interno que no se muestra en pantalla
-            CreadoEn,                 --> Dato interno que no se muestra en pantalla
-            ComodinBolsa              --> 0)
-        )
-        SELECT  LTRIM(RTRIM(@NombreInstalacion)),
-               NULL,
-               0,
-               @IdActividad,
-               @IdUsuario,
-               GETDATE(),
-                LTRIM(RTRIM(@NombreInstalacionAlterno)),
-               @IdCatalogoSCIEP,
-               @IdAreaContractual,
-               @Activo,
-               NULL,
-               NULL,
-               @IdYacimiento,
-               @IdCampo,
-               0,
-               0,
-               @IdEstatus,
-               @IdUsuario,
-               GETDATE(),
-               0
-    END
-END
+     BEGIN
+         SET NOCOUNT ON;
+         SELECT DISTINCT Anio FROM AP_CALENDARIO (NOLOCK) WHERE Anio <= YEAR(DATEADD(YEAR,1,GETDATE())) ORDER BY ANIO DESC
+     END;
--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/USP_SEL_CP_ParametroRegalia.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/USP_SEL_CP_ParametroRegalia.sql
@@ -1,108 +1,21 @@
-﻿
--- =============================================
--- Autor:				Neri Garcia del Angel
--- Fecha de Edición:	02 de Marzo del 2023
--- Descripción:			Se agregan LTRIM y RTRIM correspondientes
--- =============================================
-CREATE PROCEDURE [dbo].[ActualizarInsertarPozosAmat]
+﻿USE Adinco
+
+GO
+IF EXISTS
+    (
+        SELECT
+            1
+        FROM
+            dbo.sysobjects
+        WHERE
+            name = 'USP_SEL_CP_ParametroRegalia'
+    )
+    DROP PROCEDURE USP_SEL_CP_ParametroRegalia;
+GO
+CREATE PROCEDURE USP_SEL_CP_ParametroRegalia
     @IdUsuario INT,
-    @IdAreaContractual INT,
-    @IdInstalacion INT = 0, --> Cuando es 0 es Insert y cuando es mayor a 0 es update
-    @NombreInstalacion VARCHAR(4000),
-    @NombreInstalacionAlterno VARCHAR(4000),
-    @IdCatalogoSCIEP INT,
-    @IdCampo INT,
-    @Activo BIT
+    @IdContrato INT
 AS
 BEGIN
-    DECLARE @IdActividad INT,
-            @IdEstatus INT,
-            @IdYacimiento INT
-
-    SELECT TOP 1
-        @IdActividad = IdActividad
-    FROM CO_ActividadCIEP
-    WHERE LTRIM(RTRIM(UPPER(NombreActividad))) = 'POZOS'
-
-    SELECT TOP 1
-        @IdEstatus = idEstatus
-    FROM CO_EstadoPozos
-    WHERE LTRIM(RTRIM(UPPER(TipoEstatus))) = 'ACTIVO'
-
-    SELECT TOP 1
-        @IdYacimiento = IdYacimiento
-    FROM CO_AreaContractualYacimiento
-    WHERE IdAreaContractual = @IdAreaContractual
-
-    IF (ISNULL(@IdInstalacion, 0) <> 0)
-    BEGIN
-        UPDATE CO_Instalacion
-        SET NombreInstalacion =  LTRIM(RTRIM(@NombreInstalacion)),
-            IdInstalacionPemex = NULL,
-            EsBolsa = 0,
-            IdActividad = @IdActividad,             --> Asignar el id correspondiente a pozos
-            IdUsuario = @IdUsuario,
-            FecMovto = GETDATE(),
-            NombreInstalacionAlterno =  LTRIM(RTRIM(@NombreInstalacionAlterno)),
-            IdCatalogoSCIEP = @IdCatalogoSCIEP,
-            IdAreaContractual = @IdAreaContractual, --> Dato interno que no se muestra en pantalla
-            Activo = @Activo,
-            CUIP = NULL,
-            IdYacimiento = @IdYacimiento,           --> Id del yacimiento default del area contractual
-            IdCampo = @IdCampo,
-            UTMX = 0,
-            UTMY = 0,
-            IdEstatus = @IdEstatus,                 --> id del estatus activo de la tabla co_estadopozos
-            ModificadoPor = @IdUsuario,
-            ModificadoEn = GETDATE(),
-            ComodinBolsa = 0
-        WHERE IdInstalacion = @IdInstalacion
-    END
-
-    IF (ISNULL(@IdInstalacion, 0) = 0)
-    BEGIN
-        INSERT INTO CO_Instalacion
-        (
-            NombreInstalacion,
-            IdInstalacionPemex,
-            EsBolsa,
-            IdActividad,              --> Asignar el id correspondiente a pozos
-            IdUsuario,                --> Dato interno que no se muestra en pantalla
-            FecMovto,                 --> Dato interno que no se muestra en pantalla
-            NombreInstalacionAlterno, --> Nombre alterno que ingresa el usuario
-            IdCatalogoSCIEP,          --> ID SCIEP que ingresa el usuario
-            IdAreaContractual,        --> Dato interno que no se muestra en pantalla
-            Activo,                   --> Indicador si el pozo esta activo o no
-            CUIP,                     --> NULL
-            WelIID,                   --> Id del pozo en caso de existir, NULL para registros nuevos
-            IdYacimiento,             --> Id del yacimiento default del area contractual
-            IdCampo,                  --> Id de la macropera (mostrar combo)
-            UTMX,                     --> 0
-            UTMY,                     --> 0
-            IdEstatus,                --> id del estatus activo de la tabla co_estadopozos
-            CreadoPor,                --> Dato interno que no se muestra en pantalla
-            CreadoEn,                 --> Dato interno que no se muestra en pantalla
-            ComodinBolsa              --> 0)
-        )
-        SELECT  LTRIM(RTRIM(@NombreInstalacion)),
-               NULL,
-               0,
-               @IdActividad,
-               @IdUsuario,
-               GETDATE(),
-                LTRIM(RTRIM(@NombreInstalacionAlterno)),
-               @IdCatalogoSCIEP,
-               @IdAreaContractual,
-               @Activo,
-               NULL,
-               NULL,
-               @IdYacimiento,
-               @IdCampo,
-               0,
-               0,
-               @IdEstatus,
-               @IdUsuario,
-               GETDATE(),
-               0
-    END
+	SELECT IdParametroRegalias,Anio,An,Bn,Cn,Dn,En,Fn,Gn,Hn	FROM CP_ParametroRegalia (NOLOCK);
 END
