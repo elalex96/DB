88ad04f7436b642e8aceda99d1138ee6c31dfab1
ReynaOlvera <38890234+ReynaOlvera@users.noreply.github.com>
2025-06-26 17:28:41 -0600

    Ajustes

---
+++ AdincoPetrovendorDB/AdincoDB/dbo/Tables/Tables1/APP_Preferencias.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE APP_Preferencias(
+Id	int identity (1,1),
+Nombre	varchar(1000),
+EsDeContratista	bit,
+Descripcion	varchar(1000),
+RequiereValor	bit,
+Constraint  PK_APP_Preferencias primary key(Id)
+)
