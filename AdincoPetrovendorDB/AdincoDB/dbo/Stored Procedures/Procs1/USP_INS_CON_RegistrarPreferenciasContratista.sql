da5fbe56acef6d6966c42a07ec7c5c1ea563e976
Marcos Garcia <marcos.garcia@adinco.mx>
2025-08-01 12:59:49 -0600

    Update USP_INS_CON_RegistrarPreferenciasContratista

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/USP_INS_CON_RegistrarPreferenciasContratista.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/USP_INS_CON_RegistrarPreferenciasContratista.sql
@@ -12,8 +12,69 @@ CREATE PROCEDURE [dbo].[USP_INS_CON_RegistrarPreferenciasContratista] @IdUsuario
 	,@Preferencias CO_Type_ContratistaPreferencias READONLY
 AS
 BEGIN
-	DECLARE @Fecha DATETIME = GETDATE();
+	DECLARE @Fecha DATETIME = GETDATE()
+		,@Id INT
+		,@Valor VARCHAR(5000);
+
 	BEGIN TRY
+		CREATE TABLE #ContratistaPreferencias (
+			Id INT NULL
+			,Valor VARCHAR(5000) NULL
+			,EstaRegistrado BIT NULL
+			,ContratistaPreferenciaId INT NULL
+			);
+
+		INSERT INTO #ContratistaPreferencias (
+			Id
+			,Valor
+			,EstaRegistrado
+			,ContratistaPreferenciaId
+			)
+		SELECT Id
+			,Valor
+			,0
+			,0
+		FROM @Preferencias
+
+		UPDATE #ContratistaPreferencias
+		SET ContratistaPreferenciaId = CON_ContratistaPreferencias.Id
+			,EstaRegistrado = 1
+		FROM #ContratistaPreferencias
+		JOIN CON_ContratistaPreferencias ON #ContratistaPreferencias.Id = CON_ContratistaPreferencias.PreferenciaId
+			AND @IdContratista = CON_ContratistaPreferencias.ContratistaId
+
+		DECLARE cur CURSOR
+		FOR
+		SELECT ContratistaPreferenciaId
+			,Valor
+		FROM #ContratistaPreferencias
+		WHERE EstaRegistrado = 1;
+
+		OPEN cur;
+
+		FETCH NEXT
+		FROM cur
+		INTO @Id
+			,@Valor;
+
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			EXEC USP_UPD_CON_ActualizaPreferenciasContratistaRelacionadas @IdUsuario = @IdUsuario
+				,@IdContrato = @IdContrato
+				,@IdContratista = @IdContratista
+				,@Id = @Id
+				,@Valor = @Valor;
+
+			FETCH NEXT
+			FROM cur
+			INTO @Id
+				,@Valor;
+		END
+
+		CLOSE cur;
+
+		DEALLOCATE cur;
+
 		INSERT INTO CON_ContratistaPreferencias (
 			ContratistaId
 			,PreferenciaId
@@ -26,7 +87,8 @@ BEGIN
 			,Valor
 			,@IdUsuario
 			,@Fecha
-		FROM @Preferencias;
+		FROM #ContratistaPreferencias
+		WHERE EstaRegistrado = 0;
 
 		-- Insertar en Bitácora
 		-- Insertar en Bitácora por cada preferencia registrada
@@ -44,8 +106,11 @@ BEGIN
 			,'Valor: ' + ISNULL(p.Valor, '') + ', del registro insertado para la preferencia [' + ISNULL(c.Nombre, '') + '], contratistaId [' + CAST(@IdContratista AS VARCHAR(20)) + '].' AS Detalle
 			,@IdUsuario AS UsuarioId
 			,@IdContrato AS ContratoId
-		FROM @Preferencias p
-		INNER JOIN APP_Preferencias c ON c.Id = p.Id;-- Para obtener el nombre
+		FROM #ContratistaPreferencias p
+		INNER JOIN APP_Preferencias c ON c.Id = p.Id
+			AND p.EstaRegistrado = 0;-- Para obtener el nombre
+
+		DROP TABLE #ContratistaPreferencias
 
 		-- Retornar registro actualizado
 		SELECT *
