74a67a7455217c3a35ef030df13262cc38f86f30
reyna.olvera <reyna.olvera@ADINCO-LTP002>
2025-11-05 08:17:57 -0600

    Ajustes

--- AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/USP_INS_UPD_CO_ActualizaMarkupRegistros.sql
+++ AdincoPetrovendorDB/AdincoDB/dbo/Stored Procedures/Procs1/USP_INS_UPD_CO_ActualizaMarkupRegistros.sql
@@ -10,118 +10,110 @@
     DROP PROCEDURE USP_INS_UPD_CO_ActualizaMarkupRegistros;
 GO
 CREATE PROCEDURE [dbo].[USP_INS_UPD_CO_ActualizaMarkupRegistros]
-    @IdUsuario  INT = 0,
-    @IdContrato INT,
-    @IdCarga    INT
+@IdUsuario            INT = 0,
+@IdContrato            INT,
+@IdCarga  INT
 AS
-    BEGIN
-        --drop table #BitacoraCorrectos
-        CREATE TABLE #BitacoraCorrectos
-            (
-                FilaExcel         int,
-                IdRegistro        int,
-                IdContrato        INT,
-                Porcentaje        float,
-                TipoCambio        float,
-                PorcentajeDecimal float,
-                MontoRegistro     FLOAT,
-                MontoEquivalente  FLOAT
-            );
+BEGIN
+--drop table #BitacoraCorrectos
+	CREATE TABLE #BitacoraCorrectos (FilaExcel	int,IdRegistro	int,IdContrato INT, Porcentaje	float NULL,TipoCambio	float NULL, PorcentajeDecimal	float NULL, MontoRegistro FLOAT NULL,MontoEquivalente FLOAT NULL);
 
-        INSERT INTO #BitacoraCorrectos
-            (
-                FilaExcel,
-                IdRegistro,
-                Porcentaje,
-                TipoCambio,
-                MontoRegistro,
-                IdContrato
-            )
-                    SELECT
-                        CO_BitacoraCargaRegistroMarkupDetalle.FilaExcel,
-                        CO_BitacoraCargaRegistroMarkupDetalle.IdRegistro,
-                        CO_BitacoraCargaRegistroMarkupDetalle.Porcentaje,
-                        CO_BitacoraCargaRegistroMarkupDetalle.TipoCambio,
-                        CO_Registro.MontoRegistro,
-                        CO_BitacoraCargaRegistroMarkup.IdContrato
-                    FROM
-                        CO_BitacoraCargaRegistroMarkupDetalle (NOLOCK)
-                        JOIN
-                            CO_BitacoraCargaRegistroMarkup (NOLOCK)
-                                ON CO_BitacoraCargaRegistroMarkupDetalle.IdCarga = CO_BitacoraCargaRegistroMarkup.Id
-                                   AND CO_BitacoraCargaRegistroMarkup.Id = @IdCarga
-                        JOIN
-                            CO_Registro (NOLOCK)
-                                ON CO_BitacoraCargaRegistroMarkupDetalle.IdRegistro = CO_Registro.IdRegistro
-                    WHERE
-                        IdCarga = @IdCarga
-                        AND Correcto = 1;
+	INSERT INTO #BitacoraCorrectos(FilaExcel,IdRegistro	,Porcentaje	,TipoCambio,MontoRegistro,IdContrato )
+	SELECT CO_BitacoraCargaRegistroMarkupDetalle.FilaExcel,
+	CO_BitacoraCargaRegistroMarkupDetalle.IdRegistro	,
+	ISNULL(CO_BitacoraCargaRegistroMarkupDetalle.Porcentaje,0),
+	CO_BitacoraCargaRegistroMarkupDetalle.TipoCambio,
+	CO_Registro.MontoRegistro,
+	CO_BitacoraCargaRegistroMarkup.IdContrato
+	FROM 
+		CO_BitacoraCargaRegistroMarkupDetalle (NOLOCK)
+	JOIN
+		CO_BitacoraCargaRegistroMarkup (NOLOCK)
+		ON CO_BitacoraCargaRegistroMarkupDetalle.IdCarga	=	CO_BitacoraCargaRegistroMarkup.Id
+		AND CO_BitacoraCargaRegistroMarkup.Id = @IdCarga
+	JOIN
+		CO_Registro (NOLOCK)
+		ON CO_BitacoraCargaRegistroMarkupDetalle.IdRegistro	=	CO_Registro.IdRegistro
+	WHERE IdCarga = @IdCarga AND Correcto = 1;
 
-        UPDATE
-            #BitacoraCorrectos
-        SET
-            PorcentajeDecimal = Porcentaje / 100;
-
-        UPDATE
-            #BitacoraCorrectos
-        SET
-            MontoEquivalente = MontoRegistro * PorcentajeDecimal;
-
-        UPDATE
-            CO_RegistroMarkup
-        SET
-            CO_RegistroMarkup.Porcentaje = BC.PORCENTAJE,
-            CO_RegistroMarkup.MONTOEQUIVALENTE = BC.MONTOEQUIVALENTE,
-            CO_RegistroMarkup.TipoCambio = BC.TipoCambio,
-            CO_RegistroMarkup.MontoGasto = BC.MONTOREGISTRO,
-            ModificadoPor = @IdUsuario,
-            ModificadoEn = GETDATE()
-        FROM
-            #BitacoraCorrectos BC
-            JOIN
-                CO_RegistroMarkup
-                    ON BC.IdRegistro = CO_RegistroMarkup.GastoId
-                       AND CO_RegistroMarkup.Porcentaje = 0
-        WHERE
-            CO_RegistroMarkup.Activo = 1;
+	/*SE MANTIENE EL PORCENTAJE DE MARKUP EXISTENTE SI EN LA NUEVA IMPORTACIÓN DE ARCHIVO AGREGAN UN MARKUP VACIO Ó 0 CUANDO YA CONTIENE UN MARKUP GUARDADO*/
+	UPDATE BC
+	SET BC.Porcentaje = CO_RegistroMarkup.Porcentaje
+		FROM 
+		#BitacoraCorrectos BC
+	JOIN 
+		CO_RegistroMarkup (NOLOCK)
+		ON	BC.IdRegistro	=	CO_RegistroMarkup.GastoId
+		AND CO_RegistroMarkup.Porcentaje > 0 AND BC.Porcentaje = 0
+		WHERE 
+		CO_RegistroMarkup.Activo = 1;
 
 
-        INSERT INTO CO_RegistroMarkup
-            (
-                GastoId,
-                Porcentaje,
-                MontoEquivalente,
-                MontoGasto,
-                TipoCambio,
-                Activo,
-                CreadoPor,
-                CreadoEn,
-                ContratoId
-            )
-                    SELECT
-                        BC.IdRegistro,
-                        BC.PORCENTAJE,
-                        BC.MONTOEQUIVALENTE,
-                        BC.MONTOREGISTRO,
-                        BC.TipoCambio,
-                        1,
-                        @IdUsuario,
-                        GETDATE(),
-                        BC.IdContrato
-                    FROM
-                        #BitacoraCorrectos BC
-                        LEFT JOIN
-                            CO_RegistroMarkup (NOLOCK)
-                                ON BC.IdRegistro = CO_RegistroMarkup.GastoId
-                    WHERE
-                        CO_RegistroMarkup.GastoId IS NULL;
+    /*CALCULOS POR EL MARKUP ESTABLECIDO*/
+	UPDATE 
+	#BitacoraCorrectos
+	SET PorcentajeDecimal = Porcentaje/100;
+	 
+	UPDATE 
+	#BitacoraCorrectos
+	SET MontoEquivalente = MontoRegistro*PorcentajeDecimal;
+
+	UPDATE CO_RegistroMarkup
+	SET CO_RegistroMarkup.Porcentaje = BC.PORCENTAJE,
+		CO_RegistroMarkup.MONTOEQUIVALENTE	=	BC.MONTOEQUIVALENTE,
+		CO_RegistroMarkup.TipoCambio	=	
+		CASE
+		WHEN ISNULL(BC.TipoCambio,0) > 0 -- Si se agrega un tipo de cambio con valor se actualiza,si no se mantiene el guardado en la carga anterior
+		THEN 
+			BC.TipoCambio
+		ELSE 
+			CO_RegistroMarkup.TipoCambio
+		END,
+		CO_RegistroMarkup.MontoGasto	=	BC.MONTOREGISTRO,
+		ModificadoPor = @IdUsuario,
+		ModificadoEn = 	GETDATE()
+	FROM 
+		#BitacoraCorrectos BC
+	JOIN 
+		CO_RegistroMarkup (NOLOCK)
+		ON	BC.IdRegistro	=	CO_RegistroMarkup.GastoId
+		WHERE 
+		CO_RegistroMarkup.Activo = 1;
 
 
-        UPDATE
-            CO_BitacoraCargaRegistroMarkup
-        SET
-            MarkupRegistrado = 1
-        WHERE
-            Id = @IdCarga;
+	INSERT INTO CO_RegistroMarkup(GastoId,
+		Porcentaje,
+		MontoEquivalente,
+		MontoGasto,
+		TipoCambio,
+		Activo,
+		CreadoPor,
+		CreadoEn,
+		ContratoId)
+SELECT BC.IdRegistro,
+		BC.PORCENTAJE,
+		BC.MONTOEQUIVALENTE,
+		BC.MONTOREGISTRO,
+		BC.TipoCambio,
+		1,
+		@IdUsuario,
+		GETDATE(),
+		BC.IdContrato
+	FROM 
+		#BitacoraCorrectos BC
+	LEFT JOIN 
+		CO_RegistroMarkup (NOLOCK)
+		ON	BC.IdRegistro	=	CO_RegistroMarkup.GastoId
+	WHERE
+		CO_RegistroMarkup.GastoId IS NULL;
+	
 
-    END;
+	UPDATE CO_BitacoraCargaRegistroMarkup
+	SET MarkupRegistrado	=	1
+		WHERE Id= @IdCarga;
+			
+END;
+
+
+
+
