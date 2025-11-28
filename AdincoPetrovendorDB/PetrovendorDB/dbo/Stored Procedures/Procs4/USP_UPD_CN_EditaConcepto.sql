76dc7d7dfb999c32861c9e0d52d1676996927521
Luis David De La Cruz Bautista <38890100+Adincodavid@users.noreply.github.com>
2025-11-28 13:06:44 -0600

    Add files via upload

---
+++ AdincoPetrovendorDB/PetrovendorDB/dbo/Stored Procedures/Procs4/USP_UPD_CN_EditaConcepto.sql
@@ -0,0 +1,24 @@
+Use Petrovendor
+GO
+DROP PROC IF EXISTS USP_UPD_CN_EditaConcepto
+GO
+CREATE PROC USP_UPD_CN_EditaConcepto
+@IdCDCN INT,
+@IdActividadBS INT,
+@DescripcionBienesServicios VARCHAR(1000),
+@ValorFactura FLOAT,
+@PCN FLOAT,
+@ClasificacionSH INT,
+@IdUsuario INT = NULL,
+@IdContrato INT = NULL
+AS
+BEGIN
+	UPDATE CN_CompraDirecta
+	SET IdActividadBS = @IdActividadBS,
+	DescripcionBienesServicios = @DescripcionBienesServicios,
+	ValorFactura = @ValorFactura,
+	PCN = @PCN,
+	ModificadoPor = @IdUsuario,
+	ModificadoEl = GETDATE()
+	WHERE IdCDCN = @IdCDCN
+END
